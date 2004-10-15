08697d901b49bd42aa2f43f99ab1eff0eda00984
Steven R. Loomis <srl@icu-project.org>
2004-10-15 02:02:06 +0000

    CLDR-247 update to the format
    X-SVN-Rev: 511

--- tools/scripts/interimVettingTool.sh
+++ tools/scripts/interimVettingTool.sh
@@ -4,39 +4,74 @@
 #
 # Run this in locale/tools and it will create an InterimVettingChart in ../vetting/main
 
+# Prefix - will only show files which match the prefix.
+MATCHIT=
+#MATCHIT=ar
+#MATCHIT=fi
+
 write_index()
 {
     TREE=$1
     OUTF=${TREE}/index.html
     
     echo "INFO: Writing index file " ${OUTF}
-    echo "<h1>CLDR 1.2a - Drafts and Alts</h1>" > ${OUTF}
+    cat > ${OUTF} <<"EOF"
+<html>
+<head>
+ <title>Draft/ALT for CLDR</title>
+ <style>
+         <!--
+         table        { border-spacing: 0; border-collapse: collapse;  
+                        border: 1px solid black }
+         td, th       { border-spacing: 0; border-collapse: collapse;  color: black; 
+                        vertical-align: top; border: 1px solid black }
+         -->
+     </style> 
+ </head><body bgcolor="white">
+<h1>CLDR 1.2a - Drafts and Alts</h1>
+EOF
+
     date >> ${OUTF}
     echo '<br/>' >> ${OUTF}
     echo '<h3><a href="http://www.jtcsv.com/cgibin/cldrwiki.pl?InterimVettingCharts">What is this? Click here!</a></h3>' >> ${OUTF}
     echo '<br/>' >> ${OUTF}
-    echo "<ul>" >> ${OUTF}
-    for file in `cd ${TREE} ; ls *.html | fgrep -v index`;
-      do
-      fileb=`basename ${file} .html`
-      echo "  <li><a href=\"${file}\">${fileb}</a></li>" >> ${OUTF}
-    done
+    cat >> ${OUTF} <<EOF
+<table border=1>
+ <tr><th>Locale</th><th>Name</th><th># of changes</th><th>CVS</th></tr>
+EOF
+
+    cat ${TREE}/*.idx >> ${OUTF}
+    rm ${TREE}/*.idx
+
+    cat >> ${OUTF} <<EOF
+</table>
+<p>
+EOF
     
-    echo "</ul>" >> ${OUTF}
-    echo "<hr>" >> ${OUTF}
     echo '<h3><a href="http://www.jtcsv.com/cgibin/cldrwiki.pl?InterimVettingCharts">What is this? Click here!</a></h3>' >> ${OUTF}
     echo '<br/>' >> ${OUTF}
     echo '<i>Interim page - subject to change.</i>   ' >> ${OUTF}
     date >> ${OUTF}    
+    cat >> ${OUTF} <<EOF
+</body>
+</html>
+EOF
+
+}
+
+backup_tree()
+{
+    TREE=$1
+    rm -rf ${TREE}.backup1
+    mv ${TREE}.backup0 ${TREE}.backup1
+    mv ${TREE} ${TREE}.backup1
 }
 
 compare_tree()
 {
     TREE=$1
-    mv ${TREE}.backup0 ${TREE}.backup1
-    mv ${TREE} ${TREE}.backup1
     mkdir ${TREE}
-    for file in `cd ../common/${TREE} ; ls *.xml | fgrep -v supplementalData`;
+    for file in `cd ../common/${TREE} ; ls ${MATCHIT}*.xml | fgrep -v supplementalData`;
       do
 #      echo ${TREE} _ ${file}
       LDMLComparator -d ./${TREE} -vetting -common:gold ../common/${TREE}/${file}
@@ -52,6 +87,8 @@ fi
 mkdir ../vetting
 cd ../vetting
 echo "INFO: Starting in ../vetting"
+
+backup_tree main
 compare_tree main
 write_index main
 echo "INFO: Done with ../vetting"

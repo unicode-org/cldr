aa83a2f7b3bb556ea2e323a613723b97bfaa5345
Steven R. Loomis <srl@icu-project.org>
2004-10-19 00:22:29 +0000

    CLDR-247 clean it up a bit
    X-SVN-Rev: 537

--- tools/scripts/cldrres.mk
+++ tools/scripts/cldrres.mk
@@ -16,22 +16,25 @@
 ## now just type 'make' and you should be set.
 ##
 
-# GENRB_ALIAS_SOURCE = \
-# GENRB_SOURCE = 
-L=../../../locale
-#-include locales/cldrlist.mk
+## Root of the CLDR directory (contains common, icu, ...)
+CLDR_ROOT=../../../locale
 
-# if you want draft locales - uncomment this!  (hint: or, put it in reslocal.mk)
-#CONVDEP=-f
+## Arguments to the LDML2ICUConverter program
 
-# the tool. No invoke needed.
+## if you want draft locales - uncomment this (or, put it in reslocal.mk)
+#LDML_CONVERTER_OPTS+=-f
+
+## if you want verbose output - uncomment this (or, put it in reslocal.mk)
+#LDML_CONVERTER_OPTS+=-v
+
+## the tool. No $(INVOKE) needed.
 LDML_CONVERTER=LDML2ICUConverter
 
-# command line options to the ldml conversion tool. Shouldn't need to edit
-CONVOPTS+=     -s $(L)/common/main -d $(LOCSRCDIR) -p $(L)/icu/main $(CONVDEP)
-CONVOPTS_COL+= -s $(L)/common/collation -d $(COLSRCDIR) -p $(L)/icu/collation $(CONVDEP)
+## command line options to the ldml conversion tool. Shouldn't need to edit from here on down.
+LDML_OPTS_RES += -s $(CLDR_ROOT)/common/main -d $(LOCSRCDIR) -p $(CLDR_ROOT)/icu/main $(LDML_CONVERTER_OPTS)
+LDML_OPTS_COL += -s $(CLDR_ROOT)/common/collation -d $(COLSRCDIR) -p $(CLDR_ROOT)/icu/collation $(LDML_CONVERTER_OPTS)
 
-# some aliases
+## some aliases
 GENRB_ALIAS_PATHS=$(GENRB_ALIAS_SOURCE:%.txt=$(LOCSRCDIR)/%.txt)
 GENRB_SYNTHETIC_PATHS=$(GENRB_SYNTHETIC_ALIAS:%.txt=$(LOCSRCDIR)/%.txt)
 COLLATION_ALIAS_PATHS=$(COLLATION_ALIAS_SOURCE:%.txt=$(COLSRCDIR)/%.txt)
@@ -39,49 +42,53 @@ COLLATION_SYNTHETIC_PATHS=$(COLLATION_SYNTHETIC_ALIAS:%.txt=$(COLSRCDIR)/%.txt)
 GENRB_PATHS=$(GENRB_SOURCE:%.txt=$(LOCSRCDIR)/%.txt)
 COLLATION_PATHS=$(COLLATION_SOURCE:%.txt=$(COLSRCDIR)/%.txt)
 
-# so make doesn't takes it from us (our carefully built .txt files..)
+## so make doesn't takes it from us (our carefully built .txt files..)
 .PRECIOUS: $(GENRB_PATHS) $(LOCSRCDIR)/root.txt $(GENRB_ALIAS_PATHS) $(COLLATION_PATHS) $(COLSRCDIR)/root.txt $(COLLATION_ALIAS_PATHS)
 
-#locales/cldrlist.mk:
-#	echo -n 'GENRB_SOURCE=' > $@
-#	( cd $L/common/main ; ls *.xml | grep -v root | fgrep -v supplemental | sed -e s%xml%txt% | tr '\012' ' ' ) >> $@
+## Rebuild ICU resource .txt from XML source
+$(LOCSRCDIR)/%.txt: $(CLDR_ROOT)/common/main/%.xml
+	$(LDML_CONVERTER) $(LDML_OPTS_RES) $(<F) || ($(RMV) $@;false)
 
-$(LOCSRCDIR)/%.txt: $(L)/common/main/%.xml
-	$(LDML_CONVERTER) $(CONVOPTS) $(<F) || ($(RMV) $@;false)
+## Rebuild ICU collation .txt from XML source
+$(COLSRCDIR)/%.txt: $(CLDR_ROOT)/common/collation/%.xml
+	$(LDML_CONVERTER) $(LDML_OPTS_COL) $(<F) || ($(RMV) $@;false)
 
-$(COLSRCDIR)/%.txt: $(L)/common/collation/%.xml
-	$(LDML_CONVERTER) $(CONVOPTS_COL) $(<F) || ($(RMV) $@;false)
-
-# use the following two to clean up just the lists, or the whole deal.
+## Special target for cleaning up the resource lists
 cldr-clean-lists:
 	-$(RMV) coll/colfiles.mk locales/resfiles.mk
 
+## Special target for cleaning up ALL generated files
 cldr-clean-old: cldr-clean-lists
 	-$(RMV) $(LOCSRCDIR)/*.txt $(COLSRCDIR)/*.txt
 
+## Special target for building the resource lists
 cldr-lists:  coll/colfiles.mk locales/resfiles.mk
 
-# the following two are S-L-O-W.  
-# Don't run them if you're just cleaning up!
 
+## Alias generation. These are slow and shouldn't be rebuilt if  'make clean' is being run. 
+
+## These don't work right, yet.
 #ifneq ($(patsubst %clean,,$(MAKECMDGOALS)),)
 #ifneq ($(patsubst %cldr-clean-old,,$(MAKECMDGOALS)),)
 #ifneq ($(patsubst %cldr-clean-lists,,$(MAKECMDGOALS)),)
 
-# $(L)/common/main $(L)/icu/main
-$(GENRB_SYNTHETIC_PATHS) $(LOCSRCDIR)/resfiles.mk: $(L)/icu/deprecatedList.xml 
-	$(LDML_CONVERTER) $(CONVOPTS) -w $(L)/common/main  || ($(RMV) $@;false)
+$(GENRB_SYNTHETIC_PATHS) $(LOCSRCDIR)/resfiles.mk: $(CLDR_ROOT)/icu/deprecatedList.xml 
+	$(LDML_CONVERTER) $(LDML_OPTS_RES) -w $(CLDR_ROOT)/common/main  || ($(RMV) $@;false)
 
-#$(L)/common/collation $(L)/icu/collation
-$(COLLATION_SYNTHETIC_PATHS) $(COLSRCDIR)/colfiles.mk: $(L)/icu/deprecatedList.xml 
-	$(LDML_CONVERTER) $(CONVOPTS_COL) -w $(L)/common/collation || ($(RMV) $@;false)
+$(COLLATION_SYNTHETIC_PATHS) $(COLSRCDIR)/colfiles.mk: $(CLDR_ROOT)/icu/deprecatedList.xml 
+	$(LDML_CONVERTER) $(LDML_OPTS_COL) -w $(CLDR_ROOT)/common/collation || ($(RMV) $@;false)
 #endif
 #endif
 #endif
 
+## Special target for building a tarball of the locale data
 locales.tgz:
-	tar cvfpz $@ locales/resfiles.mk locales/*.txt coll/colfiles.mk coll/*.txt
+	tar cvf - locales/resfiles.mk locales/*.txt coll/colfiles.mk coll/*.txt | gzip > $@
 
+## Special target for building the resource .txts
 cldr-locale-txt: $(GENRB_PATHS)
 
+## Special target for building the collation .txt
 cldr-collation-txt: $(COLLATION_PATHS)
+
+cldr-all-files: cldr-lists cldr-locale-txt cldr-collation-txt

c8050ca72923ecc1198ba852c2b7eab76a368811
Mark Davis <mark@macchiato.com>
2004-12-08 08:46:02 +0000

    CLDR-146 testing mockup
    X-SVN-Rev: 672

--- tools/java/org/unicode/cldr/test/FlexibleDateTime.java
+++ tools/java/org/unicode/cldr/test/FlexibleDateTime.java
@@ -12,6 +12,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -26,31 +28,47 @@ import com.ibm.icu.text.SimpleDateFormat;
  * Test class for trying different approaches to flexible date/time
  */
 public class FlexibleDateTime {
-	static String VARIABLES = "GyYuMwWdDFgEeahHKkmsSAzZ";
+	static final boolean SHOW = false;
 	List rules = new ArrayList();
-	Map currentVariables = new HashMap();
+	Map currentVariables = new LinkedHashMap();
 	
+	private static class Variable {
+		String variable;
+		Variable(String variable) {
+			this.variable = variable;
+		}
+		public String toString() {
+			return "{" + variable + "}";
+		}
+	}
+
 	private class Rule {
-		List matchSet = new ArrayList();
 		String name;
-		String format;
+		List matchSet = new ArrayList();
+		List format = new ArrayList();
 		//boolean hasVariables;
 		
 		public String toString() {
-			return name + " <= " + format;
+			return name + " <= {" + matchSet + "} " + format;
 		}
 		
 		private Rule(String rule) {
 			int eqPos = rule.indexOf('=');
 			name = rule.substring(0, eqPos).trim();
-			rule = format = rule.substring(eqPos+1);
+			rule = rule.substring(eqPos+1);
 			boolean inVariable = false;
 			int startVariable = -1;
+			String formatPiece = "";
 			for (int i = 0; i < rule.length(); ++i) {
 				char ch = rule.charAt(i); // ok to not use UTF16, since only care about {...}
 				if (startVariable >= 0) {
 					if (ch == '}') {
 						String variable = rule.substring(startVariable, i);
+						if (variable.startsWith("*")) {
+							variable = variable.substring(1);
+						} else {
+							format.add(new Variable(variable));
+						}
 						matchSet.add(variable);
 						//if (!hasVariables && isVariable(variable)) hasVariables = true;
 						startVariable = -1;
@@ -58,19 +76,16 @@ public class FlexibleDateTime {
 				} else {
 					if (ch == '{') {
 						startVariable = i+1;
+						if (formatPiece.length() != 0) {
+							format.add(formatPiece);
+							formatPiece = "";
+						}						
+					} else {
+						formatPiece += ch;
 					}
 				}
 			}
-		}
-		
-		private boolean isVariable (String s) {
-			if (s.length() == 0) return false;
-			char c = s.charAt(0);
-			if (VARIABLES.indexOf(c) < 0) return false;
-			for (int i = 1; i < s.length(); ++i) {
-				if (c != s.charAt(i)) return false;
-			}
-			return true;
+			if (formatPiece.length() != 0) format.add(formatPiece);
 		}
 		
 		private boolean process() {
@@ -83,6 +98,22 @@ public class FlexibleDateTime {
 				if (part == null) return false;
 				foundSoFar.put(matchItem, part); // e.g. h* -> hh
 			}
+			// special check. We don't replace a name unless it is either found in the parts, or empty.
+			if (foundSoFar.get(name) == null && currentVariables.get(name) != null) {
+				return false;
+			}
+			// special check. We don't match date/time unless all of it matches
+			if (name.equals("datetime")) {
+				if (foundSoFar.size() != currentVariables.size()) return false;
+			}
+			
+			if (SHOW) {
+				LinkedHashSet s = new LinkedHashSet(currentVariables.keySet());
+				LinkedHashSet f = new LinkedHashSet(foundSoFar.values());
+				s.removeAll(f);
+				System.out.println("Matched " + this + " \tWith: " + f + " \tRemainder: " + s);
+			}
+			
 			// we have a complete match. So substitute in the format, remove from pieces, and set the variable
 			String format2 = replace(format, foundSoFar);
 			// remove all (should be on Map!)
@@ -95,17 +126,24 @@ public class FlexibleDateTime {
 		
 		/**
 		 * Dumb implementation for now.
-		 * @param format2
+		 * @param formatList
 		 * @param foundSoFar
 		 * @return
 		 */
-		private String replace(String format2, Map foundSoFar) {
-			for (Iterator it = foundSoFar.keySet().iterator(); it.hasNext();) {
-				String key = (String) it.next();
-				String value = (String) foundSoFar.get(key);
-				format2 = replace(format2, '{' + key + '}', (String)currentVariables.get(value));
+		private String replace(List formatList, Map foundSoFar) {
+			StringBuffer result = new StringBuffer();
+			for (Iterator it = formatList.iterator(); it.hasNext();) {
+				Object part = it.next();
+				if (part instanceof String) {
+					result.append(part);
+				} else {
+					String key = ((Variable)part).variable;
+					String value = (String) foundSoFar.get(key);
+					String variable = (String)currentVariables.get(value);
+					result.append(variable);
+				}
 			}
-			return format2;
+			return result.toString();
 		}
 
 		/**
@@ -113,6 +151,7 @@ public class FlexibleDateTime {
 		 * @param key
 		 * @param value
 		 */
+		/*
 		private String replace(String format2, String key, String value) {
 			int pos = 0;
 			while (true) {
@@ -122,6 +161,7 @@ public class FlexibleDateTime {
 				pos += value.length();
 			}
 		}
+		*/
 
 		private String matches(String matcher) {
 			Matcher m = Pattern.compile(matcher).matcher("");
@@ -135,10 +175,21 @@ public class FlexibleDateTime {
 	}
 	private FlexibleDateTime() {}
 	
+	static String[] FALLBACK_RULES ={
+			"date={G+|y+|Y+|u+|M+|w+|W+|d+|D+|F+|g+|E+}",
+			"date={date} {G+|y+|Y+|u+|M+|w+|W+|d+|D+|F+|g+|E+}",
+			"time={a+|H+|m+|s+|S+|A+|z+|Z+}",
+			"time={time} {a+|H+|m+|s+|S+|A+|z+|Z+}",
+			"datetime={time|date}",
+	};
+	
 	static FlexibleDateTime make (String[] data) {
 		FlexibleDateTime result = new FlexibleDateTime();
 		for (int i = 0; i < data.length; ++i) {
 			result.addRule(data[i]);
+		}
+		for (int i = 0; i < FALLBACK_RULES.length; ++i) {
+			result.addRule(FALLBACK_RULES[i]);
 		}
 		return result;
 	}
@@ -176,35 +227,38 @@ k 1..2 24 Hour [1-24].
 	
 	public static void main(String[] args) {
 		String[] data = {
-				"date={d+}-{MM}-{y+}",
+				"yyyy={y+}",
+				"time={s+}.{S+}",
+				"time={m+}:{time}",
+				"time={*H+}hh:{time} a",
+				"date={*d+}{*y+}dd-{MM}-yyyy",
 				"date={d+}. {MMM}",
 				"date={d+} {M+}",
-				"date={E+} {date}",
-				"date={y+} {date}",
+				"date={d+} [{H+}]",
+				"date={date} {M+}",
+				"date={date} {y+}",
 				"date={date} {G+}",
-				"min={mm}:{ss}",
-				"min={mm}",
-				"time={hh}:{min}",
-				"date={dd}[{hh}]",
+				"date={E+} {date}",
 				"datetime={date}, {time}",			
-				"datetime={time}",			
-				"datetime={date}",			
-				// default is to pull off each item in order and follow with space.
 		};
 		
 		String[] testData = {
-				"hhmm",
-				"MMddd",
-				"ddhh",
+				"dMMy",
+				"HHmm",
+				"yyyyHHmm",
+				"MMdd",
+				"ddHH",
 				"yyyyMMMd",
-				"yyyyMMddhhmmss",
-				"GEEEEyyyyMMddhhmmss",
+				"yyyyMMddHHmmss",
+				"GEEEEyyyyMMddHHmmss",
+				"GyyyyYYYYuuuuMMMMwwWddDDDFgEEEEaHHmmssSSSAAAAAAAzZ",
 		};
 		FlexibleDateTime fdt = FlexibleDateTime.make(data);
 		Date now = new Date(99,11,23,1,2,3);
 		for (int i = 0; i < testData.length; ++i) {
-			String dfpattern = fdt.getDateFormatPattern(testData[i]);;
-			System.out.println("Raw pattern: " + testData[i] + "\t=>\tLocalized Pattern: " + dfpattern);
+			System.out.println("Raw pattern: " + testData[i]);
+			String dfpattern = fdt.getDateFormatPattern(testData[i]);
+			System.out.println("\t=>\tLocalized Pattern: " + dfpattern);
 			DateFormat df = new SimpleDateFormat(dfpattern);
 			System.out.println("\tSample Input: " + now + "\t=>\tSample Results: " + df.format(now));
 	}
@@ -218,32 +272,18 @@ k 1..2 24 Hour [1-24].
 		String result;
 		main:
 	    while (true) {
+	    	// start from the beginning whenever we get a match
 			for (int i = 0; i < rules.size(); ++i) {
 				Rule r = (Rule) rules.get(i);
 				if (r.process()) {
 					continue main; // if we get a match, start over
 				}
 			}
-			break;
-			/*
-			if (currentVariables.size() > 1) {
-				if (currentVariables.size() > 1) {
-					throw new IllegalArgumentException(); // rule failure
-				}
-				continue;
-			}
-			// if we wound up here, then there are remaining pieces, but we can't match them.
-			// pull one off and add it with intervening space
-			String part = (String) pieces.get(0);
-			pieces.remove(0);
-			String dateValue = (String) currentVariables.get("{date}");
-			if (dateValue == null) {
-				currentVariables.put("{date}", part);
-			} else {
-				currentVariables.put("{date}", dateValue + ' ' + part);
-			}
-			*/
+			break; // stop when we hit no matchs on the way through
 	    }
+		// we made it all the way through. There ought to be nothing but a datetime left.
+		if (currentVariables.size() != 1) throw new IllegalArgumentException("Failed generation");
+		
 		return (String) currentVariables.get("datetime");
 	}
 	
@@ -255,6 +295,7 @@ k 1..2 24 Hour [1-24].
 	 */
 	// TODO put in order, check for duplicates
 	private void getParts(String string) {
+		String[] weights = new String[VARIABLES.size()];
 		currentVariables.clear();
 		if (string.length() == 0) return;
 		int start = 1;
@@ -263,15 +304,68 @@ k 1..2 24 Hour [1-24].
 		for (int i = 1; i < string.length(); ++i) {
 			char ch = string.charAt(i);
 			if (ch == last) continue;
-			String part = string.substring(lastPos, i);
-			addVariable(part, part);
+			VARIABLES.getPart(string.substring(lastPos, i), weights);
 			lastPos = i;
 			last = ch;			
 		}
-		String part = string.substring(lastPos, string.length());
-		addVariable(part, part);
+		VARIABLES.getPart(string.substring(lastPos, string.length()), weights);
+		// add to the list in REVERSE sorted order. Will unreverse later
+		for (int i = weights.length - 1; i >= 0; --i) {
+			if (weights[i] == null) continue;
+			addVariable(weights[i], weights[i]);
+		}
 	}
 	
+	static class VariableList {
+		String items = "";
+		int[] maxlengths = new int[50];
+		VariableList add(char c, int maxLen) {
+			maxlengths[items.length()] = maxLen;
+			items += c;
+			return this;
+		}
+		/**
+		 * @param part
+		 * @return
+		 */
+		public void getPart(String part, String[] weights) {
+			int weight = find(part.charAt(0), part.length());
+			if (weight < 0) throw new IllegalArgumentException("Illegal Field: " + part);
+			weights[weight] = part;
+		}
+		int find(char c, int len) {
+			int weight = items.indexOf(c);
+			if (weight < 0) return -1;
+			if (len < 1 || len > maxlengths[weight]) return -1;
+			return weight;
+		}
+		int size() {
+			return items.length();
+		}
+	}
+	
+	static VariableList VARIABLES = new VariableList()
+		.add('G', 1)
+		.add('y', 5)
+		.add('Y', 5)
+		.add('u', 5)
+		.add('M', 5)
+		.add('w', 2)
+		.add('W', 1)
+		.add('d', 2)
+		.add('D', 3)
+		.add('F', 1)
+		.add('g', Integer.MAX_VALUE)
+		.add('E', 5)
+		.add('a', 1)
+		.add('H', 2)
+		.add('m', 2)
+		.add('s', 2)
+		.add('S', Integer.MAX_VALUE)
+		.add('A', Integer.MAX_VALUE)
+		.add('z', 4)
+		.add('Z', 2);
+
 	/**
 	 * @param format2
 	 */

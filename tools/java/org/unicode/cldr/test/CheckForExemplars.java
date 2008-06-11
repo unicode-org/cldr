ee75d56c4322b60ada1ce8eb79cd6e7f4651a133
Mark Davis <mark@macchiato.com>
2008-06-11 08:15:07 +0000

    CLDR-1822 fix zero and many values.
    X-SVN-Rev: 3503

--- tools/java/org/unicode/cldr/test/CheckForExemplars.java
+++ tools/java/org/unicode/cldr/test/CheckForExemplars.java
@@ -32,8 +32,8 @@ public class CheckForExemplars extends CheckCLDR {
     "/localizedPatternChars", "/segmentations", "/dateFormatItem", "/references", "/unitPattern",
     "/intervalFormatItem",
     "/localeDisplayNames/variants/"
-    };
-  
+  };
+
   private UnicodeSet exemplars;
   private UnicodeSet scriptRegionExemplars;
   private UnicodeSet scriptRegionExemplarsWithParens;
@@ -56,17 +56,20 @@ public class CheckForExemplars extends CheckCLDR {
           "|fallbackFormat" +
           "|unitPattern" +
           "|localePattern" +
-      ")");
+  ")");
   private Matcher supposedToBeMessageFormat = SUPPOSED_TO_BE_MESSAGE_FORMAT_PATTERN.matcher("");
-  
+
   static final Pattern LEAD_OR_TRAIL_WHITESPACE_OK = Pattern.compile("/(" +
           "localeSeparator" +
           "|references/reference" +
           "|insertBetween" +
-      ")");
+  ")");
   private Matcher leadOrTrailWhitespaceOk = LEAD_OR_TRAIL_WHITESPACE_OK.matcher("");
 
-  
+  static final Pattern IS_COUNT_ZERO_ONE_TWO = Pattern.compile("/units.*\\[@count=\"(zero|one|two)\"");
+  private Matcher isCountZeroOneTwo = IS_COUNT_ZERO_ONE_TWO.matcher("");
+  private boolean hasSpecialPlurals;
+
   public CheckCLDR setCldrFileToCheck(CLDRFile cldrFile, Map<String, String> options, List<CheckStatus> possibleErrors) {
     if (cldrFile == null) return this;
     skip = true;
@@ -75,16 +78,17 @@ public class CheckForExemplars extends CheckCLDR {
       return this;
     }
     String locale = cldrFile.getLocaleID();
+    hasSpecialPlurals = locale.equals("ar") || locale.startsWith("ar_");
     informationMessage = "<a href='http://unicode.org/cldr/apps/survey?_=" + locale + "&x=characters'>characters</a>";
     col = Collator.getInstance(new ULocale(locale));
     spaceCol = Collator.getInstance(new ULocale(locale));
     spaceCol.setStrength(col.PRIMARY);
-    
+
     CLDRFile resolvedFile = cldrFile.getResolved();
     boolean[] ok = new boolean[1];
     exemplars = safeGetExemplars("", possibleErrors, resolvedFile, ok);
     if (!ok[0]) exemplars = new UnicodeSet();
-    
+
     if (exemplars == null) {
       CheckStatus item = new CheckStatus().setCause(this).setMainType(CheckStatus.errorType).setSubtype(Subtype.noExemplarCharacters)
       .setMessage("No Exemplar Characters: {0}", new Object[]{this.getClass().getName()});
@@ -96,10 +100,10 @@ public class CheckForExemplars extends CheckCLDR {
     UnicodeSet auxiliary = safeGetExemplars("auxiliary", possibleErrors, resolvedFile, ok); // resolvedFile.getExemplarSet("auxiliary", CLDRFile.WinningChoice.WINNING);
     if (auxiliary != null) exemplars.addAll(auxiliary);
     exemplars.addAll(CheckExemplars.AlwaysOK).freeze();
-    
+
     scriptRegionExemplars = (UnicodeSet) new UnicodeSet(exemplars).removeAll("();,").freeze();
     scriptRegionExemplarsWithParens = (UnicodeSet) new UnicodeSet(exemplars).removeAll(";,").freeze();
-    
+
     currencySymbolExemplars = safeGetExemplars("currencySymbol", possibleErrors, resolvedFile, ok); // resolvedFile.getExemplarSet("currencySymbol", CLDRFile.WinningChoice.WINNING);
     if (currencySymbolExemplars == null) {
       currencySymbolExemplars = new UnicodeSet(exemplars);
@@ -113,19 +117,19 @@ public class CheckForExemplars extends CheckCLDR {
   private UnicodeSet safeGetExemplars(String type, List possibleErrors, CLDRFile resolvedFile, boolean[] ok) {
     UnicodeSet result = null;
     try {
-        result = resolvedFile.getExemplarSet(type, CLDRFile.WinningChoice.WINNING);
-        ok[0] = true;
+      result = resolvedFile.getExemplarSet(type, CLDRFile.WinningChoice.WINNING);
+      ok[0] = true;
     } catch(IllegalArgumentException iae) {
       possibleErrors.add(new CheckStatus()
       .setCause(this).setMainType(CheckStatus.errorType).setSubtype(Subtype.couldNotAccessExemplars)
-          .setMessage("Could not get exemplar set: " + iae.toString()));
+      .setMessage("Could not get exemplar set: " + iae.toString()));
       ok[0] = false;
     }
     return result;
   }
-  
+
   public CheckCLDR handleCheck(String path, String fullPath, String value,
-      Map<String, String> options, List<CheckStatus> result) {
+          Map<String, String> options, List<CheckStatus> result) {
     if (fullPath == null) return this; // skip paths that we don't have
     if (value == null) return this; // skip values that we don't have ?
     if (skip) return this;
@@ -135,12 +139,12 @@ public class CheckForExemplars extends CheckCLDR {
       throw new InternalCldrException("no file to check!");
     }
     String sourceLocale = getResolvedCldrFileToCheck().getSourceLocaleID(path, otherPathStatus);
-    
+
     // if we are an alias to another path, then skip
     if (!path.equals(otherPathStatus.pathWhereFound)) {
       return this;
     }
-    
+
     // now check locale source
     if (XMLSource.CODE_FALLBACK_ID.equals(sourceLocale)) {
       return this;
@@ -149,17 +153,20 @@ public class CheckForExemplars extends CheckCLDR {
       if (true) return this;
       if (path.indexOf("/calendar") >= 0 && path.indexOf("gregorian") <= 0) return this;
     }
-    
+
     // add checks for patterns. Make sure that all and only the message format patterns have {n}
     boolean hasMessageFormatFields = patternMatcher.reset(value).find();
-    boolean supposedToHaveMessageFormatFields = supposedToBeMessageFormat.reset(path).find();
+    boolean supposedToHaveMessageFormatFields = 
+      supposedToBeMessageFormat.reset(path).find()
+      && !(hasSpecialPlurals 
+              && isCountZeroOneTwo.reset(path).find());
     if (hasMessageFormatFields != supposedToHaveMessageFormatFields) {
       result.add(new CheckStatus().setCause(this).setMainType(CheckStatus.errorType)
               .setSubtype(supposedToHaveMessageFormatFields ? Subtype.missingPlaceholders : Subtype.shouldntHavePlaceholders)
               .setMessage(supposedToHaveMessageFormatFields 
                       ? "This field is a message pattern, and should have '{0}, {1},' etc. See the English for an example."
-                      : "This field is not a message pattern, and should not have '{0}, {1},' etc. See the English for an example.",
-                      new Object[]{}));
+                              : "This field is not a message pattern, and should not have '{0}, {1},' etc. See the English for an example.",
+                              new Object[]{}));
 
     }
     if (supposedToHaveMessageFormatFields) {
@@ -171,15 +178,15 @@ public class CheckForExemplars extends CheckCLDR {
         if (path.indexOf(EXEMPLAR_SKIPS[i]) > 0 ) return this; // skip some items.
       }
     }
-    
+
     if (path.startsWith("//ldml/posix/messages")) return this;
-    
+
     if (path.contains("/currency") && path.endsWith("/symbol")) {
       if (!currencySymbolExemplars.containsAll(value)) {
         UnicodeSet missing = new UnicodeSet().addAll(value).removeAll(currencySymbolExemplars);
         String fixedMissing = CollectionUtilities.prettyPrint(missing, true, null, null, col, col);
         result.add(new CheckStatus().setCause(this).setMainType(CheckStatus.warningType).setSubtype(Subtype.charactersNotInCurrencyExemplars)
-        .setMessage("The characters \u200E{0}\u200E are not used in currency symbols in this language, according to " + informationMessage + ".", new Object[]{fixedMissing}));
+                .setMessage("The characters \u200E{0}\u200E are not used in currency symbols in this language, according to " + informationMessage + ".", new Object[]{fixedMissing}));
       }
     } else if (!exemplars.containsAll(value)) {
       UnicodeSet missing = new UnicodeSet().addAll(value).removeAll(exemplars);

bb9bb06fb27ee6b17728e16034a044cdf47009b4
Mark Davis <mark@macchiato.com>
2009-01-25 09:43:59 +0000

    CLDR-0 misc
    X-SVN-Rev: 3772

--- tools/java/org/unicode/cldr/draft/FrequencyData.java
+++ tools/java/org/unicode/cldr/draft/FrequencyData.java
@@ -2,7 +2,9 @@ package org.unicode.cldr.draft;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
 
 import org.unicode.cldr.util.Counter;
 import org.unicode.cldr.util.UnicodeMap;
@@ -10,15 +12,21 @@ import org.unicode.cldr.util.UnicodeMap;
 import com.ibm.icu.lang.UCharacter;
 import com.ibm.icu.lang.UProperty;
 import com.ibm.icu.text.Normalizer;
+import com.ibm.icu.text.NumberFormat;
 import com.ibm.icu.text.UTF16;
 import com.ibm.icu.text.UnicodeSet;
 import com.ibm.icu.text.UnicodeSetIterator;
+import com.ibm.icu.text.Normalizer.Mode;
 
 public class FrequencyData {
 
-  public static long loadFrequencies(String frequencyFile, UnicodeMap result) throws IOException {
+  private static final UnicodeSet NO_SCRIPT = new UnicodeSet("[[:script=common:][:script=inherited:][:script=unknown:]]");
+  private static final UnicodeSet PRIVATE_USE = new UnicodeSet("[:Co:]");
+  private UnicodeMap frequencies = new UnicodeMap();
+  private int total;
+
+  public FrequencyData(String frequencyFile) throws IOException {
     BufferedReader in = GenerateNormalizeForMatch.openUTF8Reader(frequencyFile);
-    long total = 0;
     while (true) {
       String line = in.readLine();
       if (line == null) break;
@@ -32,82 +40,35 @@ public class FrequencyData {
       String code = GenerateNormalizeForMatch.fromHex(pieces[0]);
       long count = Long.parseLong(pieces[1]);
       total += count;
-      result.put(code, count);
+      frequencies.put(code, count);
     }
     in.close();
-    result.freeze();
-    return total;
+    frequencies.freeze();
   }
-  
+
+
+  public long getCount(int codepoint) {
+    Long result = (Long) frequencies.getValue(codepoint);
+    return result == null ? 0 : result;
+  }
+
   static final double[] standardDeviation = {
-          0,
-          68.27d/100,
-          95.450d/100,
-          99.7300d/100,
-          99.993666d/100,
-          99.99994267d/100,
-          99.9999998027d/100,
-          99.9999999997440d/100
+    0d,
+    0.682689492137d, // 1
+    0.954499736104d, // 2
+    0.997300203937d, // 3
+    0.999936657516d, // 4
+    0.999999426697d, // 5
+    0.999999998027d, // 6
+    0.999999999997440d // 7
   };
+  private static final UnicodeSet nonNFKC = new UnicodeSet("[:nfkcqc=n:]");
   
-  /*
-  1σ  68.27%
-  1.645σ  90%
-  1.960σ  95%
-  2σ  95.450%
-  2.576σ  99%
-  3σ  99.7300%
-  3.2906σ   99.9%
-  4σ  99.993666%
-  5σ  99.99994267%
-  6σ  99.9999998027%
-  7σ  99.9999999997440%
-  */
-
-  public static void main(String[] args) throws IOException {
-    String frequencyFile = args[0];
-    UnicodeMap frequencies = new UnicodeMap();
-    long count = loadFrequencies(frequencyFile, frequencies);
-    Counter<Integer> counter = new Counter();
-    
-    int propEnum = UCharacter.getPropertyEnum("script");
-    UnicodeSet valueChars = new UnicodeSet();
-    
-    for (int i = UCharacter.getIntPropertyMinValue(propEnum); i <= UCharacter.getIntPropertyMaxValue(propEnum); ++i) {
-      String valueAlias = UCharacter.getPropertyValueName(propEnum, i, UProperty.NameChoice.LONG);
-      if (valueAlias.equalsIgnoreCase("common") || valueAlias.equalsIgnoreCase("inherited")) continue;
-      System.out.println("@" + valueAlias);
-      valueChars.clear();
-      valueChars.applyPropertyAlias("script", valueAlias);
-      if (valueChars.size() == 0) continue;
-      
-      counter.clear();
-      for (UnicodeSetIterator it = new UnicodeSetIterator(valueChars); it.next();) {
-        final Long frequency = (Long) frequencies.getValue(it.codepoint);
-        String norm = Normalizer.normalize(it.codepoint, Normalizer.COMPOSE);
-        norm = UCharacter.foldCase(norm, true);
-        norm = Normalizer.normalize(norm, Normalizer.COMPOSE);
-        int cp;
-        for (int j = 0; j < norm.length(); j += UTF16.getCharCount(cp)) {
-          cp = UTF16.charAt(norm, j);
-          counter.add(cp, frequency == null ? 0 : frequency);
-        }
-      }
-      double total = counter.getTotal();
-      double totalFrequency = 0;
-      
-      for (int cp : counter.getKeysetSortedByCount(false)) {
-        totalFrequency += counter.getCount(cp)/total;
-        final int sd = sd(totalFrequency);
-        if (sd == standardDeviation.length) break;
-        boolean isNFKC = Normalizer.isNormalized(cp, Normalizer.COMPOSE_COMPAT, 0);
-        System.out.println(new StringBuilder().appendCodePoint(cp) + "\t" + (totalFrequency*100) + "%\t" + sd + "\t" + (isNFKC ? "" : "K"));
-      }
-      System.out.println();
-    }
+  public static double getStandardDeviationLimit(int i) {
+    return standardDeviation[i];
   }
 
-  private static int sd(double totalFrequency) {
+  public static int standardDeviationInterval(double totalFrequency) {
     for (int i = standardDeviation.length - 1; i > 0; --i) {
       if (totalFrequency > standardDeviation[i]) {
         return i + 1;
@@ -115,4 +76,160 @@ public class FrequencyData {
     }
     return 1;
   }
+
+  private static class Rank {
+    int rank;
+    double frequency;
+    double cummulative;
+  }
+
+  public class RelativeFrequency {
+    private int[] rank2codepoint;
+    private Map<Integer,Rank> rankInfo = new HashMap<Integer,Rank>();
+    private double totalRelative;
+
+    public double getTotalRelative() {
+      return totalRelative;
+    }
+    private RelativeFrequency(UnicodeSet withinSet, Mode compose) {
+      Counter<Integer> counter = new Counter<Integer>();
+      for (UnicodeSetIterator it = new UnicodeSetIterator(withinSet); it.next();) {
+        final long frequency = getCount(it.codepoint);
+        if (frequency == 0) continue;
+        if (compose == null) {
+          counter.add(it.codepoint, frequency);
+        } else {
+          String norm = Normalizer.normalize(it.codepoint, compose);
+          norm = UCharacter.foldCase(norm, true);
+          norm = Normalizer.normalize(norm, compose);
+          int cp;
+          for (int j = 0; j < norm.length(); j += UTF16.getCharCount(cp)) {
+            cp = UTF16.charAt(norm, j);
+            counter.add(cp, frequency);
+          }
+        }
+      }
+      rank2codepoint = new int[counter.getItemCount()];
+      totalRelative = counter.getTotal();
+      double totalFrequency = 0;
+      int itemRank = 0;
+      for (int cp : counter.getKeysetSortedByCount(false)) {
+        Rank rank2 = new Rank();
+        rank2codepoint[itemRank] = cp;
+        rank2.rank = itemRank++;
+        final long frequency = counter.getCount(cp);
+        rank2.frequency = frequency/totalRelative;
+        totalFrequency += frequency;
+        rank2.cummulative = totalFrequency/totalRelative;
+        rankInfo.put(cp, rank2);
+      }
+    }
+    public long getRankCount() {
+      return rank2codepoint.length;
+    }
+    public long getRank(int codepoint) {
+      return rankInfo.get(codepoint).rank;
+    }
+    public double getFrequency(int codepoint) {
+      final Rank rank = rankInfo.get(codepoint);
+      return rank == null ? 0d : rank.frequency;
+    }
+    public double getCumulative(int codepoint) {
+      final Rank rank = rankInfo.get(codepoint);
+      return rank == null ? 0d : rank.cummulative;
+    }
+    public int getCodePointAtRank(int rankLevel) {
+      return rank2codepoint[rankLevel];
+    }
+  }
+
+  private RelativeFrequency getRelativeFrequency(UnicodeSet withinSet, Mode compose) {
+    return new RelativeFrequency(withinSet, compose);
+  }
+  
+  static     NumberFormat nf = NumberFormat.getInstance();
+  static {
+    nf.setGroupingUsed(true);
+  }
+
+  public static void main(String[] args) throws IOException {
+    String frequencyFile = args[0];
+
+    FrequencyData data = new FrequencyData(frequencyFile);
+    
+
+    System.out.print("Script" + "\t");
+    System.out.print(0.0d + "\t");
+    for (double item = 0.005; item < 1.0; item += item) {
+      System.out.print(item + "\t");
+    }
+    System.out.print(1.0d + "\t");
+    System.out.println("Total");
+
+    data.showData(UCharacter.getPropertyEnum("script"), NO_SCRIPT);
+    data.showData(UCharacter.getPropertyEnum("gc"), new UnicodeSet(NO_SCRIPT).complement());
+
+//    data.showData("Private Use", PRIVATE_USE);
+//    RelativeFrequency relative = data.getRelativeFrequency(new UnicodeSet("[:script=unknown:]"), Normalizer.NFKC);
+//    System.out.println(relative.getTotalRelative());
+//    for (int i = 0; i < 10; ++i) {
+//      int cp = relative.getCodePointAtRank(i);
+//      double totalFrequency = relative.getCumulative(cp);
+//      System.out.println(Integer.toHexString(cp) + "\t" + totalFrequency);
+//    }
+  }
+
+
+  private void showData(int propEnum, UnicodeSet exclusions) {
+    for (int i = UCharacter.getIntPropertyMinValue(propEnum); i <= UCharacter.getIntPropertyMaxValue(propEnum); ++i) {
+      String valueAlias = UCharacter.getPropertyValueName(propEnum, i, UProperty.NameChoice.LONG);
+      String shortValueAlias = UCharacter.getPropertyValueName(propEnum, i, UProperty.NameChoice.SHORT);
+      //if (valueAlias.equalsIgnoreCase("common") || valueAlias.equalsIgnoreCase("inherited")) continue;
+      UnicodeSet valueChars = new UnicodeSet();
+
+      valueChars.applyPropertyAlias(UCharacter.getPropertyName(propEnum, UProperty.NameChoice.SHORT), shortValueAlias);
+      valueChars.removeAll(exclusions);
+      if (valueChars.size() == 0) continue;
+      showData(shortValueAlias + " - " + valueAlias, valueChars);
+    }
+  }
+
+
+  private void showData(String title, UnicodeSet valueChars) {
+    RelativeFrequency relative;
+    relative = getRelativeFrequency(valueChars, Normalizer.NFKC);
+    UnicodeMap sds = new UnicodeMap();
+    for (int rank = 0; rank < relative.getRankCount(); ++rank) {
+      int cp = relative.getCodePointAtRank(rank);
+      double totalFrequency = relative.getCumulative(cp);
+      final int sd = standardDeviationInterval(totalFrequency);
+      sds.put(cp, sd);
+      if (sd == standardDeviation.length) break;
+      boolean isNFKC = Normalizer.isNormalized(cp, Normalizer.COMPOSE_COMPAT, 0);
+      //System.out.println(new StringBuilder().appendCodePoint(cp) + "\t" + (totalFrequency*100) + "%\t" + sd + "\t" + (isNFKC ? "" : "K"));
+    }
+    
+    double nfkcSize = new UnicodeSet(valueChars).removeAll(nonNFKC).size();
+
+    int total = 0;
+    UnicodeSet totalSet = new UnicodeSet();
+
+    System.out.print(title + ": " + nf.format(nfkcSize) + "\t");
+    System.out.print(0.0d + "\t");
+
+    for (double item = 0.005; item < 1.0; item += item) {
+      int intRank = (int) Math.round(item * nfkcSize);
+      if (intRank >= relative.getRankCount()) {
+        System.out.print(1.0 + "\t");
+        continue;
+      }
+      int cp = relative.getCodePointAtRank(intRank);
+      double totalFrequency = relative.getCumulative(cp);
+      System.out.print(totalFrequency + "\t");
+    }
+    System.out.print(1.0d + "\t");
+
+    System.out.println(relative.getTotalRelative());
+  }
+
 }

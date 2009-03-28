28d55a62b4db9a1772aaacba82b0c9dd46a4f7b0
Mark Davis <mark@macchiato.com>
2009-03-28 01:31:44 +0000

    CLDR-0 test idna context rules
    X-SVN-Rev: 3936

--- tools/java/org/unicode/cldr/draft/IdnaLabelTester.java
+++ tools/java/org/unicode/cldr/draft/IdnaLabelTester.java
@@ -9,23 +9,26 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.unicode.cldr.util.Utility.Transform;
-
 import com.ibm.icu.dev.test.util.VariableReplacer;
 import com.ibm.icu.text.Transliterator;
 import com.ibm.icu.text.UnicodeSet;
 
 public class IdnaLabelTester {
 
+  private static final boolean DEBUG = true;
+
   enum Result {none, next, fail};
 
   static class Rule {
-    Matcher before;
-    Matcher at;
-    Result result;
-    String title;
+    final Matcher before;
+    final Matcher at;
+    final Result result;
+    final String title;
+    final int lineNumber;
+    int length;
+    String label;
 
-    public Rule(String before, String at, String result, String title) {
+    public Rule(String before, String at, String result, String title, int lineNumber) {
       if (before != null) {
         before = before.trim();
       }
@@ -34,16 +37,33 @@ public class IdnaLabelTester {
       this.at = Pattern.compile(at.trim(), Pattern.COMMENTS).matcher("");
       this.result = Result.valueOf(result.toLowerCase().trim());
       this.title = title;
+      this.lineNumber = lineNumber;
     }
-    public Result match(String label, int position) {
+
+    public Result match(int position) {
+      if (before != null) {
+        //before.region(0, position);
+        before.reset(label.substring(0,position));
+        if (!before.matches()) {
+          return Result.none;
+        }
+      }
+      //at.region(position, length);
+      at.reset(label.substring(position,length));
+      if (!at.lookingAt()) {
+        return Result.none;
+      }
+
+      return result;
+    }
+
+    public void setLabel(String label) {
+      this.label = label;
       if (before != null) {
         before.reset(label);
-        before.region(0, position);
       }
       at.reset(label);
-      at.region(position, label.length());
-      if (at.lookingAt()) return result;
-      return Result.none;
+      length = label.length();
     }
   }
 
@@ -53,9 +73,9 @@ public class IdnaLabelTester {
 
     VariableReplacer variables = new VariableReplacer();
     BufferedReader in = openFile(file);
-    
+
     String title = "???";
-    for (int lineCount = 0; ; ++lineCount) {
+    for (int lineCount = 1; ; ++lineCount) {
       String line = in.readLine();
       if (line == null) break;
       int commentPos = line.indexOf("#");
@@ -64,37 +84,42 @@ public class IdnaLabelTester {
       }
       line = line.trim();
       if (line.length() == 0) continue;
-      
+
       // do title
-      
+
       if (line.startsWith("Title:")) {
         title = line.substring(6).trim();
         continue;
       }
-      
+
       // do variables
-      
+
       if (line.startsWith("$")) {
         int equals = line.indexOf("=");
         if (equals >= 0) {
           final String variable = line.substring(0,equals).trim();
-          UnicodeSet s = new UnicodeSet(line.substring(equals+1).trim()).complement().complement();
+          UnicodeSet s = new UnicodeSet(variables.replace(line.substring(equals+1).trim())).complement().complement();
+          if (DEBUG) {
+            System.out.println(variable + "\t=\t" + s.toPattern(false));
+          }
           variables.add(variable, s.toPattern(false));
           continue;
         }
       }
-      
+
       // do rules. This could be much more compact, but is broken out for debugging
-      
+
       String[] pieces = line.split("\\s*;\\s*");
-      System.out.println(Arrays.asList(pieces));
+      if (DEBUG) {
+        System.out.println(Arrays.asList(pieces));
+      }
       String before, at, result;
       switch (pieces.length) {
         case 2: before = null; at = variables.replace(pieces[0]); result= pieces[1]; break;
         case 3: before = variables.replace(pieces[0]); at = variables.replace(pieces[1]); result= pieces[2]; break;
         default: throw new IllegalArgumentException(line + " => " + Arrays.asList(pieces));
       }
-      Rule rule = new Rule(before, at, result, title);
+      Rule rule = new Rule(before, at, result, title, lineCount);
       rules.add(rule);
     }
     in.close();
@@ -108,45 +133,69 @@ public class IdnaLabelTester {
       throw new IllegalArgumentException("Bad file name: " + f.getCanonicalPath());
     }
   }
-  
+
   static class TestStatus {
     String title;
     int position;
-    public TestStatus(int position, String title) {
+    int ruleLine;
+
+    public TestStatus(int position, String title, int lineNumber) {
       this.position = position;
       this.title = title;
+      this.ruleLine = lineNumber;
     }
   }
-  
+
   /**
    * Test a label; null for success.
    * Later, return information.
    * @param label
    * @return
    */
-  
+
   public TestStatus test(String label) {
+    // initialize
+    for (Rule rule : rules) {
+      rule.setLabel(label);
+    }
     charLoop:
-    for (int i= 0; i < label.length(); ++i) {
-      for (Rule rule : rules) {
-        Result result = rule.match(label, i);
-        switch (result) {
-          case next: continue charLoop;
-          case fail: return new TestStatus(i, rule.title);
+      // note: it doesn't matter if we test in the middle of a supplemental character
+      for (int i= 0; i < label.length(); ++i) {
+        for (Rule rule : rules) {
+          Result result = rule.match(i);
+          switch (result) {
+            case next: 
+              if (DEBUG) {
+                rule.match(i);
+              }
+              continue charLoop;
+            case fail: 
+              if (DEBUG) {
+                rule.match(i);
+              }
+              return new TestStatus(i, rule.title, rule.lineNumber);
+            default:
+              if (DEBUG) {
+                rule.match(i);
+              }
+            break;
+          }
         }
       }
-    }
     return null; // success!
   }
-  
+
   public static void main(String[] args) throws IOException {
     String dir = "java/org/unicode/cldr/draft/";
     IdnaLabelTester tester = new IdnaLabelTester(dir + "idnaContextRules.txt");
     Transliterator unescaper = Transliterator.getInstance("hex-any");
+    Transliterator escaper = Transliterator.getInstance("[[:z:][:me:][:mn:][:di:]-[:cn:]] any-hex");
     BufferedReader in = openFile(dir + "idnaTestCases.txt");
     boolean expectedSuccess = true;
-    
-    for (int lineCount = 0; ; ++lineCount) {
+    int failures = 0;
+    int successes = 0;
+
+    for (int lineCount = 1; ; ++lineCount) {
       String line = in.readLine();
       if (line == null) break;
       int commentPos = line.indexOf("#");
@@ -154,10 +203,10 @@ public class IdnaLabelTester {
         line = line.substring(0,commentPos);
       }
       line = unescaper.transform(line);
-      
+
       line = line.trim();
       if (line.length() == 0) continue;
-      
+
       if ("valid".equalsIgnoreCase(line)) {
         expectedSuccess = true;
         continue;
@@ -167,20 +216,36 @@ public class IdnaLabelTester {
       }
 
       TestStatus result = tester.test(line);
+      System.out.print(lineCount + ". \t");
       if (result == null) {
         if (expectedSuccess) {
-          System.out.println("Success - expected Valid, got it:\t" + line);
+          if (DEBUG) {
+            System.out.print("Success - expected and got Valid:\t");
+          }
+          successes++;
         } else {
-          System.out.println("FAILURE - expected Invalid, was valid:\t" + line);
+          System.out.print("FAILURE - expected Invalid, was valid:\t");
+          failures++;
         }
+        System.out.println(escaper.transform(line));
       } else {
         if (expectedSuccess) {
-          System.out.println("FAILURE - expected Valid, was invalid:\t" + line);
+          System.out.print("FAILURE - expected Valid, was invalid:\t");
+          failures++;
         } else {
-          System.out.println("Success - expected Invalid, got it:\t" + line.substring(0, result.position) + "$" + line.substring(result.position) + "\t\t" + result.title);
+          if (DEBUG) {
+            System.out.print("Success - expected and got Invalid:\t");
+          }
+          successes++;
         }
+        System.out.println(escaper.transform(line.substring(0, result.position)) 
+                + "\u2639" + escaper.transform(line.substring(result.position)) 
+                + "\t\t" + result.title
+                + "; \tRuleLine: " + result.ruleLine);
       }
     }
+    System.out.println("Successes:\t" + successes);
+    System.out.println("Failures:\t" + failures);
     in.close();
   }
 }

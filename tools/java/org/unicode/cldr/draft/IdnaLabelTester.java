419973a106de79ac92e3fe85bf31459bab3ac4e1
Unknown <unknown@svn.unicode.org>
2009-03-31 22:54:25 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1-7-post-st'.
    X-SVN-Rev: 3950

--- tools/java/org/unicode/cldr/draft/IdnaLabelTester.java
+++ tools/java/org/unicode/cldr/draft/IdnaLabelTester.java
@@ -2,28 +2,23 @@ package org.unicode.cldr.draft;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.unicode.cldr.util.PrettyPrinter;
 
 import com.ibm.icu.dev.test.util.VariableReplacer;
 import com.ibm.icu.lang.UCharacter;
 import com.ibm.icu.text.Normalizer;
 import com.ibm.icu.text.Transliterator;
+import com.ibm.icu.text.UTF16;
 import com.ibm.icu.text.UnicodeSet;
 
 public class IdnaLabelTester {
 
-  private static boolean VERBOSE = false;
+  private static boolean DEBUG = false;
 
   enum Result {none, next, next2, fail};
 
@@ -37,7 +32,7 @@ public class IdnaLabelTester {
     final int lineNumber;
     transient int length;
     transient String label;
-
+    
     public String toString() {
       return "{Rule "
       + (before == null ? "" : "before: " + beforeString + ", ")
@@ -90,7 +85,7 @@ public class IdnaLabelTester {
   }
 
   private List<Rule> rules = new ArrayList<Rule>();
-
+  
   private static final UnicodeSet NOT_NFKC_CASE_FOLD = computeNotNfkcCaseFold();
 
   public IdnaLabelTester(String file) throws IOException {
@@ -109,40 +104,39 @@ public class IdnaLabelTester {
         }
         line = line.trim();
         if (line.length() == 0) continue;
-
+        
         // do debug
-
-        if (startsWithIgnoreCase(line, "VERBOSE:")) {
-          VERBOSE = line.substring(8).trim().equalsIgnoreCase("true");
-          System.out.println("Verbose = " + VERBOSE);
+        
+        if (line.startsWith("VERBOSE:")) {
+          DEBUG = line.substring(8).trim().equalsIgnoreCase("true");
           continue;
         }
 
         // do title
 
-        if (startsWithIgnoreCase(line, "Title:")) {
+        if (line.startsWith("Title:")) {
           title = line.substring(6).trim();
           continue;
         }
 
         // do variables
 
-        if (startsWithIgnoreCase(line, "$")) {
+        if (line.startsWith("$")) {
           int equals = line.indexOf("=");
           if (equals >= 0) {
             final String variable = line.substring(0,equals).trim();
             final String value = variables.replace(line.substring(equals+1).trim());
-            if (VERBOSE && value.contains("$")) {
+            if (DEBUG && value.contains("$")) {
               System.out.println("Warning: contains $ " + variable + "\t=\t" + value);
             }
             // small hack, because this property isn't in ICU until 5.2
             UnicodeSet s = value.equals("[:^nfkc_casefolded:]")
-            ? NOT_NFKC_CASE_FOLD
+                    ? NOT_NFKC_CASE_FOLD
                     : new UnicodeSet(value).complement().complement();
-            if (VERBOSE) {
-              System.out.println("{Variable: " + variable + ", value: " + toPattern(s, true) + "}");
+            if (DEBUG) {
+              System.out.println("{Variable: " + variable + ", value: " + s.toPattern(false) + "}");
             }
-            variables.add(variable, toPattern(s, false));
+            variables.add(variable, s.toPattern(false));
             continue;
           }
         }
@@ -150,9 +144,9 @@ public class IdnaLabelTester {
         // do rules. This could be much more compact, but is broken out for debugging
 
         String[] pieces = line.split("\\s*;\\s*");
-        //        if (DEBUG) {
-        //          System.out.println(Arrays.asList(pieces));
-        //        }
+//        if (DEBUG) {
+//          System.out.println(Arrays.asList(pieces));
+//        }
         String before, at, result;
         switch (pieces.length) {
           case 2: before = null; at = pieces[0]; result= pieces[1]; break;
@@ -160,7 +154,7 @@ public class IdnaLabelTester {
           default: throw new IllegalArgumentException(line + " => " + Arrays.asList(pieces));
         }
         Rule rule = new Rule(before, at, result, title, lineCount, variables);
-        if (VERBOSE) {
+        if (DEBUG) {
           System.out.println(rule);
         }
         rules.add(rule);
@@ -199,44 +193,14 @@ public class IdnaLabelTester {
     }
   }
 
-  private static final Charset UTF8 = Charset.forName("utf-8");
-
   private static BufferedReader openFile(String file) throws IOException {
     try {
-      File file1 = new File(file);
-      //System.out.println("Reading:\t" + file1.getCanonicalPath());
-      return new BufferedReader(new InputStreamReader(new FileInputStream(file1), UTF8),1024*64);
+      return GenerateNormalizeForMatch.openUTF8Reader(file);
     } catch (Exception e) {
       File f = new File(file);
       throw new IllegalArgumentException("Bad file name: " + f.getCanonicalPath());
     }
   }
-
-  private static boolean startsWithIgnoreCase(String line, final String string) {
-    // we don't care about performance, and the arguments are only ASCII
-    return line.toLowerCase(Locale.ENGLISH).startsWith(string.toLowerCase(Locale.ENGLISH));
-  }
-
-  private static final UnicodeSet TO_QUOTE = new UnicodeSet("[[:z:][:me:][:mn:][:di:][:c:]-[\u0020]]");
-
-  private static final Transliterator UNESCAPER = Transliterator.getInstance("hex-any");
-  private static final Transliterator ESCAPER = Transliterator.getInstance("any-hex");
-  static {
-    ESCAPER.setFilter(TO_QUOTE);
-  }
-
-  private static final PrettyPrinter PRETTY_PRINTER = new PrettyPrinter()
-    .setToQuote(TO_QUOTE)
-    .setOrdering(null)
-    .setSpaceComparator(null);
-
-  private static String toPattern(UnicodeSet s, boolean escape) {
-    return !escape 
-            ? s.toPattern(false) 
-            : PRETTY_PRINTER.toPattern(s);
-  }
-
-  // ==================== Test Code =======================
 
   static class TestStatus {
     String title;
@@ -264,61 +228,62 @@ public class IdnaLabelTester {
     }
     boolean skipOverFail = false;
     boolean skipOverFailAndNext2 = false;
-    // note: it doesn't matter if we test in the middle of a supplemental character
-    for (int i= 0; i < label.length(); ++i) {
-      for (Rule rule : rules) {
-
-        // handle the skipping
-
-        switch (rule.result) {
-          case fail: if (skipOverFail || skipOverFailAndNext2) continue;
-          break;
-          case next2: if (skipOverFailAndNext2) continue;
-          break;
-        }
-        skipOverFail = false;
-        skipOverFailAndNext2 = false;
-
-        // check the rule
-
-        Result result = rule.match(i);
-        switch (result) {
-          case next: 
-            if (VERBOSE) {
-              rule.match(i);
-            }
-            skipOverFailAndNext2 = true;
+      // note: it doesn't matter if we test in the middle of a supplemental character
+      for (int i= 0; i < label.length(); ++i) {
+        for (Rule rule : rules) {
+          
+          // handle the skipping
+          
+          switch (rule.result) {
+            case fail: if (skipOverFail || skipOverFailAndNext2) continue;
             break;
-          case next2: 
-            if (VERBOSE) {
-              rule.match(i);
-            }
-            skipOverFail = true;
+            case next2: if (skipOverFailAndNext2) continue;
             break;
-          case fail: 
-            if (VERBOSE) {
-              rule.match(i);
-            }
-            return new TestStatus(i, rule.title, rule.lineNumber);
-          default:
-            if (VERBOSE) {
-              rule.match(i);
-            }
-          break;
+          }
+          skipOverFail = false;
+          skipOverFailAndNext2 = false;
+          
+          // check the rule
+          
+          Result result = rule.match(i);
+          switch (result) {
+            case next: 
+              if (DEBUG) {
+                rule.match(i);
+              }
+              skipOverFailAndNext2 = true;
+              break;
+            case next2: 
+              if (DEBUG) {
+                rule.match(i);
+              }
+              skipOverFail = true;
+              break;
+            case fail: 
+              if (DEBUG) {
+                rule.match(i);
+              }
+              return new TestStatus(i, rule.title, rule.lineNumber);
+            default:
+              if (DEBUG) {
+                rule.match(i);
+              }
+            break;
+          }
         }
       }
-    }
     return null; // success!
   }
 
   public static void main(String[] args) throws IOException {
     String dir = "java/org/unicode/cldr/draft/";
     IdnaLabelTester tester = new IdnaLabelTester(dir + "idnaContextRules.txt");
+    Transliterator unescaper = Transliterator.getInstance("hex-any");
+    Transliterator escaper = Transliterator.getInstance("[[:z:][:me:][:mn:][:di:]-[:cn:]] any-hex");
     BufferedReader in = openFile(dir + "idnaTestCases.txt");
     boolean expectedSuccess = true;
     int failures = 0;
     int successes = 0;
-    boolean firstTestLine = true;
 
     for (int lineCount = 1; ; ++lineCount) {
       String line = in.readLine();
@@ -327,7 +292,7 @@ public class IdnaLabelTester {
       if (commentPos >= 0) {
         line = line.substring(0,commentPos);
       }
-      line = UNESCAPER.transform(line);
+      line = unescaper.transform(line);
 
       line = line.trim();
       if (line.length() == 0) continue;
@@ -340,26 +305,17 @@ public class IdnaLabelTester {
         expectedSuccess = false;
         continue;
       }
-
-      if (startsWithIgnoreCase(line, "VERBOSE:")) {
-        VERBOSE = line.substring(8).trim().equalsIgnoreCase("true");
-        System.out.println("Verbose = " + VERBOSE);
+      if (line.startsWith("VERBOSE:")) {
+        DEBUG = line.substring(8).trim().equalsIgnoreCase("true");
         continue;
       }
 
-      if (firstTestLine) {
-        if (VERBOSE) {
-          System.out.println("# Test lines are in the form <lineNumber>. <successOrFailure> <reason>;");
-        }
-        firstTestLine = false;
-      }
-      
       TestStatus result = tester.test(line);
 
       boolean showLine = false;
       if (result == null) {
         if (expectedSuccess) {
-          if (VERBOSE) {
+          if (DEBUG) {
             System.out.print(lineCount + ". \tSuccess - expected and got Valid:\t");
             showLine = true;
           }
@@ -370,7 +326,7 @@ public class IdnaLabelTester {
           showLine = true;
         }
         if (showLine) {
-          System.out.println(ESCAPER.transform(line));
+          System.out.println(escaper.transform(line));
         }
       } else {
         if (expectedSuccess) {
@@ -378,15 +334,15 @@ public class IdnaLabelTester {
           failures++;
           showLine = true;
         } else {
-          if (VERBOSE) {
+          if (DEBUG) {
             System.out.print(lineCount + ". \tSuccess - expected and got Invalid:\t");
             showLine = true;
           }
           successes++;
         }
         if (showLine) {
-          System.out.println(ESCAPER.transform(line.substring(0, result.position)) 
-                  + "\u2639" + ESCAPER.transform(line.substring(result.position)) 
+          System.out.println(escaper.transform(line.substring(0, result.position)) 
+                  + "\u2639" + escaper.transform(line.substring(result.position)) 
                   + "\t\t" + result.title
                   + "; \tRuleLine: " + result.ruleLine);
         }

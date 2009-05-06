0dde1cd29de229738b1000761a37a3d6a1c9199a
Mark Davis <mark@macchiato.com>
2009-05-06 19:02:59 +0000

    CLDR-0 added comparison/formatting tools
    X-SVN-Rev: 4130

---
+++ tools/java/org/unicode/cldr/tool/CLDRFormat.java
@@ -0,0 +1,106 @@
+package org.unicode.cldr.tool;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.unicode.cldr.util.CLDRFile;
+import org.unicode.cldr.util.Log;
+import org.unicode.cldr.util.Utility;
+import org.unicode.cldr.util.CLDRFile.DraftStatus;
+import org.unicode.cldr.util.CLDRFile.Factory;
+import org.unicode.cldr.util.XPathParts.Comments;
+
+import com.ibm.icu.dev.test.util.BagFormatter;
+import com.ibm.icu.dev.test.util.CollectionUtilities;
+import com.ibm.icu.dev.tool.UOption;
+
+public class CLDRFormat {
+  public static void main(String[] args) throws Exception {
+    // TODO - make these parameters
+    String filter = Utility.getProperty("filter", ".*");
+    Matcher matcher = Pattern.compile(filter).matcher("");
+    File src = new File(Utility.COMMON_DIRECTORY);
+    File dest = new File(Utility.GEN_DIRECTORY + "common/");
+    //Log.setLog(Utility.GEN_DIRECTORY + "logCldr.txt");
+    for (String subDir : src.list()) {
+      if (subDir.equals("CVS") || subDir.equals("posix") || subDir.equals("test")) continue;
+      final String srcSubdir = src + "/" + subDir;
+      final File srcDir = new File(srcSubdir);
+      if (!srcDir.isDirectory()) continue;
+      final String destSubdir = dest + "/" + subDir;
+      Factory cldrFactory = Factory.make(srcSubdir, ".*");
+      for (String key : cldrFactory.getAvailable()) {
+        String subDirKey = subDir + "/" + key;
+        if (!matcher.reset(subDirKey).find()) {
+          //System.out.println("Skipping " + srcSubdir);
+          continue;
+        }
+        CLDRFile cldrFile = cldrFactory.make(key, false);
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter pw = new PrintWriter(stringWriter);
+        cldrFile.write(pw);
+        pw.flush();
+        final String results = stringWriter.toString();
+        pw.close();
+        
+        // write
+        PrintWriter out = BagFormatter.openUTF8Writer(destSubdir, key + ".xml");
+        out.write(results);
+        out.close();
+        
+        // check
+        try {
+          byte[] utf8 = results.getBytes("utf-8");
+          CLDRFile regenFile = CLDRFile.make(key, key, new ByteArrayInputStream(utf8), DraftStatus.unconfirmed);
+          String diff = findFirstDifference(cldrFile, regenFile);
+          if (diff != null) {
+            System.out.println("\tERROR: difference introduced in reformatting " + srcSubdir + "/" + key + ".xml" + "\n" + diff);
+          }
+        } catch (Exception e) {
+          System.err.println("\tERROR: can't read reformatted file " + srcSubdir + "/" + key + ".xml");
+          // e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  static Set<String> keys1 = new TreeSet();
+  static Set<String> keys2 = new TreeSet();
+  
+  private static String findFirstDifference(CLDRFile cldrFile, CLDRFile regenFile) {
+    keys1.clear();
+    keys2.clear();
+    CollectionUtilities.addAll(cldrFile.iterator(), keys1);
+    CollectionUtilities.addAll(regenFile.iterator(), keys2);
+    if (!keys1.equals(keys2)) {
+      Set missing = new TreeSet(keys1);
+      missing.removeAll(keys2);
+      Set extras = new TreeSet(keys2);
+      extras.removeAll(keys1);
+      return "\tMissing: " + missing.toString().replace(", ", ",\n") + ";\n\tExtras: " + extras.toString().replace(", ", ",\n");
+      
+    }
+    for (String path : keys1) {
+      if (path.startsWith("//ldml/identity/generation") || path.startsWith("//ldml/identity/version")) {
+        continue;
+      }
+      String full1 = cldrFile.getFullXPath(path);
+      String full2 = regenFile.getFullXPath(path);
+      if (!full1.equals(full2)) {
+        return "\tFull XPaths differ: " + full1 + "!=" + full2;
+      }
+    }
+    Comments comments1 = cldrFile.getXpath_comments();
+    Comments comments2 = regenFile.getXpath_comments();
+    // TODO fix later
+    return null;
+  }
+}

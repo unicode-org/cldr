82bd8bbca7bf896016b6ea62e79c02d70bb3a71b
Unknown <unknown@svn.unicode.org>
2011-03-11 23:10:10 +0000

    CLDR-3483 fix approved translation's column, again
    X-SVN-Rev: 5446

--- tools/cldr-apps/src/org/unicode/cldr/web/DataSection.java
+++ tools/cldr-apps/src/org/unicode/cldr/web/DataSection.java
@@ -743,13 +743,14 @@ public class DataSection extends Registerable {
         public CandidateItem getCurrentItem() {
             if (this.currentItems == null) {
 
-                // String resultXpath = getResultXpath();
+                getResultXpath(); /* Make sure the resultXpath is loaded. */
 
                 List<DataSection.DataRow.CandidateItem> currentItems = new ArrayList<DataSection.DataRow.CandidateItem>();
                 List<DataSection.DataRow.CandidateItem> proposedItems = new ArrayList<DataSection.DataRow.CandidateItem>();
 
                 for (CandidateItem item : items) {
-		    if(sm.isUnofficial && false) System.err.println("Considering: " + item+", xpid="+item.xpathId+", result="+resultXpath_id+", base="+this.base_xpath+", ENO="+errorNoOutcome);
+		    //if(sm.isUnofficial && false) System.err.println("Considering: " + item+", xpid="+item.xpathId+", result="+resultXpath_id+", base="+this.base_xpath+", ENO="+errorNoOutcome);
+		    // item.toString()
                     if (((item.xpathId == resultXpath_id) || (resultXpath_id == -1
                             && item.xpathId == this.base_xpath && !errorNoOutcome))
                             && // do NOT add as current, if vetting said 'no' to

aacec1f655d8d6e7c722ba12abda11983dc16165
Peter Edberg <pedberg@unicode.org>
2011-12-07 07:33:25 +0000

    CLDR-4248 update GEN_VERSION in CLDRFile
    X-SVN-Rev: 6283

--- tools/java/org/unicode/cldr/util/CLDRFile.java
+++ tools/java/org/unicode/cldr/util/CLDRFile.java
@@ -98,7 +98,7 @@ public class CLDRFile implements Freezable<CLDRFile>, Iterable<String> {
   public static final String SUPPLEMENTAL_NAME = "supplementalData";
   public static final String SUPPLEMENTAL_METADATA = "supplementalMetadata";
   public static final String SUPPLEMENTAL_PREFIX = "supplemental";
-  public static final String GEN_VERSION = "2.0";
+  public static final String GEN_VERSION = "21.0";
 
   private boolean locked;
   XMLSource dataSource;  // TODO(jchye): make private

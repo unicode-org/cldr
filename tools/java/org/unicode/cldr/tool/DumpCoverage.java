6cee3e32f9e6649cc06a1ea6bcc61cd59acc3872
John Emmons <emmo@us.ibm.com>
2012-05-17 02:14:05 +0000

    CLDR-4637 Add DumpCoverage tool
    X-SVN-Rev: 7130

---
+++ tools/java/org/unicode/cldr/tool/DumpCoverage.java
@@ -0,0 +1,37 @@
+package org.unicode.cldr.tool;
+
+
+import com.ibm.icu.dev.test.util.BagFormatter;
+import com.ibm.icu.util.ULocale;
+import org.unicode.cldr.util.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
+import java.util.TreeSet;
+public class DumpCoverage {
+
+/**
+  * @param args
+  */
+    public static void main(String[] args) throws IOException {
+
+        SupplementalDataInfo sdi = SupplementalDataInfo.getInstance();
+        Factory cldrFactory = Factory.make(CldrUtility.MAIN_DIRECTORY, ".*");
+        Set<String> languages = cldrFactory.getAvailableLanguages();
+        PrintWriter out = BagFormatter.openUTF8Writer(CldrUtility.GEN_DIRECTORY,"coverageDump.txt");
+        for ( String lang : languages ) {
+            ULocale loc = new ULocale(lang);
+            CLDRFile cf = cldrFactory.makeWithFallback(lang);
+            Set<String> paths = new TreeSet<String>();
+            cf.getPaths("//ldml", null, paths);
+            System.out.println("Dumping coverage for locale --> " + loc);
+            for ( String path : paths) {
+                int cov = sdi.getCoverageValue(path, loc);
+                out.println(lang + " [" + cov + "] --> " + path);
+                
+            }
+        }
+        out.close();
+    }
+}

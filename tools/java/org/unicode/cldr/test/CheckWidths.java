d9719ebd44ac631427b72771560b87ce3525f341
Mark Davis <mark@macchiato.com>
2012-06-19 22:08:32 +0000

    CLDR-4906 Refine the width measurements, provide warnings for items close to being too long.
    X-SVN-Rev: 7326

--- tools/java/org/unicode/cldr/test/CheckWidths.java
+++ tools/java/org/unicode/cldr/test/CheckWidths.java
@@ -17,95 +17,129 @@ public class CheckWidths extends CheckCLDR {
 
     private static final boolean DEBUG = true;
 
+    private enum Measure {CODE_POINTS, DISPLAY_WIDTH}
+    private enum LimitType {MINIMUM, MAXIMUM}
     private enum Special {NONE, QUOTES}
-    private static class Limits {
-        public Limits(int minimumLength, int maximumLength, double minimumWidth, double maximumWidth,
-            String errorType, Special special) {
-            this.minimumLength = minimumLength;
-            this.maximumLength = maximumLength;
-            this.minimumWidth = minimumWidth;
-            this.maximumWidth = maximumWidth;
-            this.errorType = errorType;
-            this.special = special;
-        }
-        final int minimumLength;
-        final int maximumLength;
-        final double minimumWidth;
-        final double maximumWidth;
+    
+    private static class Limit {
+        final double reference;
+        final LimitType limit;
+        final Measure measure;
         final String errorType;
         final Special special;
+        final String message;
+        final Subtype subtype;
+
+        public Limit(double reference, Measure measure, LimitType limit, String errorType, Special special) {
+            this.reference = reference;
+            this.limit = limit;
+            this.measure = measure;
+            this.errorType = errorType;
+            this.special = special;
+            switch (limit) {
+            case MINIMUM:
+                this.message = measure == Measure.CODE_POINTS 
+                ? "Expected length of ≥{0} character(s), but was {1}" 
+                    : "Expected width of ≥{0} em, but was ~{1} em." ;
+                this.subtype = Subtype.valueTooNarrow;
+                break;
+            case MAXIMUM:
+                this.message = measure == Measure.CODE_POINTS 
+                ? "Expected length of ≤{0} character(s), but was {1}."
+                    : "Expected width of ≤{0} em, but was ~{1} em.";
+                this.subtype = Subtype.valueTooWide;
+                break;
+            default: throw new IllegalArgumentException();
+            }
+        }
+
         boolean hasProblem(String value, List<CheckStatus> result, CheckCLDR cause) {
             if (special == Special.QUOTES) {
                 value = value.replace("'","");
             }
-            boolean lengthProblem = false;
-            if (minimumLength >= 0 
-                && value.codePointCount(0, value.length()) < minimumLength) {
-                lengthProblem = true;
-                result.add(new CheckStatus().setCause(cause).setMainType(errorType).setSubtype(Subtype.valueTooNarrow) 
-                    .setMessage("There are too few characters: should be at least {0} character long.", minimumLength)); 
-            } else if (maximumLength >= 0 
-                && value.codePointCount(0, value.length()) > maximumLength) {
-                lengthProblem = true;
-                result.add(new CheckStatus().setCause(cause).setMainType(errorType).setSubtype(Subtype.valueTooWide) 
-                    .setMessage("There are too many characters: should be at most {0} character long.", maximumLength)); 
-            }
-            if (!lengthProblem && (minimumWidth >= 0 || maximumWidth >= 0)) {
-                double width = ApproximateWidth.getWidth(value) / EM;
-                if (minimumWidth >= 0 
-                    && width < minimumWidth) {
-                    lengthProblem = true;
-                    result.add(new CheckStatus().setCause(cause).setMainType(errorType).setSubtype(Subtype.valueTooNarrow) 
-                        .setMessage("The value is not wide enough: should have at least a width of {0} em, but was ~{1} em.", minimumWidth, width)); 
-                } else if (maximumWidth >= 0 
-                    && width > maximumWidth) {
-                    lengthProblem = true;
-                    result.add(new CheckStatus().setCause(cause).setMainType(errorType).setSubtype(Subtype.valueTooWide) 
-                        .setMessage("The value is too wide: should have at most a width of {0} em, but was ~{1} em.", maximumWidth, width)); 
+            double valueMeasure = measure == Measure.CODE_POINTS 
+                ? value.codePointCount(0, value.length()) 
+                    : ApproximateWidth.getWidth(value) / EM;
+            switch (limit) {
+            case MINIMUM: 
+                if (valueMeasure >= reference) {
+                    return false;
                 }
+                break;
+            case MAXIMUM: 
+                if (valueMeasure <= reference) {
+                    return false;
+                }
+                break;
             }
-            return lengthProblem;
+            result.add(new CheckStatus().setCause(cause).setMainType(errorType).setSubtype(subtype) 
+                .setMessage(message, reference, valueMeasure)); 
+            return true;
         }
     }
 
-    static RegexLookup<Limits> lookup = new RegexLookup<Limits>()
+    // WARNING: errors must occur before warnings!!
+    
+    static RegexLookup<Limit[]> lookup = new RegexLookup<Limit[]>()
         .setPatternTransform(RegexLookup.RegexFinderTransformPath)
         .addVariable("%A", "\"[^\"]+\"")
-        .add("//ldml/delimiters/(quotation|alternateQuotation)", new Limits(1, 1, -1, -1, CheckStatus.errorType, Special.NONE))
+        .add("//ldml/delimiters/(quotation|alternateQuotation)", new Limit[] {
+            new Limit(1, Measure.CODE_POINTS, LimitType.MAXIMUM, CheckStatus.errorType, Special.NONE)
+            })
 
-        // The following are rough measures, just to check strange cases
-
-        .add("//ldml/characters/ellipsis[@type=\"final\"]", new Limits(-1, -1, -1, 2 * ApproximateWidth.getWidth("{0}…"), CheckStatus.warningType, Special.NONE))
-        .add("//ldml/characters/ellipsis[@type=\"initial\"]", new Limits(-1, -1, -1, 2 * ApproximateWidth.getWidth("…{0}"), CheckStatus.warningType, Special.NONE))
-        .add("//ldml/characters/ellipsis[@type=\"medial\"]", new Limits(-1, -1, -1, 2 * ApproximateWidth.getWidth("{0}…{1}"), CheckStatus.warningType, Special.NONE))
-
-        // Narrow items should be no wider than 2.5 em
-
-        .add("//ldml/dates/calendars/calendar.*[@type=\"narrow\"](?!/cyclic|/dayPeriod)", new Limits(-1, -1, -1, 2.5, CheckStatus.errorType, Special.NONE))
-        // \"(?!am|pm)[^\"]+\"\\
         // Numeric items should be no more than a single character
 
-        .add("//ldml/numbers/symbols[@numberSystem=%A]/(decimal|group|minus|percent|perMille|plus)", new Limits(1, 1, -1, -1, CheckStatus.errorType, Special.NONE))
-        
-        // Compact number formats must be no bigger than 4 EM
+        .add("//ldml/numbers/symbols[@numberSystem=%A]/(decimal|group|minus|percent|perMille|plus)", new Limit[] {
+            new Limit(1, Measure.CODE_POINTS, LimitType.MAXIMUM, CheckStatus.errorType, Special.NONE)
+            })
 
-        .add("//ldml/numbers/decimalFormats[@numberSystem=%A]/decimalFormatLength[@type=\"short\"]/decimalFormat[@type=%A]/pattern[@type=\"1", new Limits(-1, -1, -1, 4.5, CheckStatus.errorType, Special.QUOTES))
-        ;
-    
-    Set<Limits> found = new LinkedHashSet();
+        // Now widths
+        // The following are rough measures, just to check strange cases
+
+        .add("//ldml/characters/ellipsis[@type=\"final\"]", new Limit[] {
+            new Limit(1.5 * ApproximateWidth.getWidth("{0}…"), Measure.DISPLAY_WIDTH, LimitType.MAXIMUM, CheckStatus.warningType, Special.NONE)
+            })
+        .add("//ldml/characters/ellipsis[@type=\"initial\"]", new Limit[] {
+            new Limit(1.5 * ApproximateWidth.getWidth("…{0}"), Measure.DISPLAY_WIDTH, LimitType.MAXIMUM, CheckStatus.warningType, Special.NONE)
+            })
+        .add("//ldml/characters/ellipsis[@type=\"medial\"]", new Limit[] {
+            new Limit(1.5 * ApproximateWidth.getWidth("{0}…{1}"), Measure.DISPLAY_WIDTH, LimitType.MAXIMUM, CheckStatus.warningType, Special.NONE)
+            })
+
+        // Narrow items
+
+        .add("//ldml/dates/calendars/calendar.*[@type=\"narrow\"](?!/cyclic|/dayPeriod)", new Limit[] {
+            new Limit(2.25, Measure.DISPLAY_WIDTH, LimitType.MAXIMUM, CheckStatus.errorType, Special.NONE),
+            new Limit(1.5, Measure.DISPLAY_WIDTH, LimitType.MAXIMUM, CheckStatus.warningType, Special.NONE)
+            })
+        // \"(?!am|pm)[^\"]+\"\\
+
+        // Compact number formats
+
+        .add("//ldml/numbers/decimalFormats[@numberSystem=%A]/decimalFormatLength[@type=\"short\"]/decimalFormat[@type=%A]/pattern[@type=\"1", 
+            new Limit[] {
+            new Limit(4.5, Measure.DISPLAY_WIDTH, LimitType.MAXIMUM, CheckStatus.errorType, Special.QUOTES),
+            new Limit(4, Measure.DISPLAY_WIDTH, LimitType.MAXIMUM, CheckStatus.warningType, Special.QUOTES)
+            })
+            ;
+
+    Set<Limit> found = new LinkedHashSet();
 
     public CheckCLDR handleCheck(String path, String fullPath, String value, Map<String, String> options, List<CheckStatus> result) {
         if (value == null) {
             return this; // skip
         }
-//        Limits item0 = lookup.get("//ldml/numbers/decimalFormats[@numberSystem=\"latn\"]/decimalFormatLength[@type=\"short\"]/decimalFormat[@type=\"standard\"]/pattern[@type=\"1000000000\"][@count=\"other\"]");
-//        item0.check("123456789", result, this);
-        
-        Limits item = lookup.get(path);
-        if (item != null) {
-            if (item.hasProblem(value, result, this)) {
-                if (DEBUG && !found.contains(item)) {
-                    found.add(item);
+        //        Limits item0 = lookup.get("//ldml/numbers/decimalFormats[@numberSystem=\"latn\"]/decimalFormatLength[@type=\"short\"]/decimalFormat[@type=\"standard\"]/pattern[@type=\"1000000000\"][@count=\"other\"]");
+        //        item0.check("123456789", result, this);
+
+        Limit[] items = lookup.get(path);
+        if (items != null) {
+            for (Limit item : items) {
+                if (item.hasProblem(value, result, this)) {
+                    if (DEBUG && !found.contains(item)) {
+                        found.add(item);
+                    }
+                    break; // only one error per item
                 }
             }
         }

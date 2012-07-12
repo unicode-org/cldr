9fd0843cd96e7b0c60fe0958bebda4274ad7628b
Peter Edberg <pedberg@unicode.org>
2012-07-12 21:41:35 +0000

    CLDR-4745 Restore changes for this as of r7395; add temporary debugging info in SimpleFactory.java
    X-SVN-Rev: 7406

--- tools/java/org/unicode/cldr/util/SimpleFactory.java
+++ tools/java/org/unicode/cldr/util/SimpleFactory.java
@@ -115,11 +115,21 @@ public class SimpleFactory extends Factory {
         if (result == null) {
             if (resolved) {
                 result = new CLDRFile(makeResolvingSource(localeName, minimalDraftStatus));
+                if (result==null && true /*DEBUG*/) {
+                    System.out.println("# SimpleFactory.handleMake, resolved=T, locale " + localeName + ", new CLDRFile returned null"); 
+                }
             } else {
                 final File parentDir = getSourceDirectoryForLocale(localeName);
                 if(parentDir != null) {
                     result = makeFile(localeName, parentDir, minimalDraftStatus);
                     result.freeze();
+                }
+                if (result==null && true /*DEBUG*/) {
+                    if (parentDir == null) {
+                        System.out.println("# SimpleFactory.handleMake, resolved=F, locale " + localeName + ", getSourceDirectoryForLocale returned null"); 
+                    } else {
+                        System.out.println("# SimpleFactory.handleMake, resolved=F, locale " + localeName + ", makeFile returned null"); 
+                    }
                 }
             }
             if(result!=null) {
@@ -228,6 +238,9 @@ public class SimpleFactory extends Factory {
             final File xmlFile = makeFileName(localeName,dir);
             if(xmlFile.canRead()) {
                 return dir;
+            } else if (localeName.equals("en_150") && true /*DEBUG*/) {
+                System.out.println("# SimpleFactory.getSourceDirectoryForLocale en_150, can't read file, isSupplemental "
+                        + isSupplemental + ", dir " + dir.getPath() + ", xmlFile " + xmlFile.getPath()); 
             }
         }
         return null;

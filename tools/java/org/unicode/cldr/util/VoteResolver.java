fff980957e8983f29185bfc23c25a6ac0ce2c2b3
Rick McGowan <ricksvn@36748296+rickmcgowan@users.noreply.github.com>
2013-01-14 21:48:31 +0000

    CLDR-5572 Added Bangor Univ as an org (Welsh).
    X-SVN-Rev: 8035

--- tools/java/org/unicode/cldr/util/VoteResolver.java
+++ tools/java/org/unicode/cldr/util/VoteResolver.java
@@ -83,10 +83,10 @@ public class VoteResolver<T> {
      * so that we know when new ones show up.
      */
     public enum Organization {
-        // adobe, afrigen, apple, bhutan, breton, cherokee, georgia_isi, gnome, google, guest, ibm, india, iran_hci, kendra,
+        // adobe, afrigen, apple, bangor_univ, bhutan, breton, cherokee, georgia_isi, gnome, google, guest, ibm, india, iran_hci, kendra,
         // kotoistus, lao_dpt, openinstitute, openoffice_org, oracle, pakistan, sil, srilanka, sun, surveytool, utilika,
         // wikimedia, yahoo;
-        adobe, afrigen, apple, bhutan, breton, cherokee, georgia_isi, gnome, google, guest, ibm, india, iran_hci, kendra, kotoistus, lao_dpt, openinstitute, openoffice_org, oracle, pakistan, sil, srilanka, sun, surveytool, utilika, wikimedia, yahoo;
+        adobe, afrigen, apple, bangor_univ, bhutan, breton, cherokee, georgia_isi, gnome, google, guest, ibm, india, iran_hci, kendra, kotoistus, lao_dpt, openinstitute, openoffice_org, oracle, pakistan, sil, srilanka, sun, surveytool, utilika, wikimedia, yahoo;
         // Not used (but keep in to avoid breakage):   sun
         // Please update Locales.txt for default coverage when adding an organization here. 
         

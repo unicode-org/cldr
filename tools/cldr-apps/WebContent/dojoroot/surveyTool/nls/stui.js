80a14d9023a5c12a27cec37a4f1b06417ab8357e
Steven R. Loomis <srl@icu-project.org>
2013-04-26 14:22:01 +0000

    CLDR-5951 fix oldvotes order: yes, no, dontask
    X-SVN-Rev: 8598

--- tools/cldr-apps/WebContent/dojoroot/surveyTool/nls/stui.js
+++ tools/cldr-apps/WebContent/dojoroot/surveyTool/nls/stui.js
@@ -217,9 +217,9 @@ define({
            
            v_oldvote_remind_msg: "CLDR: Old Votes Reminder Message",
            v_oldvote_remind_desc_msg: "You currently have ${count} votes from previous CLDR vetting periods. Would you like to view them for import into the current release?<p>  (Note: You can always import these votes via the '<span class=notselected>Manage</span>' link once logged in.)",
-           v_oldvote_remind_yes: "View Old Votes",
-           v_oldvote_remind_no: "Not today",
-           v_oldvote_remind_dontask: "Don’t ask me again",
+           v_oldvote_remind_yes: "Yes, view old votes",
+           v_oldvote_remind_no: "No, not today",
+           v_oldvote_remind_dontask: "No, and don't ask again",
            "v-title_desc": "This area shows the date before which votes are considered “old”.",
            special_oldvotes: "Import Old Votes",
            section_general: "General Info",
--- tools/cldr-apps/WebContent/js/survey.js
+++ tools/cldr-apps/WebContent/js/survey.js
@@ -4186,55 +4186,55 @@ function showV() {
 					// any special message? "oldVotesRemind":{"count":60,"pref":"oldVoteRemind24", "remind":"* | ##"}
 					if(json.oldVotesRemind && surveyCurrentSpecial!='oldvotes') {
 						var vals = { count: dojoNumber.format(json.oldVotesRemind.count) };
-						
+
 						function updPrefTo(target) {
-                            var updurl  = contextPath + "/SurveyAjax?_="+theLocale+"&s="+surveySessionId+"&what=pref&pref=oldVoteRemind&_v="+target+cacheKill();                             myLoad(updurl, "updating coldremind " + target, function(json2) {
-                            	if(!verifyJson(json2,'pref')) {
-                            		return;
-                            	} else {
-                            		console.log('Server set  coldremind successfully.');
-                            	}
-                            });
-                        }						
+							var updurl  = contextPath + "/SurveyAjax?_="+theLocale+"&s="+surveySessionId+"&what=pref&pref=oldVoteRemind&_v="+target+cacheKill();                             myLoad(updurl, "updating coldremind " + target, function(json2) {
+								if(!verifyJson(json2,'pref')) {
+									return;
+								} else {
+									console.log('Server set  coldremind successfully.');
+								}
+							});
+						}						
 						var oldVoteRemindDialog = new Dialog({
 							title: stui.sub("v_oldvote_remind_msg",vals), 
 							content: stui.sub("v_oldvote_remind_desc_msg", vals)});
-						
-                            oldVoteRemindDialog.addChild(new Button({
-                            	label: stui.str("v_oldvote_remind_dontask"),
-                            	onClick: function() {
-                            	    updPrefTo('*'); // hide permanently
-                            	    oldVoteRemindDialog.hide();
-                            	}
-                            }));
-                            oldVoteRemindDialog.addChild(new Button({
-                            	label: stui.str("v_oldvote_remind_no"),
-                            	onClick: function() {
-                            	    updPrefTo(new Date().getTime() + (1000 * 86400)); // hide for 24 hours
-                            	    oldVoteRemindDialog.hide();
-                            	}                            	
-                            }));
-                            oldVoteRemindDialog.addChild(new Button({
-                            	label: stui.str("v_oldvote_remind_yes"),
-                            	onClick: function() {
-                            	    updPrefTo(new Date().getTime() + (1000 * 3600));// hide for 1 hr
-                            	    oldVoteRemindDialog.hide();
-                            	    surveyCurrentSpecial="oldvotes";
-                            	    surveyCurrentLocale='';
-                            	    surveyCurrentPage='';
-                            	    surveyCurrentSection='';
-                            	    reloadV();
-                            	}
-                            	
-                            }));
-						
-					    var now = new Date();
-					    if(json.oldVotesRemind.remind && now.getTime()<=parseInt(json.oldVotesRemind.remind)) {
-					        console.log("Have " + json.oldVotesRemind.count + " old votes, but will remind again in " + (parseInt(json.oldVotesRemind.remind)-now.getTime())/1000 + " seconds.");
-					    } else {
-    						oldVoteRemindDialog.show();
-	    					console.log("Showed oldVotesRemind 6");
-					    }
+
+						oldVoteRemindDialog.addChild(new Button({
+							label: stui.str("v_oldvote_remind_yes"),
+							onClick: function() {
+								updPrefTo(new Date().getTime() + (1000 * 3600));// hide for 1 hr
+								oldVoteRemindDialog.hide();
+								surveyCurrentSpecial="oldvotes";
+								surveyCurrentLocale='';
+								surveyCurrentPage='';
+								surveyCurrentSection='';
+								reloadV();
+							}
+
+						}));
+						oldVoteRemindDialog.addChild(new Button({
+							label: stui.str("v_oldvote_remind_no"),
+							onClick: function() {
+								updPrefTo(new Date().getTime() + (1000 * 86400)); // hide for 24 hours
+								oldVoteRemindDialog.hide();
+							}                            	
+						}));
+						oldVoteRemindDialog.addChild(new Button({
+							label: stui.str("v_oldvote_remind_dontask"),
+							onClick: function() {
+								updPrefTo('*'); // hide permanently
+								oldVoteRemindDialog.hide();
+							}
+						}));
+
+						var now = new Date();
+						if(json.oldVotesRemind.remind && now.getTime()<=parseInt(json.oldVotesRemind.remind)) {
+							console.log("Have " + json.oldVotesRemind.count + " old votes, but will remind again in " + (parseInt(json.oldVotesRemind.remind)-now.getTime())/1000 + " seconds.");
+						} else {
+							oldVoteRemindDialog.show();
+							console.log("Showed oldVotesRemind 6");
+						}
 					} else {
 						stdebug("Did not need to showoldvotesremind : " + Object.keys(json).toString());
 					}

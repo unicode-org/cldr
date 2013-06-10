7a373e7dc8f945931cb17057e4f3d3518072872e
Peter Edberg <pedberg@unicode.org>
2013-06-10 06:35:25 +0000

    CLDR-6225 Add 05BE and 05F3 to CheckExemplars.AlwaysOK, remove them from Hebrew aux exemplars
    X-SVN-Rev: 8867

--- tools/java/org/unicode/cldr/test/CheckExemplars.java
+++ tools/java/org/unicode/cldr/test/CheckExemplars.java
@@ -34,7 +34,7 @@ public class CheckExemplars extends FactoryCheckCLDR {
         "[[:Hangul_Syllable_Type=LVT:][:Hangul_Syllable_Type=LV:]]").freeze();
 
     public static final UnicodeSet AlwaysOK = new UnicodeSet(
-        "[[[:Nd:][:script=common:][:script=inherited:]-[:Default_Ignorable_Code_Point:]-[:C:] - [_]] [\u066A-\u066C]" +
+        "[[[:Nd:][:script=common:][:script=inherited:]-[:Default_Ignorable_Code_Point:]-[:C:] - [_]] [\u05BE \u05F3 \u066A-\u066C]" +
             "[[؉][་ །༌][ཱ]‎‎{য়}য়]" + // TODO Fix this Hack
             "]").freeze(); // [\\u200c-\\u200f] [:script=common:][:script=inherited:]
 

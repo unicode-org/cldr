a581ace59f1360b7dbd933bbc974a7d0eb3519d4
Craig Cornelius <ccornelius@google.com>
2014-01-22 22:34:16 +0000

    CLDR-6801 Detect Zawgyi font encoding in input, and convert to Unicode.  This update adds a regular expression to identify Zawgyi encoding and also a transliteration rule that converts such text to Unicode.
    X-SVN-Rev: 9654

--- tools/java/org/unicode/cldr/unittest/TestDisplayAndInputProcessor.java
+++ tools/java/org/unicode/cldr/unittest/TestDisplayAndInputProcessor.java
@@ -1,5 +1,6 @@
 package org.unicode.cldr.unittest;
 
+import org.apache.tools.ant.util.StringUtils;
 import org.unicode.cldr.test.DisplayAndInputProcessor;
 import org.unicode.cldr.unittest.TestAll.TestInfo;
 import org.unicode.cldr.util.CLDRFile;
@@ -54,6 +55,92 @@ public class TestDisplayAndInputProcessor extends TestFmwk {
         }
     }
 
+    public void TestMyanmarZawgyi() {
+        // Check that the Zawgyi detector and Zawgyi->Unicode converter perform correctly.
+        DisplayAndInputProcessor daip = new DisplayAndInputProcessor(info.getCldrFactory().make("my", false));
+        String z_mi = "ေမာင္းရီ (နယူးဇီလန္ကၽြန္းရွိ ပင္ရင္းတိုင္းရင္းသားလူမ်ိဳး)";  // language mi in CLDR 24
+        String u_mi = "မောင်းရီ (နယူးဇီလန်ကျွန်းရှိ ပင်ရင်းတိုင်းရင်းသားလူမျိုး)";  // mi converted to Unicode
+        
+        // Check that z_mi is detected as Zawgyi, and converted to u_mi.
+        // Check that the converted version is detected as Unicode.
+        String converted_z_mi = daip.processInput("", z_mi, null);
+        if (!converted_z_mi.equals(u_mi)) {
+            errln("Myanmar Zawgyi value incorrectly normalized: \n " + z_mi + " to \n" + ">" +
+                converted_z_mi + "<, expected\n" + ">" + u_mi + "<");
+        }
+        String converted_u_mi = daip.processInput("", u_mi, null);
+        if (!converted_u_mi.equals(u_mi)) {
+            errln("Myanmar Unicode value incorrectly changed:\n" + u_mi + " to\n" + converted_u_mi);
+        }
+        // TODO(ccorn): test other strings with the converter.
+        String mixed_latn_zawgyi = "ABCDE " + z_mi + "XYZ";
+        String mixed_latn_unicode = "ABCDE " + u_mi + "XYZ";
+        String converted_mixed = daip.processInput("", mixed_latn_zawgyi, null);
+        if (!converted_mixed.equals(mixed_latn_unicode)) {
+            errln("Myanmar mixed value incorrectly normalized:" + converted_mixed.length() + "\n" + mixed_latn_zawgyi + " to " + mixed_latn_unicode.length() + "\n" + converted_mixed + ", expected\n" +
+                mixed_latn_unicode);
+        }
+        
+        // Test 1039 conversion - simple cases.
+        String z1039 = "\u1031\u1019\u102c\u1004\u1039\u1038\u101b\u102e\u0020\u0028\u1014" +
+        "\u101A\u1030\u1038\u1007\u102E\u101C\u1014\u1039\u1000\u107D\u103C\u1014\u1039\u1038\u101B\u103D\u102D";
+        String u103a = "\u1019\u1031\u102c\u1004\u103a\u1038\u101b\u102e\u0020\u0028\u1014" +
+        "\u101A\u1030\u1038\u1007\u102E\u101C\u1014\u103A\u1000\u103B\u103D\u1014\u103A\u1038\u101B\u103E\u102D";
+        String converted_1039 = daip.processInput("", z1039, null);
+        if (!converted_1039.equals(u103a)) {
+            errln("Myanmar #1039 (Unicode) was changed: \n" + z1039 + " to \n" + converted_1039 + ", expected \n" + u103a);
+        }
+        
+        String z0 = "\u1000\u1005\u102C\u1038\u101E\u1019\u102C\u1038";  // Test #0
+        String converted_0 = daip.processInput("", z0, null);
+        if (!converted_0.equals(z0)) {
+            errln("Myanmar #0 (Unicode) was changed: " + z0 + " to " + converted_0);
+        }
+        
+        String z5 = "\u1021\u101E\u1004\u1039\u1038\u1019\u103D";  // Test #5
+        String u5 = "\u1021\u101E\u1004\u103A\u1038\u1019\u103E";
+        String converted_5 = daip.processInput("", z5, null);
+        if (!converted_5.equals(u5)) {
+            errln("Myanmar #5 incorrectly normalized: " + z5 + " to " + converted_5);
+        }        
+ 
+        String z_with_space = "\u0020\u102e\u0020\u1037\u0020\u1039";  // Test #5
+        String u_with_space = "\u00a0\u102e\u00a0\u1037\u00a0\u103a";
+        String converted_space = daip.processInput("", z_with_space, null);
+        if (!converted_space.equals(u_with_space)) {
+            errln("Myanmar with space incorrectly normalized:\n" + z_with_space + " to\n" + converted_space +
+                '\n' + u_with_space);
+        }
+        
+        String z_zero = "\u1031\u1040\u1037";
+        String u_zero = "\u101d\u1031\u1037";
+        String converted_zero = daip.processInput("", z_zero, null);
+        if (!converted_zero.equals(u_zero)) {
+            errln("Myanmar with diacritics and zero incorrectly normalized:\n" + z_zero + " to\n" + converted_zero +
+                '\n' + u_zero);
+        }
+        // Check that multiple digits are not converted.
+        z_zero = "\u1041\u1040\u1037";
+        u_zero = "\u1041\u1040\u1037";
+        converted_zero = daip.processInput("", z_zero, null);
+        if (!converted_zero.equals(u_zero)) {
+            errln("Myanmar with two zeros incorrectly normalized:\n" + z_zero + " to\n" + converted_zero +
+                '\n' + u_zero);
+        }
+        
+        // More checks that Unicode is not converted.
+        String is_unicode = "\u1019\u101B\u103E\u102D\u101E\u1031\u102C";
+        String check_is_unicode = daip.processInput("", is_unicode, null);
+        if (!check_is_unicode.equals(is_unicode)) {
+            errln("Myanmar should not have converted:\n" + is_unicode + " to\n" + check_is_unicode);
+        }
+        is_unicode = "\u1001\u103A\u103B";
+        check_is_unicode = daip.processInput("", is_unicode, null);
+        if (!check_is_unicode.equals(is_unicode)) {
+            errln("Myanmar should not have converted:\n" + is_unicode + " to\n" + check_is_unicode);
+        }
+    }
+    
     public void TestCompactNumberFormats() {
         DisplayAndInputProcessor daip = new DisplayAndInputProcessor(info.getEnglish(), false);
         String xpath = "//ldml/numbers/decimalFormats[@numberSystem=\"latn\"]/decimalFormatLength[@type=\"long\"]/decimalFormat[@type=\"standard\"]/pattern[@type=\"1000\"] ";

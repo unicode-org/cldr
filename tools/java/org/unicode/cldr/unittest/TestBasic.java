1ea4787a85bcd7c71d03021abd0ba9d381726cc4
John Emmons <emmo@us.ibm.com>
2014-02-25 00:03:22 +0000

    CLDR-6412 Put in framework for old TestDtdCompatibility test into TestBasic - disabling until 6903 is fixed.
    X-SVN-Rev: 9777

--- tools/java/org/unicode/cldr/unittest/TestBasic.java
+++ tools/java/org/unicode/cldr/unittest/TestBasic.java
@@ -36,9 +36,12 @@ import org.unicode.cldr.util.CharacterFallbacks;
 import org.unicode.cldr.util.CldrUtility;
 import org.unicode.cldr.util.Counter;
 import org.unicode.cldr.util.DtdData;
+import org.unicode.cldr.util.DiscreteComparator.Ordering;
 import org.unicode.cldr.util.DtdData.Attribute;
 import org.unicode.cldr.util.DtdData.Element;
 import org.unicode.cldr.util.DtdData.ElementType;
+import org.unicode.cldr.util.DiscreteComparator;
+import org.unicode.cldr.util.ElementAttributeInfo;
 import org.unicode.cldr.util.Factory;
 import org.unicode.cldr.util.InputStreamFactory;
 import org.unicode.cldr.util.LanguageTagParser;
@@ -891,9 +894,111 @@ public class TestBasic extends TestFmwkPlus {
             }            
         }
     }
+    public void TestBasicDTDCompatibility() {
+        if (logKnownIssue("Cldrbug:6903", "Need the directory before enabling this test")) {
+            return; 
+        }
+
+        final String oldCommon = CldrUtility.getProperty("oldCommon",
+            "/Users/markdavis/Documents/workspace/cldr-archive/cldr-22.1/common");
+
+        // set up exceptions
+        Set<String> changedToEmpty = new HashSet<String>(Arrays.asList(new String[] { "version", "languageCoverage",
+            "scriptCoverage", "territoryCoverage", "currencyCoverage", "timezoneCoverage", "skipDefaultLocale" }));
+        Set<String> PCDATA = new HashSet<String>();
+        PCDATA.add("PCDATA");
+        Set<String> EMPTY = new HashSet<String>();
+        EMPTY.add("EMPTY");
+        Set<String> VERSION = new HashSet<String>();
+        VERSION.add("version");
+
+        // test all DTDs
+        for (DtdType dtd : DtdType.values()) {
+            try {
+                ElementAttributeInfo oldDtd = ElementAttributeInfo.getInstance(oldCommon, dtd);
+                ElementAttributeInfo newDtd = ElementAttributeInfo.getInstance(dtd);
+
+                if (oldDtd == newDtd) {
+                    continue;
+                }
+                Relation<String, String> oldElement2Children = oldDtd.getElement2Children();
+                Relation<String, String> newElement2Children = newDtd.getElement2Children();
+
+                Relation<String, String> oldElement2Attributes = oldDtd.getElement2Attributes();
+                Relation<String, String> newElement2Attributes = newDtd.getElement2Attributes();
+
+                for (String element : oldElement2Children.keySet()) {
+                    Set<String> oldChildren = oldElement2Children.getAll(element);
+                    Set<String> newChildren = newElement2Children.getAll(element);
+                    if (newChildren == null) {
+                        errln("Old " + dtd + " contains element not in new: <" + element + ">");
+                        continue;
+                    }
+                    Set<String> funny = containsInOrder(newChildren, oldChildren);
+                    if (funny != null) {
+                        if (changedToEmpty.contains(element) && oldChildren.equals(PCDATA) && newChildren.equals(EMPTY)) {
+                            // ok, skip
+                        } else {
+                            errln("Old " + dtd + " element <" + element + "> has children Missing/Misordered:\t"
+                                + funny
+                                + "\n\t\tOld:\t" + oldChildren + "\n\t\tNew:\t" + newChildren);
+                        }
+                    }
+
+                    Set<String> oldAttributes = oldElement2Attributes.getAll(element);
+                    if (oldAttributes == null) {
+                        oldAttributes = Collections.emptySet();
+                    }
+                    Set<String> newAttributes = newElement2Attributes.getAll(element);
+                    if (newAttributes == null) {
+                        newAttributes = Collections.emptySet();
+                    }
+                    if (!newAttributes.containsAll(oldAttributes)) {
+                        LinkedHashSet<String> missing = new LinkedHashSet<String>(oldAttributes);
+                        missing.removeAll(newAttributes);
+                        if (element.equals(dtd.toString()) && missing.equals(VERSION)) {
+                            // ok, skip
+                        } else {
+                            errln("Old " + dtd + " element <" + element + "> has attributes Missing:\t" + missing
+                                + "\n\t\tOld:\t" + oldAttributes + "\n\t\tNew:\t" + newAttributes);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                errln("Failure with " + dtd);
+            }
+        }
+    }
+
+    private <T> Set<T> containsInOrder(Set<T> superset, Set<T> subset) {
+        if (!superset.containsAll(subset)) {
+            LinkedHashSet<T> missing = new LinkedHashSet<T>(subset);
+            missing.removeAll(superset);
+            return missing;
+        }
+        // ok, we know that they are subsets, try order
+        Set<T> result = null;
+        DiscreteComparator<T> comp = new DiscreteComparator.Builder<T>(Ordering.ARBITRARY).add(superset).get();
+        T last = null;
+        for (T item : subset) {
+            if (last != null) {
+                int order = comp.compare(last, item);
+                if (order != -1) {
+                    if (result == null) {
+                        result = new HashSet<T>();
+                        result.add(last);
+                        result.add(item);
+                    }
+                }
+            }
+            last = item;
+        }
+        return result;
+    }
 
     public void TestDtdCompatibility() {
-        if (logKnownIssue("6827", "Need the directory before enabling this test")) {
+        if (logKnownIssue("Cldrbug:6903", "Need the directory before enabling this test")) {
             return; 
         }
         for (DtdType type : DtdType.values()) {
@@ -957,6 +1062,8 @@ public class TestBasic extends TestFmwkPlus {
                     case keyboard:
                     case platform:
                         tooOld = version.compareTo(Versions.v22_1) >= 0;
+                        break;
+                    default:
                         break;
                     }
                     if (tooOld) {

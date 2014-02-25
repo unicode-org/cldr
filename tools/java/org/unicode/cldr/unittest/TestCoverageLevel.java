d76733e15c53950b52dcb7e3a7c359724d461df7
John Emmons <emmo@us.ibm.com>
2014-02-25 03:17:07 +0000

    CLDR-6412 Unit test cleanup - change known issue to current bug number
    X-SVN-Rev: 9783

--- tools/java/org/unicode/cldr/unittest/TestCoverageLevel.java
+++ tools/java/org/unicode/cldr/unittest/TestCoverageLevel.java
@@ -358,7 +358,7 @@ public class TestCoverageLevel extends TestFmwk {
         );
 
     public void TestEnglishModern() {
-        if (logKnownIssue("Cldrbug:5712", "Finish enabling more comprehensive tests")) {
+        if (logKnownIssue("Cldrbug:6114", "Finish enabling more comprehensive tests")) {
             return;
         }
         SupplementalDataInfo sdi = testInfo.getSupplementalDataInfo();

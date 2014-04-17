4b649bbd946b7a3ad4b281209a76f4d6e02c300a
Steven R. Loomis <srl@icu-project.org>
2014-04-17 17:41:34 +0000

    CLDR-7239 check "cldr.home" for the parent of the /cldr directory before catalina.home.   Not using catalina.base because it's unfriendly to eclipse.
    X-SVN-Rev: 10109

--- tools/cldr-apps/src/org/unicode/cldr/util/CLDRConfigImpl.java
+++ tools/cldr-apps/src/org/unicode/cldr/util/CLDRConfigImpl.java
@@ -105,7 +105,7 @@ public class CLDRConfigImpl extends CLDRConfig implements JSONString {
         System.err.println(CLDRConfigImpl.class.getName() + ".init(), cldrHome=" + cldrHome);
         if (cldrHome == null) {
             String homeParent = null;
-            String props[] = { "catalina.home", "websphere.home", "user.dir" };
+            String props[] = { "cldr.home", "catalina.home", "catalina.base", "websphere.base", "websphere.home", "user.dir" };
             for (String prop : props) {
                 if (homeParent == null) {
                     homeParent = System.getProperty(prop);
@@ -118,7 +118,7 @@ public class CLDRConfigImpl extends CLDRConfig implements JSONString {
             }
             if (homeParent == null) {
                 throw new InternalError(
-                    "Could not find cldrHome. please set catalina.home, user.dir, etc, or set a servlet parameter cldr.home");
+                    "Could not find cldrHome. please set cldr.home, catalina.base, or user.dir, etc");
                 // for(Object qq : System.getProperties().keySet()) {
                 // SurveyLog.logger.warning("  >> "+qq+"="+System.getProperties().get(qq));
                 // }
@@ -130,7 +130,7 @@ public class CLDRConfigImpl extends CLDRConfig implements JSONString {
                 createBasicCldr(homeFile); // attempt to create
             }
             if (!homeFile.exists()) {
-                throw new InternalError("$(catalina.home)/cldr isn't working as a CLDR home. Not a directory: "
+                throw new InternalError("{SERVER}/cldr isn't working as a CLDR home. Not a directory: "
                     + homeFile.getAbsolutePath());
             }
             cldrHome = homeFile.getAbsolutePath();

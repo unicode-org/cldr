9b253060c12f034f8c04f2fe32a3a5a09a43b7f5
John Emmons <emmo@us.ibm.com>
2014-05-08 23:07:12 +0000

    CLDR-7348 Suppress same as English warnings for abbreviated/narrow quarters, duration unit patterns, etc.
    X-SVN-Rev: 10251

--- tools/java/org/unicode/cldr/test/CheckForCopy.java
+++ tools/java/org/unicode/cldr/test/CheckForCopy.java
@@ -40,19 +40,20 @@ public class CheckForCopy extends FactoryCheckCLDR {
             "|intervalFormatItem" +
             "|exemplarCharacters\\[@type=\"(currencySymbol|index)\"]" +
             "|scientificFormat" +
-            "|timeZoneNames/(hourFormat|gmtFormat)" +
+            "|timeZoneNames/(hourFormat|gmtFormat|gmtZeroFormat)" +
             "|dayPeriod" +
-            "|(monthWidth|dayWidth)\\[@type=\"(narrow|abbreviated)\"]" +
+            "|(monthWidth|dayWidth|quarterWidth)\\[@type=\"(narrow|abbreviated)\"]" +
             "|exemplarCity" +
             "|localeDisplayNames/(scripts|territories)" +
             "|currency\\[@type=\"[A-Z]+\"]/symbol" +
-            "|commonlyUsed" +
             "|pattern" +
             "|field\\[@type=\"dayperiod\"]" +
             "|defaultNumberingSystem" +
             "|otherNumberingSystems" +
             "|exemplarCharacters" +
+            "|durationUnitPattern" +
             "|unitLength\\[@type=\"(short|narrow)\"\\]/unit\\[@type=\"[^\"]++\"\\]/unitPattern\\[@count=\"[^\"]++\"\\]" +
+            "|unitLength\\[@type=\"(short|narrow)\"\\]/unit\\[@type=\"[^\"]++\"\\]/perUnitPattern" +
             ")", true)
         .add("^//ldml/dates/calendars/calendar\\[@type=\"gregorian\"]", false)
         .add("^//ldml/dates/calendars/calendar", true);

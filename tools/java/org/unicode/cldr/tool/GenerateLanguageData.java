f3c38bf388aee1b045f024e09e313a9c235b1a1c
Steven R. Loomis <srl@icu-project.org>
2014-07-29 01:09:58 +0000

    CLDR-7698 option to generate en.xml from ISO639Data
    X-SVN-Rev: 10684

--- tools/java/org/unicode/cldr/tool/GenerateLanguageData.java
+++ tools/java/org/unicode/cldr/tool/GenerateLanguageData.java
@@ -1,31 +1,92 @@
 package org.unicode.cldr.tool;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import org.unicode.cldr.icu.LDMLConstants;
+import org.unicode.cldr.util.CLDRFile;
+import org.unicode.cldr.util.CLDRFile.DraftStatus;
+import org.unicode.cldr.util.CLDRConfig;
 import org.unicode.cldr.util.CLDRPaths;
+import org.unicode.cldr.util.CLDRTool;
 import org.unicode.cldr.util.CldrUtility;
 import org.unicode.cldr.util.Iso639Data;
 import org.unicode.cldr.util.Iso639Data.Scope;
 import org.unicode.cldr.util.Iso639Data.Source;
 import org.unicode.cldr.util.Iso639Data.Type;
+import org.unicode.cldr.util.SimpleXMLSource;
 import org.unicode.cldr.util.StandardCodes;
 import org.unicode.cldr.util.SupplementalDataInfo;
+import org.unicode.cldr.util.XPathParts;
+import org.unicode.cldr.util.XPathParts.Comments.CommentType;
 
+import com.ibm.icu.dev.util.BagFormatter;
 import com.ibm.icu.dev.util.Relation;
+import com.ibm.icu.text.MessageFormat;
 
+@CLDRTool(alias="langdata",description="Generate a list of ISO639 language data. Use '--en' to build en.xml")
 public class GenerateLanguageData {
     // static StandardCodes sc = StandardCodes.make();
     static SupplementalDataInfo supplementalData = SupplementalDataInfo.getInstance(CLDRPaths.SUPPLEMENTAL_DIRECTORY);
     static Iso639Data iso639Data = new Iso639Data();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        MessageFormat nameFmt = new MessageFormat("{0,plural, one{# other name} other{# other names}}");
+        Set<String> bcp47languages = StandardCodes.make().getAvailableCodes("language");
+
+        if(args.length>0 && args[0].equals("--en")) {
+            CLDRConfig cldrConfig = CLDRConfig.getInstance();
+            XPathParts xpp = new XPathParts(null,null)
+                       .addElement(LDMLConstants.LDML)
+                       .addElement(LDMLConstants.LDN)
+                       .addElement(LDMLConstants.LANGUAGES)
+                       .addElement(LDMLConstants.LANGUAGE);
+            // could add draft status here, ex:
+            xpp.setAttribute(-1, LDMLConstants.DRAFT, DraftStatus.unconfirmed.toString());
+            System.out.println("generating en.xml.. to " + CLDRPaths.GEN_DIRECTORY);
+            SimpleXMLSource xmlSource = new SimpleXMLSource("en");
+            CLDRFile newEn = new CLDRFile(xmlSource);
+            CLDRFile oldEn = cldrConfig.getEnglish();
+            Set<String> all = Iso639Data.getAvailable();
+            newEn.addComment("//ldml", "by " +
+            GenerateLanguageData.class.getSimpleName() + 
+                " from Iso639Data v"+iso639Data.getVersion()+" on " + new java.util.Date() 
+                + " - "+all.size() +" codes.",
+                    CommentType.PREBLOCK);
+            System.out.println(all.size() + " ISO 639 codes to process");
+            for(String languageCode : all) {
+                xpp.setAttribute(-1, LDMLConstants.TYPE, languageCode);
+                String xpath = xpp.toString();
+
+                Set<String> names = Iso639Data.getNames(languageCode);
+
+                newEn.add(xpp.toString(), oldEn.getName(languageCode));
+                
+                String oldValue = oldEn.getStringValue(xpath);
+                
+                if ( oldValue != null &&
+                    !oldValue.equals(languageCode) ) {
+                    newEn.addComment(xpath,
+                                     "was already in en.xml",
+                                      CommentType.LINE);
+                }
+            }
+            final String filename = newEn.getLocaleID()+".xml";
+            try (PrintWriter w = 
+                    BagFormatter.openUTF8Writer(CLDRPaths.GEN_DIRECTORY, filename)) {
+                newEn.write(w);
+                System.out.println("Wrote to " + CLDRPaths.GEN_DIRECTORY +"/"+filename );
+            }
+            return;
+        }
+        
         // Set<String> languageRegistryCodes = sc.getAvailableCodes("language");
         Set<String> languageCodes = new TreeSet<String>(Iso639Data.getAvailable());
 
         System.out.println("Macrolanguages");
-        Set<String> bcp47languages = StandardCodes.make().getAvailableCodes("language");
         for (String languageCode : languageCodes) {
             Set<String> suffixes = Iso639Data.getEncompassedForMacro(languageCode);
             if (suffixes == null) continue;

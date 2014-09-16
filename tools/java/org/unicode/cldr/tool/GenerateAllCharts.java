d418a1c64725071502e93cc416b641cd9a787d04
Mark Davis <mark@macchiato.com>
2014-09-16 07:00:05 +0000

    CLDR-7172 mash the line endings for the java chart programs.
    X-SVN-Rev: 10987

--- tools/java/org/unicode/cldr/tool/GenerateAllCharts.java
+++ tools/java/org/unicode/cldr/tool/GenerateAllCharts.java
@@ -2,6 +2,7 @@ package org.unicode.cldr.tool;
 
 public class GenerateAllCharts {
     public static void main(String[] args) throws Exception {
+        System.setProperty("line.separator", "\n");
         ShowLanguages.main(args);
         GenerateBcp47Text.main(args);
         GenerateSidewaysView.main(args);

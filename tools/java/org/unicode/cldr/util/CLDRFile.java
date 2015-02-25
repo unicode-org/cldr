1f7b3f01777c6d09bce871f4d81fef8ad5633e07
John Emmons <emmo@us.ibm.com>
2015-02-25 04:33:02 +0000

    CLDR-8199 BRS Task A10 - Check on Version in DTDs and CLDRFile
    X-SVN-Rev: 11310

--- tools/java/org/unicode/cldr/util/CLDRFile.java
+++ tools/java/org/unicode/cldr/util/CLDRFile.java
@@ -1,6 +1,6 @@
 /*
  **********************************************************************
- * Copyright (c) 2002-2012, International Business Machines
+ * Copyright (c) 2002-2015, International Business Machines
  * Corporation and others.  All Rights Reserved.
  **********************************************************************
  * Author: Mark Davis

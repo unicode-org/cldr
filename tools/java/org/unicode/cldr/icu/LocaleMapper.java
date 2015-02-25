fe9484e50d0af870e039b7faf520d23a1fd6cca8
Peter Edberg <pedberg@unicode.org>
2015-02-25 21:42:10 +0000

    CLDR-8163 Per review, remove support and description for <contextTransform> value "∅∅∅"
    X-SVN-Rev: 11315

--- tools/java/org/unicode/cldr/icu/LocaleMapper.java
+++ tools/java/org/unicode/cldr/icu/LocaleMapper.java
@@ -41,7 +41,6 @@ public class LocaleMapper extends Mapper {
     private static final Map<String, String> enumMap = Builder.with(new HashMap<String, String>())
         .put("titlecase-firstword", "1")
         .put("no-change", "0")
-        .put("∅∅∅", "0")
         .freeze();
 
     private static final Pattern DRAFT_PATTERN = Pattern.compile("\\[@draft=\"\\w+\"]");

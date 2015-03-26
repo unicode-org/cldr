9c2bbdb712e3dde1a2bda40d85a61bee6cc5bf85
Mark Davis <mark@macchiato.com>
2015-03-26 13:09:54 +0000

    CLDR-8297 modify the source to put '*' after everything else. Also add test to catch bogus data, so that the resulting arrays are uniformly 4 items each.
    X-SVN-Rev: 11454

--- tools/cldr-unittest/src/org/unicode/cldr/unittest/LanguageInfoTest.java
+++ tools/cldr-unittest/src/org/unicode/cldr/unittest/LanguageInfoTest.java
@@ -1,11 +1,14 @@
 package org.unicode.cldr.unittest;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.unicode.cldr.unittest.TestAll.TestInfo;
+import org.unicode.cldr.util.Pair;
 
 import com.ibm.icu.dev.test.TestFmwk;
 import com.ibm.icu.impl.Row.R4;
@@ -41,13 +44,38 @@ public class LanguageInfoTest extends TestFmwk {
     //	}
 
     public void testGetData() {
+        Set<Pair<String,String>> alreadySeen = new HashSet<>();
         for (R4<String, String, Integer, Boolean> foo : testInfo.getSupplementalDataInfo().getLanguageMatcherData("written")) {
             //            assertTrue("check bounds", foo.get2() >= 0 && foo.get2() <= 100);
-            logln(foo.get2()
-                + "\t" + foo.get0() + "\t" + getName(foo.get0())
-                + "\t" + foo.get1() + "\t" + getName(foo.get1())
-                + "\t" + foo.get3());
+            
+            String desired = foo.get0();
+            String supported = foo.get1();
+            Integer score = foo.get2();
+            Boolean oneway = foo.get3();
+            assertEquals("Same number of fields", count('_', desired), count('_', supported));
+            
+            Pair<String,String> source = Pair.of(desired, supported);
+            if (alreadySeen.contains(source)) {
+                errln("Duplicate entry for " + source);
+                continue;
+            }
+            alreadySeen.add(source);
+            logln(score
+                + "\t" + desired + "\t" + getName(desired)
+                + "\t" + supported + "\t" + getName(supported)
+                + "\t" + oneway
+                );
         }
+    }
+
+    private int count(char c, String string) {
+        int count = 0;
+        int pos = string.indexOf(c);
+        while (pos >= 0) {
+            ++count;
+            pos = string.indexOf(c,pos+1);
+        }
+        return count;
     }
 
     public static String getName(String item) {

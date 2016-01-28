a708aaa36c0d51d52e234dbae7586e5c8f7e16a4
Sascha Brawer <sascha@brawer.ch>
2016-01-28 15:00:31 +0000

    CLDR-9212 Arabic transliteration from Amharic
    X-SVN-Rev: 12344

--- tools/java/org/unicode/cldr/util/CLDRTransforms.java
+++ tools/java/org/unicode/cldr/util/CLDRTransforms.java
@@ -145,6 +145,7 @@ public class CLDRTransforms {
             addDependency(".*Digit.*", "NumericPinyin-Pinyin", "Pinyin-NumericPinyin");
             addDependency("Latin-NumericPinyin(/.*)?", "Tone-Digit", "Digit-Tone");
             addDependency("NumericPinyin-Latin(/.*)?", "Tone-Digit", "Digit-Tone");
+            addDependency("am-ar", "am-am_FONIPA", "und_FONIPA-ar");
             addDependency("am-fa", "am-am_FONIPA", "und_FONIPA-fa");
             addDependency("cs-am", "cs-cs_FONIPA", "am-am_FONIPA");
             addDependency("cs-ar", "cs-cs_FONIPA", "und_FONIPA-ar");

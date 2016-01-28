1e80f45218cdcdca417542b8491124d97f26c41f
Sascha Brawer <sascha@brawer.ch>
2016-01-28 01:51:36 +0000

    CLDR-9214 Farsi transliteration of Romansh Sursilvan
    X-SVN-Rev: 12334

--- tools/java/org/unicode/cldr/util/CLDRTransforms.java
+++ tools/java/org/unicode/cldr/util/CLDRTransforms.java
@@ -175,6 +175,7 @@ public class CLDRTransforms {
             addDependency("pl-am", "pl-pl_FONIPA", "am-am_FONIPA");
             addDependency("pl-fa", "pl-pl_FONIPA", "und_FONIPA-fa");
             addDependency("pl-ja", "pl-pl_FONIPA", "pl_FONIPA-ja");
+            addDependency("rm_SURSILV-fa", "rm_SURSILV-rm_FONIPA_SURSILV", "und_FONIPA-fa");
             addDependency("ro-am", "ro-ro_FONIPA", "am-am_FONIPA");
             addDependency("ro-fa", "ro-ro_FONIPA", "und_FONIPA-fa");
             addDependency("ro-ja", "ro-ro_FONIPA", "ro_FONIPA-ja");

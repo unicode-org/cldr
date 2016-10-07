98ecebed190af037a79ed2b9f61b8803267c5313
Mark Davis <mark@macchiato.com>
2016-10-07 16:51:46 +0000

    CLDR-9849 fix versions
    X-SVN-Rev: 13040

--- tools/java/org/unicode/cldr/tool/ToolConstants.java
+++ tools/java/org/unicode/cldr/tool/ToolConstants.java
@@ -24,7 +24,7 @@ public class ToolConstants {
 
     // Change the following for each release depending on the phase
 
-    private static final String DEFAULT_CHART_VERSION = "30";
+    private static final String DEFAULT_CHART_VERSION = "31";
     private static final ChartStatus DEFAULT_CHART_STATUS = ChartStatus.beta;
 
     // DON'T CHANGE ANY OF THE FOLLOWING; THEY ARE DRIVEN BY THE ABOVE
@@ -60,8 +60,8 @@ public class ToolConstants {
         "26.0",
         "27.0",
         "28.0",
-        "29.0"
-        //"30.0"
+        "29.0",
+        "30.0"
         // add to this once the release is final!
         );
     public static final String PREVIOUS_CHART_VERSION;

edbf2d10d976b914ecbb851ad518c2de483eb771
Mark Davis <mark@macchiato.com>
2017-10-03 08:30:25 +0000

    CLDR-10610 checked in the fixed languageGroup.xml, tweaks to chart.
    X-SVN-Rev: 13755

--- tools/java/org/unicode/cldr/tool/ChartLanguageGroups.java
+++ tools/java/org/unicode/cldr/tool/ChartLanguageGroups.java
@@ -25,6 +25,10 @@ import com.ibm.icu.util.ULocale;
 
 public class ChartLanguageGroups extends Chart {
 
+    private static final String SHOULD_NOT_BE_LEAF_NODE = "🍂";
+    private static final String LEAF_NODES = "🍃";
+    private static final String TREE_NODES = "🌲";
+
     public static void main(String[] args) {
         new ChartLanguageGroups().writeChart(null);
     }
@@ -55,14 +59,24 @@ public class ChartLanguageGroups extends Chart {
 
     @Override
     public String getExplanation() {
-        return "<p>This chart shows language groups based on data extracted from wikidata. "
-            + "There are some patches to the wikidata, and only the wikidata containment for "
-            + "<a href='http://unicode.org/reports/tr35/#unicode_language_subtag'>valid language codes</a> is used.</p>\n"
+        return "<p>This chart shows draft language groups based on data extracted from wikidata. "
+            + "The <b>Status</b> cell indicates the nature of the items in the adjacent <b>Contained</b> cell:<p>"
             + "<ul>\n"
-            + "<li>A ʹ indicates a language collection (ISO 636 Scope).</li>\n"
-            + "<li>A * in the Name column indicates that the contained languages don't contain any themselves, "
-            + "and are listed in one cell, separated by ‘;’.</li>\n"
-            + "</ul>\n";
+            + "<li>A " + TREE_NODES
+            + " indicates that the contained languages are tree nodes (contain other languages or langauge groups), "
+            + "and will be listed further down in the chart in a <b>Language Group</b> cell.</li>\n"
+            + "<li>A " + LEAF_NODES
+            + " indicates that the contained languages are leaf nodes (contain nothing).</li>\n"
+            + "<li>A " + SHOULD_NOT_BE_LEAF_NODE
+            + " before an item <i>in</i> a <b>Contained</b> cell indicates a leaf node that shouldn’t be — that is, its ISO 639 Scope is "
+            + "<a href='http://www-01.sil.org/iso639-3/scope.asp#C' target='_blank'>Collection</a>.</li>\n"
+            + "</ul>\n"
+            + "<p><b>Caveats:</b> Only the wikidata containment for "
+            + "<a href='http://unicode.org/reports/tr35/#unicode_language_subtag'>valid language codes</a> is used."
+            + "The containment data is not complete: "
+            + "if a language doesn't appear in the chart it could be an isolate, or just be missing data."
+            + "The data doesn't completely match wikipedia’s; there are some patches for CLDR languages.</p>\n"
+            ;
     }
 
     Collator ENGLISH_ORDER = Collator.getInstance(ULocale.ENGLISH);
@@ -74,7 +88,9 @@ public class ChartLanguageGroups extends Chart {
 
         TablePrinter tablePrinter = new TablePrinter()
             .addColumn("Language Group", "class='source'", CldrUtility.getDoubleLinkMsg(), "class='source'", true)
+            .setBreakSpans(true)
             .addColumn("Name", "class='source'", null, "class='source'", true)
+            .addColumn("St.", "class='source'", null, "class='source'", true)
             .addColumn("Contained", "class='source'", null, "class='target'", true)
             .setBreakSpans(true);
 
@@ -101,30 +117,23 @@ public class ChartLanguageGroups extends Chart {
         for (String lang : children) {
             nameAndCode.add(Pair.of(getLangName(lang), lang));
         }
-        StringBuilder childrenString = new StringBuilder();
+        StringBuilder treeList = new StringBuilder();
+        StringBuilder leafList = new StringBuilder();
         LinkedHashSet<Pair<String,String>> nameAndCodeWithChildren = new LinkedHashSet<>();
         for (Pair<String, String> pair : nameAndCode) {
             String code = pair.getSecond();
             if (lg.containsKey(code)) {
+                addChildren(treeList, TREE_NODES, pair, false);
                 nameAndCodeWithChildren.add(pair);
-                tablePrinter.addRow()
-                .addCell(collectionPrime(parent))
-                .addCell(getLangName(parent))
-                .addCell(getPairName(pair))
-                .finishRow();
             } else if (!code.equals("und")){
-                if (childrenString.length() != 0) {
-                    childrenString.append("; ");
-                }
-                childrenString.append(getPairName(pair));
+                addChildren(leafList, LEAF_NODES, pair, true);
             }
         }
-        if (childrenString.length() != 0) {
-            tablePrinter.addRow()
-            .addCell(collectionPrime(parent))
-            .addCell(getLangName(parent) + "*")
-            .addCell(childrenString.toString())
-            .finishRow();
+        if (treeList.length() != 0) {
+            addRow(parent, tablePrinter, TREE_NODES, treeList);
+        }
+        if (leafList.length() != 0) {
+            addRow(parent, tablePrinter, LEAF_NODES, leafList);
         }
 
         for (Pair<String, String> pair : nameAndCodeWithChildren) {
@@ -132,19 +141,32 @@ public class ChartLanguageGroups extends Chart {
         }
     }
 
-    private String getPairName(Pair<String, String> pair) {
-        return collectionPrime(pair.getSecond()) + " “" + pair.getFirst() + "”";
+    private void addRow(String parent, TablePrinter tablePrinter, String marker, StringBuilder treeList) {
+        tablePrinter.addRow()
+        .addCell(parent)
+        .addCell(getLangName(parent))
+        .addCell(marker)
+        .addCell(treeList.toString())
+        .finishRow();
+    }
+
+    private void addChildren(StringBuilder treeList, String marker, Pair<String, String> pair, boolean showCollections) {
+        if (treeList.length() != 0) {
+            treeList.append("; ");
+        }
+        treeList.append(getPairName(pair, showCollections));
+    }
+
+    private String getPairName(Pair<String, String> pair, boolean showCollection) {
+        return (showCollection && COLLECTIONS.contains(pair.getSecond()) 
+            ? SHOULD_NOT_BE_LEAF_NODE + " ": 
+            "")
+            + pair.getSecond() + " “" + pair.getFirst() + "”";
     }
 
     private String getLangName(String langCode) {
-        return langCode.equals("mul") 
-            ? "All" 
-            : ENGLISH.getName(CLDRFile.LANGUAGE_NAME, langCode).replace(" (Other)", "").replace(" languages", "");
-    }
-
-    private String collectionPrime(String langCode) {
-        return langCode + (COLLECTIONS.contains(langCode) 
-            ? "ʹ" : 
-                "");
+        return langCode.equals("mul") ? "All" 
+            : langCode.equals("zh") ? "Mandarin Chinese"
+                : ENGLISH.getName(CLDRFile.LANGUAGE_NAME, langCode).replace(" (Other)", "").replace(" languages", "");
     }
 }

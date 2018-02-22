9b924633736285f7e592628aa29b13caf8f06a28
Mark Davis <mark@macchiato.com>
2018-02-22 14:47:18 +0000

    CLDR-10939 removing empty files
    X-SVN-Rev: 13931

--- tools/java/org/unicode/cldr/tool/RemoveEmptyCLDR.java
+++ tools/java/org/unicode/cldr/tool/RemoveEmptyCLDR.java
@@ -5,25 +5,35 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.unicode.cldr.util.CLDRPaths;
 import org.unicode.cldr.util.Pair;
 import org.unicode.cldr.util.XMLFileReader;
 
 public class RemoveEmptyCLDR {
     public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            args = new String[]{"annotations", "annotationsDerived"};
+        }
         // eg /Users/markdavis/Google Drive/workspace/Generated/vxml/common/annotations
-        String dir = args[0];
-        main:
-            for (File f : new File(dir).listFiles()) {
-                List<Pair<String, String>> data = new ArrayList<>();
-                String canonicalPath = f.getCanonicalPath();
-                XMLFileReader.loadPathValues(canonicalPath, data, false);
-                for (Pair<String, String> item : data) {
-                    if (item.getFirst().contains("/identity")) {
+        for (String dir : args) {
+            main:
+                for (File f : new File(CLDRPaths.COMMON_DIRECTORY + dir).listFiles()) {
+                    List<Pair<String, String>> data = new ArrayList<>();
+                    String canonicalPath = f.getCanonicalPath();
+                    if (!canonicalPath.endsWith(".xml")) {
                         continue;
                     }
-                    continue main;
+                    XMLFileReader.loadPathValues(canonicalPath, data, false);
+                    for (Pair<String, String> item : data) {
+                        if (item.getFirst().contains("/identity")) {
+                            continue;
+                        }
+                        System.out.println("Skipping: " + canonicalPath);
+                        continue main;
+                    }
+                    System.out.println("Deleting: " + canonicalPath);
+                    f.delete();
                 }
-                f.delete();
-            }
+        }
     }
 }

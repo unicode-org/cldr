4df27fc9ad58860b385ee95ffca0f234771403cd
John Emmons <emmo@us.ibm.com>
2018-04-09 17:55:06 +0000

    CLDR-11039 SBRS Step S5 - Update ICU4J/Guava/Gson to latest ( also fixed a problem in ScriptMetadata that prevented the tests from running ).
    X-SVN-Rev: 14002

--- tools/java/org/unicode/cldr/draft/ScriptMetadata.java
+++ tools/java/org/unicode/cldr/draft/ScriptMetadata.java
@@ -30,7 +30,7 @@ import com.ibm.icu.util.VersionInfo;
 public class ScriptMetadata {
     private static final int MAX_RANK = 33;
     private static final String DATA_FILE = "/org/unicode/cldr/util/data/Script_Metadata.csv";
-    private static final VersionInfo UNICODE_VERSION = VersionInfo.getInstance(CldrUtility.getProperty("SCRIPT_UNICODE_VERSION", "10"));
+    private static final VersionInfo UNICODE_VERSION = VersionInfo.getInstance(CldrUtility.getProperty("SCRIPT_UNICODE_VERSION", "11"));
 
     // To get the data, go do the Script MetaData spreadsheet
     // Download As Comma Separated Items into DATA_FILE

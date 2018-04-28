ae434f2decc801905e37b1d3f14b3f3c881d67a0
Tom Bishop <btangmu@users.noreply.github.com>
2018-04-28 14:26:25 +0000

    CLDR-11056 unit test, STFactory
    X-SVN-Rev: 14032

--- tools/cldr-apps/src/org/unicode/cldr/unittest/web/TestImportOldVotes.java
+++ tools/cldr-apps/src/org/unicode/cldr/unittest/web/TestImportOldVotes.java
@@ -1,16 +1,36 @@
 package org.unicode.cldr.unittest.web;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import org.unicode.cldr.unittest.web.TestAll.WebTestInfo;
+import org.unicode.cldr.util.CLDRPaths;
+import org.unicode.cldr.web.CookieSession;
+import org.unicode.cldr.web.DBUtils;
+import org.unicode.cldr.web.STFactory;
 import org.unicode.cldr.web.SurveyAjax;
 import org.unicode.cldr.web.SurveyAjax.JSONWriter;
+import org.unicode.cldr.web.SurveyLog;
 import org.unicode.cldr.web.SurveyMain;
+import org.unicode.cldr.web.UserRegistry;
 import org.unicode.cldr.web.UserRegistry.User;
+import org.unicode.cldr.web.XPathTable;
 
 import com.ibm.icu.dev.test.TestFmwk;
+import com.ibm.icu.dev.util.ElapsedTimer;
 
 public class TestImportOldVotes extends TestFmwk {
-  
+
+    TestAll.WebTestInfo testInfo = WebTestInfo.getInstance();
+
+    STFactory gFac = null;
+    UserRegistry.User gUser = null;
+
     public static void main(String[] args) {
-        new TestImportOldVotes().run(args);
+        // new TestImportOldVotes().run(args);
+        new TestImportOldVotes().run(TestAll.doResetDb(args));
     }
 
     /**
@@ -26,44 +46,51 @@ public class TestImportOldVotes extends TestFmwk {
         boolean isSubmit = false;
         JSONWriter r = null;
 
-        /* Confirm we can create a new SurveyAjax. */
-        try {
-            sa = new SurveyAjax();
-        } catch (Exception e) {
-            errln("new SurveyAjax threw unexpected exception: " + e.getMessage() + "\n");
-            return;
-        }
-
-        /* Confirm importOldVotes throws NullPointerException if JSONWriter and SurveyMain are null. */
-        boolean gotNullPointerException = false;
-        try {
-            sa.importOldVotes(r, user, sm, isSubmit, val, loc);
-        } catch (Exception e) {
-            if (e instanceof NullPointerException) {
-                // This is expected, no errln
-                gotNullPointerException = true;
+        if (false) {
+            /* Confirm we can create a new SurveyAjax. */
+            try {
+                sa = new SurveyAjax();
+            } catch (Exception e) {
+                errln("new SurveyAjax threw unexpected exception: " + e.getMessage() + "\n");
+                return;
             }
-            else {
-                errln("importOldVotes with sm null, expected NullPointerException, got exception: "
-                    + e.toString() + " - " + e.getMessage() + "\n");
-                // e.printStackTrace();
+    
+            /* Confirm importOldVotes throws NullPointerException if JSONWriter and SurveyMain are null. */
+            boolean gotNullPointerException = false;
+            try {
+                sa.importOldVotes(r, user, sm, isSubmit, val, loc);
+            } catch (Exception e) {
+                if (e instanceof NullPointerException) {
+                    // This is expected, no errln
+                    gotNullPointerException = true;
+                }
+                else {
+                    errln("importOldVotes with sm null, expected NullPointerException, got exception: "
+                        + e.toString() + " - " + e.getMessage() + "\n");
+                    // e.printStackTrace();
+                    return;
+                }
+            }
+            if (!gotNullPointerException) {
+                errln("importOldVotes with sm null, expected NullPointerException, got no exception\n");
                 return;
             }
         }
-        if (!gotNullPointerException) {
-            errln("importOldVotes with sm null, expected NullPointerException, got no exception\n");
-            return;
-        }
 
-        /* Confirm we can create a new SurveyMain.
-         * TODO: consider using getFactory().sm instead.
-         */
+        /* Confirm we can create STFactory and SurveyMain. */
+        STFactory fac = null;
         try {
+            fac = getFactory();
+            sm = fac.sm;
             // SurveyMain.cldrHome = SurveyMain.getHome() + "/cldr";
             // SurveyMain.vap = "testingvap";
-            sm = new SurveyMain();
+            // sm = new SurveyMain();
         } catch (Exception e) {
-            errln("new SurveyMain threw unexpected exception: " + e.toString() + " - " + e.getMessage() + "\n");
+            errln("getFactory or fac.sm threw unexpected exception: " + e.toString() + " - " + e.getMessage() + "\n");
+            return;
+        }
+        if (fac == null || sm == null) {
+            errln("null fac or sm");
             return;
         }
 
@@ -73,6 +100,7 @@ public class TestImportOldVotes extends TestFmwk {
          *  "NOTE:  not inside of web process, using temporary CLDRHOME /Users/tbishop/Documents/WenlinDocs/Organizations/Unicode/CLDR_job/cldr/tools/cldr-apps/testing_cldr_home"
          *  Also we get NullPointerException for this line in SurveyMain.java doStartup:
          *      dbUtils.setupDBProperties(this, survprops);
+         *      -- because dbUtils is null!!
          */
         try {
             // CLDRConfig config = CLDRConfig.getInstance();
@@ -81,11 +109,19 @@ public class TestImportOldVotes extends TestFmwk {
             // sm.init(config);
             // SurveyMain.cldrHome = SurveyMain.getHome() + "/cldr";
             // SurveyMain.vap = "testingvap";
-            sm.init();
+            // sm.init();
             sm.doStartup();
             // sm.doStartupDB();
         } catch (Exception e) {
-            errln("sm.init or doStartup threw unexpected exception: " + e.toString() + " - " + e.getMessage() + "\n");
+            errln("doStartup threw unexpected exception: " + e.toString() + " - " + e.getMessage() + "\n");
+            return;
+        }
+
+        /* Confirm we can create a new SurveyAjax. */
+        try {
+            sa = new SurveyAjax();
+        } catch (Exception e) {
+            errln("new SurveyAjax threw unexpected exception: " + e.getMessage() + "\n");
             return;
         }
 
@@ -161,5 +197,67 @@ public class TestImportOldVotes extends TestFmwk {
         // submitOldVotes(user, sm, locale, val, oldVotesTable, newVotesTable, oldvotes, fac);
 
         errln("importOldVotes test reached end, but test is still incomplete.\n");
+    }
+    
+    /**
+     * Get an STFactory for testing.
+     * This function is based on one in TestTFactory.java.
+     * @return the STFactory
+     */
+   private STFactory getFactory() throws SQLException {
+        if (gFac == null) {
+            long start = System.currentTimeMillis();
+            TestAll.setupTestDb();
+            logln("Set up test DB: " + ElapsedTimer.elapsedTime(start));
+
+            ElapsedTimer et0 = new ElapsedTimer("clearing directory");
+            //File cacheDir = TestAll.getEmptyDir(CACHETEST);
+            logln(et0.toString());
+
+            et0 = new ElapsedTimer("setup SurveyMain");
+            SurveyMain sm = new SurveyMain();
+            CookieSession.sm = sm; // hack - of course.
+            logln(et0.toString());
+
+            SurveyMain.fileBase = CLDRPaths.MAIN_DIRECTORY;
+            SurveyMain.fileBaseSeed = new File(CLDRPaths.BASE_DIRECTORY, "seed/main/").getAbsolutePath();
+            SurveyMain.fileBaseA = new File(CLDRPaths.BASE_DIRECTORY, "common/annotations/").getAbsolutePath();
+            SurveyMain.fileBaseASeed = new File(CLDRPaths.BASE_DIRECTORY, "seed/annotations/").getAbsolutePath();
+
+            SurveyMain.setFileBaseOld(CLDRPaths.BASE_DIRECTORY);
+            // sm.twidPut(Vetting.TWID_VET_VERBOSE, true); // set verbose
+            // vetting
+            SurveyLog.logger = Logger.getAnonymousLogger();
+
+            et0 = new ElapsedTimer("setup DB");
+            Connection conn = DBUtils.getInstance().getDBConnection();
+            logln(et0.toString());
+
+            et0 = new ElapsedTimer("setup Registry");
+            sm.reg = UserRegistry.createRegistry(SurveyLog.logger, sm);
+            logln(et0.toString());
+
+            et0 = new ElapsedTimer("setup XPT");
+            sm.xpt = XPathTable.createTable(conn, sm);
+            sm.xpt.getByXpath("//foo/bar[@type='baz']");
+            logln(et0.toString());
+            et0 = new ElapsedTimer("close connection");
+            DBUtils.closeDBConnection(conn);
+            logln(et0.toString());
+            // sm.vet = Vetting.createTable(sm.logger, sm);
+
+            // CLDRDBSourceFactory fac = new CLDRDBSourceFactory(sm,
+            // sm.fileBase, Logger.getAnonymousLogger(), cacheDir);
+            // logln("Setting up DB");
+            // sm.setDBSourceFactory(fac);ignore
+            // fac.setupDB(DBUtils.getInstance().getDBConnection());
+            // logln("Vetter Ready (this will take a while..)");
+            // fac.vetterReady(TestAll.getProgressIndicator(this));
+
+            et0 = new ElapsedTimer("Set up STFactory");
+            gFac = sm.getSTFactory();
+            logln(et0.toString());
+        }
+        return gFac;
     }
 }

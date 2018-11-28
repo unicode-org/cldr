ab7fb4c4ddd42c7bd1516a1823afd0b07960e987
Peter Edberg <pedberg@unicode.org>
2018-11-28 03:44:55 +0000

    CLDR-11484 Add test for invalid unit patterns
    X-SVN-Rev: 14700

--- tools/java/org/unicode/cldr/test/CheckUnits.java
+++ tools/java/org/unicode/cldr/test/CheckUnits.java
@@ -7,6 +7,8 @@ import org.unicode.cldr.test.CheckCLDR.CheckStatus.Subtype;
 import org.unicode.cldr.util.PatternCache;
 import org.unicode.cldr.util.XPathParts;
 
+import com.ibm.icu.text.SimpleFormatter;
+
 public class CheckUnits extends CheckCLDR {
     private static final Pattern HOUR_SYMBOL = PatternCache.get("h{1,2}");
     private static final Pattern MINUTE_SYMBOL = PatternCache.get("m{1,2}");
@@ -16,6 +18,36 @@ public class CheckUnits extends CheckCLDR {
     @Override
     public CheckCLDR handleCheck(String path, String fullPath, String value, Options options,
         List<CheckStatus> result) {
+
+        if (!path.startsWith("//ldml/units")) {
+            return this;
+        }
+        // Note, the following test has some overlaps with the checkAndReplacePlaceholders
+        // test in CheckForExamplars (why there?). That is probably OK, they check in
+        // different ways, but some errors will produce two somewhat different error messages.
+        if (path.startsWith("//ldml/units/unitLength")) {
+            int min = 0;
+            int max = 0;
+            if (path.contains("compoundUnitPattern")) {
+                min = 2;
+                max = 2;
+            } else if (path.contains("perUnitPattern") || path.contains("coordinateUnitPattern")) {
+                min = 1;
+                max = 1;
+            } else if (path.contains("unitPattern")) {
+                min = 0;
+                max = 1;
+            } // else we have displayName, with min/max = 0
+            if (max > 0) {
+                try {
+                    SimpleFormatter sf = SimpleFormatter.compileMinMaxArguments(value, min, max);
+                } catch (Exception e) {
+                    result.add(new CheckStatus().setCause(this).setMainType(CheckStatus.errorType)
+                        .setSubtype(Subtype.invalidPlaceHolder)
+                        .setMessage("Invalid unit pattern, must have min " + min + " and max " + max + " distinct placeholders of the form {n}"));
+                }
+            }
+        }
 
         if (!path.contains("durationUnitPattern")) {
             return this;

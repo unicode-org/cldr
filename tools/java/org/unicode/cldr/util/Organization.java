c5a2bef0d0302540c36d9c5779789e6e07cb2a35
Mark Davis <mark@macchiato.com>
2019-05-30 09:33:39 +0200

    CLDR-13061 Add orgs, cleanup enum (#38)
    CLDR-13061 Fix typo

--- tools/java/org/unicode/cldr/util/Organization.java
+++ tools/java/org/unicode/cldr/util/Organization.java
@@ -9,16 +9,46 @@ import java.util.Map;
  */
 public enum Organization {
     // Please update Locales.txt for default coverage when adding an organization here.
-
-    adlam("Winden Jangen Adlam"), adobe("Adobe"), afghan_csa("Afghan CSA"), afghan_mcit("Afghan MCIT"), afrigen("Afrigen"), apple("Apple"), bangladesh(
-        "Bangladesh Computer Council"), bangor_univ("Bangor Univ."), bhutan("Bhutan DDC"), breton("Office of Breton Lang"), cherokee("Cherokee Nation"), cldr(
-            "Cldr"), facebook("Facebook"), gaeilge("Foras na Gaeilge"), georgia_isi("Georgia ISI"), gnome("Gnome Foundation"), google(
-                "Google"), guest("Guest (Unicode)"), ibm("IBM"), india("India MIT"), iran_hci("Iran HCI"), kendra("Kendra (Nepal)"), kotoistus(
-                    "Kotoistus (Finnish IT Ctr)"), lakota_lc("Lakota LC"), lao_dpt("Lao Posts/Telecom??"), longnow("The Long Now Foundation", "Long Now",
-                        "PanLex"), microsoft("Microsoft"), mozilla("Mozilla"), netflix("Netflix"), openinstitute("Open Inst (Cambodia)"), openoffice_org(
-                            "Open Office"), oracle("Oracle", "sun", "Sun Micro"), pakistan("Pakistan"), rumantscha("Lia Rumantscha"), sil("SIL"), srilanka(
-                                "Sri Lanka ICTA",
-                                "Sri Lanka"), surveytool("Survey Tool"), welsh_lc("Welsh LC"), wikimedia("Wikimedia Foundation"), yahoo("Yahoo"),
+    adlam("Winden Jangen Adlam"),
+    adobe("Adobe"),
+    afghan_csa("Afghan CSA"),
+    afghan_mcit("Afghan MCIT"),
+    afrigen("Afrigen"),
+    apple("Apple"),
+    bangladesh("Bangladesh", "Bangladesh Computer Council"),
+    bangor_univ("Bangor Univ."),
+    bhutan("Bhutan DDC"),
+    breton("Office of Breton Lang"),
+    cherokee("Cherokee Nation"),
+    cldr("Cldr"),
+    facebook("Facebook"),
+    gaeilge("Foras na Gaeilge"),
+    georgia_isi("Georgia ISI"),
+    gnome("Gnome Foundation"),
+    google("Google"),
+    guest("Guest (Unicode)"),
+    ibm("IBM"),
+    india("India MIT"),
+    iran_hci("Iran HCI"),
+    kendra("Kendra (Nepal)"),
+    kotoistus("Kotoistus (Finnish IT Ctr)"),
+    lakota_lc("Lakota LC"),
+    lao_dpt("Lao Posts/Telecom??"),
+    longnow("The Long Now Foundation", "Long Now","PanLex"),
+    microsoft("Microsoft"),
+    mozilla("Mozilla"),
+    netflix("Netflix"),
+    openinstitute("Open Inst (Cambodia)"),
+    openoffice_org("Open Office"),
+    oracle("Oracle", "sun", "Sun Micro"),
+    pakistan("Pakistan"),
+    rumantscha("Lia Rumantscha"),
+    sil("SIL", "SIL International"),
+    srilanka("Sri Lanka ICTA", "Sri Lanka"),
+    surveytool("Survey Tool"),
+    welsh_lc("Welsh LC"),
+    wikimedia("Wikimedia Foundation"),
+    yahoo("Yahoo"),
     // To be removed.
     ;
 

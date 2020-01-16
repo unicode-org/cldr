19157af0ed990690552bc13c9fda5de41033cbb2
btangmu <thomasbishop@wenlin.com>
2020-01-16 14:40:41 -0500

    CLDR-11677 Permanent TC votes
    -New class and source file PermanentVote.java
    
    -Change voteReduced to voteLevelChanged
    
    -Change reducedLevelVote to an array voteCountMenu
    
    -Change canVoteAtReducedLevel to new getVoteCountMenu and canVoteWithCount
    
    -New class VoteResolver.VC with constants for vote counts
    
    -VC.PERMANENT = 1000, VC.LOCKING = 2000
    
    -Related changes to VoteResolver.Level
    
    -New database table LOCKED_XPATHS (cldr_locked_xpaths)
    
    -FORBID_PERMANENT_WITHOUT_FORUM; E_PERMANENT_VOTE_NO_FORUM; message valueIsLocked
    
    -New boolean VoteResolver.valueIslocked, included in json as vr.valueIslocked
    
    -New methods VoteResolver.isValueLlocked, canFlagOnLosing
    
    -Extend SurveyForum.couldFlagOnLosing to enable flagging when locked
    
    -Fix leak warning in makeProposedFile with DBUtils.close(ps2)
    
    -Remove unused votecount_alt from json
    
    -Remove unused timeValue from VoteResolver.MaxCounter.add
    
    -Remove unused param voter from OrganizationToValueAndVote.addInternal
    
    -Remove unused locale from SELECT in openQueryByLocaleRW (old TODO)
    
    -Remove unused isEstablished, and other dead code, fixing some warnings
    
    -Comments

--- tools/java/org/unicode/cldr/test/CheckCLDR.java
+++ tools/java/org/unicode/cldr/test/CheckCLDR.java
@@ -69,7 +69,7 @@ import com.ibm.icu.util.ICUUncheckedIOException;
  */
 abstract public class CheckCLDR {
 
-    public static final boolean LIMITED_SUBMISSION = false; // TODO represent differently
+    public static final boolean LIMITED_SUBMISSION = true; // TODO represent differently
 
     private static CLDRFile displayInformation;
 

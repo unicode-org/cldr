ee7e3f1635f8453fe98f8e8bb3bc88391c8daee5
pedberg-icu <42151464+pedberg-icu@users.noreply.github.com>
2020-05-19 10:08:57 -0700

    CLDR-13781 update CLDR version, readme; update ICU4J libs to release-67-1 (#468)

--- tools/java/org/unicode/cldr/util/CLDRFile.java
+++ tools/java/org/unicode/cldr/util/CLDRFile.java
@@ -128,7 +128,7 @@ public class CLDRFile implements Freezable<CLDRFile>, Iterable<String> {
     public static final String SUPPLEMENTAL_NAME = "supplementalData";
     public static final String SUPPLEMENTAL_METADATA = "supplementalMetadata";
     public static final String SUPPLEMENTAL_PREFIX = "supplemental";
-    public static final String GEN_VERSION = "37";
+    public static final String GEN_VERSION = "38";
     public static final List<String> SUPPLEMENTAL_NAMES = Arrays.asList("characters", "coverageLevels", "dayPeriods", "genderList", "grammaticalFeatures", "languageInfo",
         "languageGroup", "likelySubtags", "metaZones", "numberingSystems", "ordinals", "plurals", "postalCodeData", "rgScope", "supplementalData",
         "supplementalMetadata", "telephoneCodeData", "units", "windowsZones");

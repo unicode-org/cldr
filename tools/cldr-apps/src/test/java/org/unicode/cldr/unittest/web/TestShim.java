4e9198e8783ac8557584d96602161844ff0c595a
Steven R. Loomis <srl295@gmail.com>
2020-09-28 16:22:22 -0500

    CLDR-11585 improve maven tests (#741)
    - You can run individual tests using testArgs properties.
    
    Example to run only one test from the main unit tests:
    
        mvn test '-Dorg.unicode.cldr.unittest.testArgs=-f:TestUntimedCounter -n -q'
    
    Example to run only one test from web: (could be combined w/ prior example)
    
        mvn test '-Dorg.unicode.cldr.unittest.web.testArgs=-f:TestMisc -n -q'
    
    (cherry picked from commit 8b6d27416abc7020534ef5463a7bec668fb0829b)

--- tools/cldr-apps/src/test/java/org/unicode/cldr/unittest/web/TestShim.java
+++ tools/cldr-apps/src/test/java/org/unicode/cldr/unittest/web/TestShim.java
@@ -4,13 +4,15 @@ import java.io.PrintWriter;
 
 import org.junit.jupiter.api.Test;
 
+import org.unicode.cldr.util.TestShimUtils;
+
 /**
  * a JUnit test that calls TestAll.
  */
 class TestShim {
     @Test
     public void TestAll() {
-        String args[] = { "-q" };
+        String args[] = TestShimUtils.getArgs(TestShim.class, "-n -q");
         // regular main() will System.exit() which is not too friendly.
         // call this instead.
         TestAll.main(args, new PrintWriter(System.out)); // TODO: parameterize
--- tools/cldr-unittest/src/org/unicode/cldr/unittest/TestShim.java
+++ tools/cldr-unittest/src/org/unicode/cldr/unittest/TestShim.java
@@ -2,8 +2,11 @@ package org.unicode.cldr.unittest;
 
 import java.io.PrintWriter;
 
+import org.unicode.cldr.util.TestShimUtils;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
  * a JUnit test that calls TestAll.
@@ -11,10 +14,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class TestShim {
     @Test
     public void TestAll() {
-        String args[] = { "-n", "-q" }; // TODO: parameterize
+        String args[] = TestShimUtils.getArgs(TestShim.class, "-n -q");
         // regular main() will System.exit() which is not too friendly.
         // call this instead.
         int errCount = TestAll.runTests(args);
         assertEquals(errCount, 0, "Test had errors");
+    }
+
+    @Test
+    public void TestTestShimUtilTest() {
+        // Note: checks the system property corresponding to java.lang,
+        // we expect this to be unset!
+        String args[] = TestShimUtils.getArgs(java.lang.String.class, "-a -b -c");
+        String expectArgs[] = { "-a", "-b", "-c" };
+        assertArrayEquals(args, expectArgs, "Expected arg parse");
     }
 }

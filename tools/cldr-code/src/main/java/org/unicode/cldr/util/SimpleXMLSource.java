5e45110d9f914757991d720334428ca04f69d6ff
Mark Davis <mark@macchiato.com>
2021-01-28 12:01:10 -0800

    CLDR-11581 Allow U+202F to distinguish collisions

--- tools/cldr-code/src/main/java/org/unicode/cldr/util/SimpleXMLSource.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/SimpleXMLSource.java
@@ -168,9 +168,14 @@ public class SimpleXMLSource extends XMLSource {
     static final Normalizer2 NFKC = Normalizer2.getNFKCInstance();
 
     // The following includes letters, marks, numbers, currencies, and *selected* symbols/punctuation
-    static final UnicodeSet NON_ALPHANUM = new UnicodeSet("[^[:L:][:M:][:N:][:Sc:]/+\\-°′″%‰‱٪؉−⍰()⊕☉]").freeze();
+    static final UnicodeSet NON_ALPHANUM = new UnicodeSet("[^[:L:][:M:][:N:][:Sc:][\\u00A0\\u202F\\uFFFF _ ¡ « ( ) \\[ \\] \\{ \\} § / \\\\ % ٪ ‰ ؉ ‱-″ ` \\^ ¯ ¨ ° + ¬ | ¦ ~ − ⊕ ⍰ ☉ © ®]]").freeze();
 
     public static String normalize(String valueToMatch) {
+        if (valueToMatch.indexOf('\u202F') >= 0) { // special hack to allow \u202f, which is otherwise removed by NFKC
+            String temp = valueToMatch.replace('\u202F', '\uFFFF');
+            String result = replace(NON_ALPHANUM, NFKCCF.normalize(temp), "");
+            return result.replace('\uFFFF','\u202F');
+        }
         return replace(NON_ALPHANUM, NFKCCF.normalize(valueToMatch), "");
     }
 

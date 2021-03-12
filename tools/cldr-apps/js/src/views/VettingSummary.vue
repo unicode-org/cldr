41d51d241bb0e473b693542c536c552ff915cc9b
btangmu <thomasbishop@wenlin.com>
2021-03-12 13:54:40 -0500

    CLDR-14590 Formatting
    -Revise pretty.sh to run npx prettier on all .vue files
    
    -Resulting formatting changes

--- tools/cldr-apps/js/src/views/VettingSummary.vue
+++ tools/cldr-apps/js/src/views/VettingSummary.vue
@@ -1,90 +1,87 @@
 <template>
-    <div id="home">
-        Current Status Code: {{ statusData.status }}
-        <button v-on:click="stop()">Stop</button>
-        <button v-on:click="start()">Start</button>
-        <br>
-        Current Status Message:
-            <span v-html="statusData.ret"></span>
-        <br>
-        Results:
-            <span v-html="statusData.output"></span>
-    </div>
+  <div id="home">
+    Current Status Code: {{ statusData.status }}
+    <button v-on:click="stop()">Stop</button>
+    <button v-on:click="start()">Start</button>
+    <br />
+    Current Status Message:
+    <span v-html="statusData.ret"></span>
+    <br />
+    Results:
+    <span v-html="statusData.output"></span>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ["specialPage", "cldrOpts"],
-    data: function() {
-        return {
-            statusData: {
-                wait: "Loading…"
-            }
-        }
+  props: ["specialPage", "cldrOpts"],
+  data: function () {
+    return {
+      statusData: {
+        wait: "Loading…",
+      },
+    };
+  },
+  created: function () {
+    this.fetchStatus();
+  },
+  methods: {
+    fetchStatus: function () {
+      const SECONDS_IN_MS = 1000;
+      // timeouts
+      const RETRY_ON_FETCH_ERR = 15 * SECONDS_IN_MS; // When we couldn't talk to server
+      const NORMAL_RETRY = 5 * SECONDS_IN_MS; // "Normal" retry: starting or about to start
+      const BUSTED_RETRY = 60 * SECONDS_IN_MS; // ST is down
+      const sessionId = this.cldrOpts.cldrStatus.getSessionId();
+      fetch(`api/summary?ss=${sessionId}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          loadingPolicy: "NOSTART",
+        }),
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          this.statusData = data;
+          window.setTimeout(this.fetchStatus.bind(this), NORMAL_RETRY);
+        });
     },
-    created: function() {
-        this.fetchStatus();
+    stop: function () {
+      const sessionId = this.cldrOpts.cldrStatus.getSessionId();
+      fetch(`api/summary?ss=${sessionId}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          loadingPolicy: "FORCESTOP",
+        }),
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          this.statusData = data;
+        });
     },
-    methods: {
-        fetchStatus: function() {
-        const SECONDS_IN_MS = 1000;
-        // timeouts
-        const RETRY_ON_FETCH_ERR = 15 * SECONDS_IN_MS; // When we couldn't talk to server
-        const NORMAL_RETRY       = 5 * SECONDS_IN_MS;  // "Normal" retry: starting or about to start
-        const BUSTED_RETRY       = 60 * SECONDS_IN_MS; // ST is down
-        const sessionId = this.cldrOpts.cldrStatus.getSessionId();
-            fetch(`api/summary?ss=${sessionId}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    loadingPolicy: 'NOSTART'
-                })
-            })
-            .then(r => r.json())
-            .then(data => {
-                this.statusData = data;
-                window.setTimeout(this.fetchStatus.bind(this), NORMAL_RETRY);
-            });
+    start: function () {
+      const sessionId = this.cldrOpts.cldrStatus.getSessionId();
+      fetch(`api/summary?ss=${sessionId}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        stop: function() {
-        const sessionId = this.cldrOpts.cldrStatus.getSessionId();
-            fetch(`api/summary?ss=${sessionId}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    loadingPolicy: 'FORCESTOP'
-                })
-            })
-            .then(r => r.json())
-            .then(data => {
-                this.statusData = data;
-            });
-        },
-        start: function() {
-        const sessionId = this.cldrOpts.cldrStatus.getSessionId();
-            fetch(`api/summary?ss=${sessionId}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    loadingPolicy: 'START'
-                })
-            })
-            .then(r => r.json())
-            .then(data => {
-                this.statusData = data;
-            });
-        },
-    }
-}
+        body: JSON.stringify({
+          loadingPolicy: "START",
+        }),
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          this.statusData = data;
+        });
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>

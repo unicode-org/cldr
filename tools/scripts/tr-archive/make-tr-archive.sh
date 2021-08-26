3d8327b2f4b03186e4e4b700f9927d8d668f37bd
Steven R. Loomis <srl295@gmail.com>
2021-08-26 10:57:39 -0500

    CLDR-14649 doc: update TR archiver (#1455)
    - locally fetch logo60s2.gif for local check
    - use reports-v2 CSS
    - add a <title>
    - fix a TR error, errant “<none>”
    - add the red/gray top bar
    - add a doctype

--- tools/scripts/tr-archive/make-tr-archive.sh
+++ tools/scripts/tr-archive/make-tr-archive.sh
@@ -2,11 +2,15 @@
 npm i
 set -x
 # copy this locally
-if [ ! -f ./reports.css ];
+if [ ! -f ./reports-v2.css ];
 then
-    wget -c 'https://www.unicode.org/reports/tr35/reports.css'
+    wget -c 'http://www.unicode.org/reports/reports-v2.css'
+fi
+# copy this locally
+if [ ! -f ./logo60s2.gif ];
+then
+    wget -c 'https://www.unicode.org/reports/logo60s2.gif'
 fi
 mkdir -p dist
 cp -vR ../../../docs/ldml/ ./dist/
-node archive.js
-zip -r tr35.zip dist/*.html dist/images
+node archive.js && zip -r tr35.zip dist/*.html dist/images

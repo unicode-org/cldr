0fc1a8ef0d6da7ab27f759f571e0f258b1d7eae8
Mark Davis <mark@macchiato.com>
2021-11-22 18:01:21 -0800

    CLDR-15165 2nd batch from minimal review (#1630)
    * CLDR-15165 2nd batch from minimal review
    
    * CLDR-15165 added nom/gen patterns for sv.
    
    * CLDR-15165 fix typos
    
    * CLDR-15165 finish slovak also
    
    * CLDR-15165 Fix typo in Slovak pattern, adjust test to make clearer.

--- tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestExampleGenerator.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestExampleGenerator.java
@@ -582,7 +582,7 @@ public class TestExampleGenerator extends TestFmwk {
     public void TestPluralSamples() {
         ExampleGenerator exampleGenerator = getExampleGenerator("sv");
         String path = "//ldml/units/unitLength[@type=\"short\"]/unit[@type=\"length-centimeter\"]/unitPattern[@count=\"one\"]";
-        checkValue("Number should be one", "〖❬1❭ cm〗", exampleGenerator, path);
+        checkValue("Number should be one", "〖❬1❭ cm〗〖❬Jag tror att 1❭ cm❬ är tillräckligt.❭〗", exampleGenerator, path);
     }
 
     public void TestLocaleDisplayPatterns() {
@@ -1079,14 +1079,23 @@ public class TestExampleGenerator extends TestFmwk {
                     } else {
                         pattern = cldrFile.getStringValue(path);
                         String actualRaw = exampleGenerator.getExampleHtml(path, pattern);
-                        examples = ExampleGenerator.simplify(actualRaw, false)
+                        String actualSimplified = ExampleGenerator.simplify(actualRaw, false);
+                        examples = actualSimplified
                             .replace("〗〖", "\t")
                             .replace("〗", "")
                             .replace("〖", "")
                             ;
                         List<String> exampleList = com.google.common.base.Splitter.on('\t').trimResults().splitToList(examples);
-                        if (exampleList.size() != 2) {
-                            throw new IllegalArgumentException("Expecting exactly 2 examples: " + exampleList);
+                        final int exampleListSize = exampleList.size();
+                        switch(exampleListSize) {
+                        case 2: // ok
+                            break;
+                        case 1:
+                            warnln("Expecting exactly 2 examples: " + exampleList + ", but got " + exampleListSize);
+                            break;
+                        default:
+                            errln("Expecting exactly 2 examples: " + exampleList + ", but got " + exampleListSize);
+                            break;
                         }
                         StringBuilder exampleBuffer = new StringBuilder();
                         for (String exampleItem : exampleList) {

119a313d9486e0d76064e1238fd468c02485e716
Steven R. Loomis <srl295@gmail.com>
2022-02-01 09:20:10 -0600

    CLDR-15304 Update ConsoleCheckCLDR to output file:line if possible (#1719)

--- tools/cldr-code/src/test/java/org/unicode/cldr/util/TestSourceLocation.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/util/TestSourceLocation.java
@@ -14,5 +14,9 @@ public class TestSourceLocation {
         assertEquals(path_f + ":123:456: ", location.toString());
         assertEquals("c/d.xml" + ":123:456: ", location.toString(path_b));
         assertEquals("c/d.xml" + ":123:456: ", location.toString(path_b + "/")); // with trailing slash
+        assertEquals("c/d.xml", location.getSystem(path_b));
+        assertEquals("c/d.xml", location.getSystem(path_b + "/")); // with trailing slash
+        assertEquals("file=c/d.xml,line=123,col=456", location.forGitHub(path_b));
+        assertEquals("file=c/d.xml,line=123,col=456", location.forGitHub(path_b + "/")); // with trailing slash
     }
 }

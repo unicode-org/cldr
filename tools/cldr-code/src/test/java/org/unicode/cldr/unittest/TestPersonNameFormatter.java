6aed5b4b065ccf42b23a73c6195b0b52fe9785c5
Mark Davis <mark@macchiato.com>
2022-03-14 08:02:50 -0700

    CLDR-15404 Add string convenience methods, and validity checks (#1793)
    * CLDR-15404 Add string convenience methods, and validity checks
    
    * CLDR-15404 Add to TestAll
    
    * CLDR-15404 Make sure no exceptions occur based on input parameters

--- tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestPersonNameFormatter.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestPersonNameFormatter.java
@@ -1,25 +1,20 @@
 package org.unicode.cldr.unittest;
 
-import java.util.EnumSet;
+import java.util.Collection;
+import java.util.Map.Entry;
 
 import org.unicode.cldr.tool.ToolConfig;
 import org.unicode.cldr.util.personname.PersonNameFormatter;
-import org.unicode.cldr.util.personname.PersonNameFormatter.Field;
 import org.unicode.cldr.util.personname.PersonNameFormatter.FormatParameters;
-import org.unicode.cldr.util.personname.PersonNameFormatter.Length;
-import org.unicode.cldr.util.personname.PersonNameFormatter.ModifiedField;
-import org.unicode.cldr.util.personname.PersonNameFormatter.Modifier;
 import org.unicode.cldr.util.personname.PersonNameFormatter.NameObject;
 import org.unicode.cldr.util.personname.PersonNameFormatter.NamePattern;
 import org.unicode.cldr.util.personname.PersonNameFormatter.NamePatternData;
 import org.unicode.cldr.util.personname.PersonNameFormatter.Order;
-import org.unicode.cldr.util.personname.PersonNameFormatter.ParameterMatcher;
-import org.unicode.cldr.util.personname.PersonNameFormatter.Style;
-import org.unicode.cldr.util.personname.PersonNameFormatter.Usage;
 import org.unicode.cldr.util.personname.SimpleNameObject;
 
-import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.TreeMultimap;
 import com.ibm.icu.dev.test.TestFmwk;
 import com.ibm.icu.util.ULocale;
 
@@ -29,70 +24,57 @@ public class TestPersonNameFormatter extends TestFmwk{
         new TestPersonNameFormatter().run(args);
     }
 
+    private final NameObject sampleNameObject1 = new SimpleNameObject(
+        "locale=fr, prefix=Mr., given=John, given2-initial=B, given2= Bob, surname=Smith, surname2= Barnes Pascal, suffix=Jr.");
+
+    private void check(PersonNameFormatter personNameFormatter, NameObject nameObject, String nameFormatParameters, String expected) {
+        FormatParameters nameFormatParameters1 = FormatParameters.from(nameFormatParameters);
+        String actual = personNameFormatter.format(nameObject, nameFormatParameters1);
+        assertEquals("\n\t\t" + personNameFormatter + ";\n\t\t" + nameObject + ";\n\t\t" + nameFormatParameters1.toString(), expected, actual);
+    }
+
     /**
-     * TODO flesh out and add more tests as the engine adds functionality
+     * TODO Mark TODO Peter flesh out and add more tests as the engine adds functionality
      */
 
     public void TestBasic() {
 
-        // TODO make a convenience function ParameterMatcher.from() that takes a single string and parses, for ease of testing
-
-        ParameterMatcher nameRuleParameters1 = new ParameterMatcher(
-            EnumSet.of(Length.short_name, Length.medium),
-            EnumSet.of(Style.formal),
-            EnumSet.of(Usage.addressing),
-            null);
         ImmutableMap<ULocale, Order> localeToOrder = ImmutableMap.of(); // don't worry about using the order from the locale right now.
 
-        // TODO Make a version of NamePattern.from() that takes a single string and parses, for ease of testing
-        // TOOD For each example in the spec, make a test case for it.
+        // TOOD Mark For each example in the spec, make a test case for it.
 
         NamePatternData namePatternData = new NamePatternData(
             localeToOrder,
-            ImmutableListMultimap.of(
-                nameRuleParameters1, NamePattern.from("{given}", " ", "{given2-initial}", ". ", "{surname}"),
-                nameRuleParameters1, NamePattern.from("{given}", " ", "{surname}"),
-                ParameterMatcher.MATCH_ALL, NamePattern.from("{given}", " ", "{given2}", " ", "{surname}")
-                ));
+            "length=short medium; style=formal; usage=addressing", "{given} {given2-initial}. {surname}",
+            "length=short medium; style=formal; usage=addressing", "{given} {surname}",
+            "", "{prefix} {given} {given2} {surname} {surname2} {suffix}"
+                );
+
         PersonNameFormatter personNameFormatter = new PersonNameFormatter(namePatternData);
 
-        // TODO make a SimpleNameObject.from() that takes a single string and parses, for ease of testing
-
-
-        FormatParameters formatParameters1 = FormatParameters.from("length=short style=formal usage=addressing");
-        FormatParameters formatParameters2 = FormatParameters.from("length=long style=formal usage=addressing");
-
-        check(personNameFormatter, sampleNameObject1, formatParameters1, "John B. Smith");
-        check(personNameFormatter, sampleNameObject1, formatParameters2, "John Bob Smith");
-    }
-
-    private NameObject sampleNameObject1 = new SimpleNameObject(ULocale.FRENCH, ImmutableMap.of(
-        new ModifiedField(Field.prefix), "Mr.",
-        new ModifiedField(Field.given), "John",
-        new ModifiedField(Field.given2, Modifier.initial), "B",
-        new ModifiedField(Field.given2), "Bob",
-        new ModifiedField(Field.surname), "Smith",
-        new ModifiedField(Field.suffix), "Jr."
-        ));
-
-
-    private void check(PersonNameFormatter personNameFormatter, NameObject nameObject, FormatParameters nameFormatParameters1, String expected) {
-        String actual = personNameFormatter.format(nameObject, nameFormatParameters1);
-        assertEquals("\n\t\t" + personNameFormatter + ";\n\t\t" + nameObject + ";\n\t\t" + nameFormatParameters1.toString(), expected, actual);
+        check(personNameFormatter, sampleNameObject1, "length=short; style=formal; usage=addressing", "John B. Smith");
+        check(personNameFormatter, sampleNameObject1, "length=long; style=formal; usage=addressing", "Mr. John Bob Smith Barnes Pascal Jr.");
     }
 
     public void TestWithCLDR() {
         PersonNameFormatter personNameFormatter = new PersonNameFormatter(ToolConfig.getToolInstance().getEnglish());
 
-        // TODO Mark make a SimpleNameObject.from() that takes a single string and parses, for ease of testing
-
-        FormatParameters formatParameters1 = FormatParameters.from("length=short usage=sorting");
-        FormatParameters formatParameters2 = FormatParameters.from("length=long style=formal usage=referring");
-
         // TODO Rich once the code is fixed to strip empty fields, fix this test
 
-        check(personNameFormatter, sampleNameObject1, formatParameters1, "Smith, null");
-        check(personNameFormatter, sampleNameObject1, formatParameters2, "John Bob Smith Jr.");
+        check(personNameFormatter, sampleNameObject1, "length=short; usage=sorting", "Smith, null");
+        check(personNameFormatter, sampleNameObject1, "length=long; style=formal; usage=referring", "John Bob Smith Jr.");
 
+        // check that no exceptions happen
+        Multimap<String, FormatParameters> values = TreeMultimap.create();
+        for (FormatParameters item : FormatParameters.all()) {
+            Collection<NamePattern> actual = personNameFormatter.getBestMatchSet(sampleNameObject1, item);
+            values.put(actual.toString(), item);
+        }
+        for (Entry<String, Collection<FormatParameters>> entry : values.asMap().entrySet()) {
+            logln("\t" + entry.getKey());
+            for (FormatParameters value : entry.getValue()) {
+                logln("\t\t" + value);
+            }
+        }
     }
 }

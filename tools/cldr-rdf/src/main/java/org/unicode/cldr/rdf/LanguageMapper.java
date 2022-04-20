8a8a69eab876051313c0c98f5a47499bd5a7e755
Steven R. Loomis <srl295@gmail.com>
2022-04-20 10:28:51 -0500

    CLDR-15292 rdf: fix LanguageMapper (#1921)

--- tools/cldr-rdf/src/main/java/org/unicode/cldr/rdf/LanguageMapper.java
+++ tools/cldr-rdf/src/main/java/org/unicode/cldr/rdf/LanguageMapper.java
@@ -8,8 +8,6 @@ import org.apache.jena.query.Query;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.sparql.lang.sparql_11.ParseException;
-import org.unicode.cldr.util.CLDRConfig;
-import org.unicode.cldr.util.CoverageInfo;
 import org.unicode.cldr.util.XPathParts;
 
 /**
@@ -17,25 +15,23 @@ import org.unicode.cldr.util.XPathParts;
  * @author srl295
  */
 public class LanguageMapper implements XPathMapper {
-    
+
     static final String O_LANGUAGE = "?language";
     static final String DBO_639_3 = "dbo:iso6393Code";
     static final String DBO_639_2 = "dbo:iso6392Code";
     static final String DBO_639_1 = "dbo:iso6391Code";
     static final boolean DEBUG = false;
-    
+
     public LanguageMapper() {
     }
 
-    
+
     @Override
     public int addEntries(AbstractCache cache) throws ParseException {
     	int newAdd = 0;
-        final CLDRConfig config = CLDRConfig.getInstance();
-        CoverageInfo ci = config.getCoverageInfo();
         ResultSet rs = queryLanguageResources();
-        
-        
+
+
         XPathParts xpp = XPathParts.getFrozenInstance("//ldml/localeDisplayNames/languages/language").cloneAsThawed();
         while(rs.hasNext()) {
             final QuerySolution qs = rs.next();
@@ -44,13 +40,25 @@ public class LanguageMapper implements XPathMapper {
                 code = QueryClient.getLiteralOrNull(qs, "iso6392");
             }
             final String res = QueryClient.getResourceOrNull(qs, "language");
-//            final String abs = QueryClient.getStringOrNull(qs, "abstract");
+            if (code == null) {
+                // SPARQL should have filtered out this entry.
+                System.err.println("!!! SPARQL error: No code for " + res);
+                continue;
+            }
+            if (code.isEmpty()) {
+                if (DEBUG) {
+                    System.err.println("!!! Empty code for " + res);
+                }
+                // This happens for https://dbpedia.org/page/Grebo_language which has a junk extra entry
+                continue;
+            }
             if(code.length() != 3 && code.length() != 2) {
                 if(DEBUG) System.out.println("!!!" + rs.getRowNumber() + " - " + code + " " + res);
                 int spaceLoc = code.indexOf(' ');
                 if(!Character.isLowerCase(code.charAt(0))
                        || (spaceLoc != 2 && spaceLoc != 3)) {
-                    System.err.println("Rejecting: " + code + " for " + res);
+                    // Happens for Igbo
+                    System.err.println("Rejecting wrong-length " + code + " for " + res);
                     continue;
                 }
                 code = code.substring(0, spaceLoc);
@@ -58,7 +66,7 @@ public class LanguageMapper implements XPathMapper {
             } else {
             	if(DEBUG) System.out.println("== " + rs.getRowNumber() + " - " + code + " " + res);
             }
-            
+
             xpp.setAttribute(-1, "type", code);
             final String xpath = xpp.toString();
             if(DEBUG) System.out.println("\t"+res+"\t"+xpath);
@@ -84,12 +92,7 @@ public class LanguageMapper implements XPathMapper {
     			//                    .addOptional(resType, "dbo:iso6393Code", "?iso6393")  // Future: 639-3 code
 
 
-    			.addFilter("(?iso6391 || ?iso6392 "
-    					//                            + "|| ?iso6393"
-    					+ ") && "
-    					+ "(?iso6391 != 'none' && ?iso6392 != 'none' "
-    					//                            + "&& ?iso6393 != 'none'"
-    					+ ")")
+                .addFilter("(bound(?iso6391) || bound(?iso6392))") // At least 639-1 or 639-2, or both
     			;
     	System.out.println(builder.buildString());
     	Query q = builder.build();

069e337c7155c7036e1433a7f50ef2d029c46cbd
macchiati <mark@macchiato.com>
2022-05-18 12:37:59 -0700

    CLDR-15637 minor change to force rebuild

--- tools/cldr-code/src/main/java/org/unicode/cldr/test/CheckPlaceHolders.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/test/CheckPlaceHolders.java
@@ -150,7 +150,6 @@ public class CheckPlaceHolders extends CheckCLDR {
                                 .setSubtype(Subtype.invalidPlaceHolder)
                                 .setMessage("Names must have a value for the ‘surname’ field if they have a ‘surname2’ field."));
                         }
-
                         break;
                     default:
                         break;

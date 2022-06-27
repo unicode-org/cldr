64bff5eba475b6bdeed58e441b9f30a7f97fe85f
Mark Davis <mark@macchiato.com>
2022-06-27 08:49:02 -0700

    CLDR-15737 Problems with compact number report (#2127)
    * CLDR-15737 Add test of problem cases; will fail for now.
    
    * CLDR-15737 Fix the problems with plural rules also.

--- tools/cldr-code/src/main/java/org/unicode/cldr/test/BuildIcuCompactDecimalFormat.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/test/BuildIcuCompactDecimalFormat.java
@@ -15,9 +15,12 @@ import org.unicode.cldr.util.SupplementalDataInfo;
 import org.unicode.cldr.util.XPathParts;
 
 import com.ibm.icu.impl.number.DecimalFormatProperties;
+import com.ibm.icu.impl.number.PatternStringParser;
 import com.ibm.icu.text.CompactDecimalFormat;
 import com.ibm.icu.text.CompactDecimalFormat.CompactStyle;
+import com.ibm.icu.text.DecimalFormat;
 import com.ibm.icu.text.DecimalFormat.PropertySetter;
+import com.ibm.icu.text.PluralRules;
 import com.ibm.icu.util.Currency;
 import com.ibm.icu.util.ULocale;
 
@@ -27,6 +30,7 @@ public class BuildIcuCompactDecimalFormat {
     static final int MINIMUM_ARRAY_LENGTH = 15;
     static final Pattern PATTERN = PatternCache.get("([^0,]*)([0]+)([.]0+)?([^0]*)");
     static final Pattern TYPE = PatternCache.get("1([0]*)");
+    private static final boolean DEBUG = false;
 
     public enum CurrencyStyle {
         PLAIN, CURRENCY, LONG_CURRENCY, ISO_CURRENCY, UNIT
@@ -38,21 +42,50 @@ public class BuildIcuCompactDecimalFormat {
      * @param currencyStyle
      * @param currencyCode
      */
-    public static final CompactDecimalFormat build(CLDRFile resolvedCldrFile,
+    public static final CompactDecimalFormat build(
+        CLDRFile resolvedCldrFile,
         Set<String> debugCreationErrors, String[] debugOriginals,
         CompactStyle style, ULocale locale, CurrencyStyle currencyStyle, String currencyCodeOrUnit) {
 
+        // get the custom data from CLDR for use with the special setCompactCustomData
+
         final Map<String, Map<String, String>> customData = buildCustomData(resolvedCldrFile, style, currencyStyle);
-        CompactDecimalFormat cdf = CompactDecimalFormat.getInstance(locale, style);
+        if (DEBUG) {
+            System.out.println("\nCustom Data:");
+            customData.forEach((k, v) -> System.out.println("\t" + k + "\t" + v));
+        }
+
+        // get the common CLDR data used for number/date/time formats
+
         ICUServiceBuilder builder = new ICUServiceBuilder().setCldrFile(resolvedCldrFile);
 
+        DecimalFormat decimalFormat = currencyStyle == CurrencyStyle.PLAIN
+            ?  builder.getNumberFormat(1)
+                : builder.getCurrencyFormat(currencyCodeOrUnit);
+        final String pattern = decimalFormat.toPattern();
+        if (DEBUG) {
+            System.out.println("Pattern:\t" + pattern);
+        }
+
+        final PluralRules rules = SupplementalDataInfo.getInstance().getPlurals(locale.toString()).getPluralRules();
+
+        // create a compact decimal format, and reset its data
+
+        CompactDecimalFormat cdf = CompactDecimalFormat.getInstance(locale, style);
         cdf.setDecimalFormatSymbols(builder.getDecimalFormatSymbols("latn"));
+
         cdf.setProperties(new PropertySetter() {
             @Override
             public void set(DecimalFormatProperties props) {
                 props.setCompactCustomData(customData);
+                PatternStringParser.parseToExistingProperties(pattern, props, PatternStringParser.IGNORE_ROUNDING_ALWAYS);
+                props.setPluralRules(rules);
             }
         });
+
+        if (DEBUG) {
+            System.out.println("CompactDecimalFormat:\t" + cdf.toString().replace("}, ", "},\n\t"));
+        }
         return cdf;
     }
 
@@ -60,7 +93,8 @@ public class BuildIcuCompactDecimalFormat {
 
         final Map<String, Map<String, String>> customData = new TreeMap<>();
 
-        String prefix = currencyStyle == CurrencyStyle.PLAIN ? "//ldml/numbers/decimalFormats[@numberSystem=\"latn\"]/decimalFormatLength"
+        String prefix = currencyStyle == CurrencyStyle.PLAIN
+            ? "//ldml/numbers/decimalFormats[@numberSystem=\"latn\"]/decimalFormatLength"
             : "//ldml/numbers/currencyFormats[@numberSystem=\"latn\"]/currencyFormatLength";
 
         Iterator<String> it = resolvedCldrFile.iterator(prefix);
--- tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestCompactNumbers.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestCompactNumbers.java
@@ -13,15 +13,19 @@ import org.unicode.cldr.util.Factory;
 import org.unicode.cldr.util.ICUServiceBuilder;
 import org.unicode.cldr.util.StandardCodes;
 import org.unicode.cldr.util.SupplementalDataInfo;
+import org.unicode.cldr.util.SupplementalDataInfo.PluralInfo;
+import org.unicode.cldr.util.SupplementalDataInfo.PluralInfo.Count;
 import org.unicode.cldr.util.VerifyCompactNumbers;
 
 import com.ibm.icu.text.CompactDecimalFormat;
 import com.ibm.icu.text.CompactDecimalFormat.CompactStyle;
 import com.ibm.icu.text.NumberFormat;
+import com.ibm.icu.text.PluralRules.FixedDecimal;
+import com.ibm.icu.util.Currency;
 import com.ibm.icu.util.ULocale;
 
 public class TestCompactNumbers  extends TestFmwkPlus {
-    static final boolean DEBUG = false;
+    static final boolean DEBUG = true;
     private static StandardCodes sc = StandardCodes.make();
     private static final CLDRConfig CLDRCONFIG = CLDRConfig.getInstance();
     private static final SupplementalDataInfo SDI = CLDRCONFIG.getSupplementalDataInfo();
@@ -86,6 +90,70 @@ public class TestCompactNumbers  extends TestFmwkPlus {
                 + "\tclnf:\t" + compactLongFormattedNumber
                 + "\tccnf:\t" + compactCurrFormattedNumber
                 );
+        }
+    }
+
+    /** Test case for VerifyCompactNumbers
+     * https://unicode-org.atlassian.net/browse/CLDR-15737
+     * https://unicode-org.atlassian.net/browse/CLDR-15762
+     * */
+
+    public void TestVerifyCompactNumbers() {
+        Set<String> debugCreationErrors = new LinkedHashSet<>();
+        String[] debugOriginals = null;
+        String oldLocale = "";
+        String oldCurrencyCode = "";
+        CompactDecimalFormat cdfCurr = null;
+
+
+        Object[][] tests = {
+            {"cs", "", 1100000d, "1,1 milionu"}, // should be 'many', not 1,1 milionů == 'other'
+            /* Background for cs
+             * <pattern type="1000000" count="one">0 milion</pattern>
+             * <pattern type="1000000" count="few">0 miliony</pattern>
+             * <pattern type="1000000" count="many">0 milionu</pattern>
+             * <pattern type="1000000" count="other">0 milionů</pattern>
+             * https://unicode-org.github.io/cldr-staging/charts/41/supplemental/language_plural_rules.html#cs
+             */
+            {"fr", "EUR", 100d, "100 €"},
+            {"fr", "EUR", 1000d, "1 k €"},
+        };
+
+        for (Object[] row : tests) {
+            String locale = row[0].toString();
+            String currencyCode = row[1].toString();
+            double value = (double) row[2];
+            String expected = row[3].toString();
+
+            if (!locale.equals(oldLocale) || !currencyCode.equals(oldCurrencyCode)) {
+                oldLocale = locale;
+                oldCurrencyCode = currencyCode;
+                CLDRFile cldrFile = factory2.make(locale, true);
+
+                debugCreationErrors.clear();
+                if (currencyCode.isBlank()) {
+                    cdfCurr = BuildIcuCompactDecimalFormat.build(cldrFile, debugCreationErrors,
+                        debugOriginals, CompactStyle.LONG, ULocale.forLanguageTag(locale),
+                        CurrencyStyle.PLAIN, null);
+                    // note: custom data looks good:
+                    // 1000000={other=0 milionů, one=0 milion, few=0 miliony, many=0 milionu}
+                } else {
+                    cdfCurr = BuildIcuCompactDecimalFormat.build(cldrFile, debugCreationErrors,
+                        debugOriginals, CompactStyle.SHORT, ULocale.forLanguageTag(locale), CurrencyStyle.CURRENCY, currencyCode);
+
+                    cdfCurr.setCurrency(Currency.getInstance(currencyCode));
+                    int sigDigits = 3;
+                    cdfCurr.setMaximumSignificantDigits(sigDigits);
+                }
+            }
+            String actual = cdfCurr.format(value);
+            if (!assertEquals("Formatted " + value, expected, actual)) {
+                PluralInfo rules = SupplementalDataInfo.getInstance().getPlurals(locale);
+                int v, f, e;
+                FixedDecimal fd = new FixedDecimal(1.1d, 1, 1, 0);
+                Count count = rules.getCount(fd);
+                System.out.println(fd + " => " + count);
+            }
         }
     }
 }

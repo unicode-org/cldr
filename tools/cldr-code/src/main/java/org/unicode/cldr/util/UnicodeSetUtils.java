c7263d85792a30347672d6824121c28ffe80e70a
macchiati <mark@macchiato.com>
2022-07-08 19:36:10 -0600

    CLDR-4903 Handle emoji properly

--- tools/cldr-code/src/main/java/org/unicode/cldr/util/UnicodeSetUtils.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/UnicodeSetUtils.java
@@ -1,13 +1,15 @@
 package org.unicode.cldr.util;
 
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.unicode.cldr.util.CLDRFile.ExemplarType;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableSet;
 import com.ibm.icu.impl.Utility;
 import com.ibm.icu.text.Collator;
@@ -17,7 +19,25 @@ import com.ibm.icu.util.ULocale;
 
 public class UnicodeSetUtils {
 
-    public static final UnicodeSet TO_QUOTE = new UnicodeSet("[➖❰❱[:cc:][:Default_Ignorable_Code_Point:][:patternwhitespace:][:whitespace:]]").freeze();
+    public static final UnicodeSet TO_QUOTE = new UnicodeSet("[➖❰❱"
+        + "[:cc:][:Default_Ignorable_Code_Point:]"
+        + "[:patternwhitespace:][:whitespace:]"
+        + "-[[:block=tags:]-[:cn:]]" // don't quote tag characters, they are only in emoji
+        + "]").freeze();
+
+    public static final UnicodeSet EMOJI_EXCEPTIONS = new UnicodeSet("[:emoji_component:]").retainAll(TO_QUOTE).freeze();
+    public static final UnicodeSet EMOJI = new UnicodeSet("[:emoji:]");
+    public static final char ZERO_WIDTH_JOINER = 0x200D;
+    public static final char VARIATION_SELECTOR_16 = 0xFE0F;
+
+    static { // future proofing.
+        if (EMOJI_EXCEPTIONS.size() != 2
+            || !EMOJI_EXCEPTIONS.contains(ZERO_WIDTH_JOINER)
+            || !EMOJI_EXCEPTIONS.contains(VARIATION_SELECTOR_16)
+            ) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     public static class Quoter implements Function<String,String> {
         @Override
@@ -26,21 +46,54 @@ public class UnicodeSetUtils {
                 return source;
             }
             StringBuilder result = new StringBuilder();
+            // TODO detect emoji and don't quote ZWJ, etc
+            boolean afterEmojiPlus = false;
             int cp;
-            // TODO detect emoji and don't quote
             for (int i = 0; i < source.length(); i += Character.charCount(cp)) {
                 cp = source.codePointAt(i);
-                format(cp, result);
+                if (TO_QUOTE.contains(cp)) {
+                    // We have some simple tests to avoid quoting in the middle of an emoji sequence
+                    if (cp == ZERO_WIDTH_JOINER) {
+                        afterEmojiPlus = false;
+                        // quote unless right before emoji
+                        if (EMOJI.contains(codePointAtBounded(source, i + Character.charCount(cp)))) {
+                            result.appendCodePoint(cp);
+                            continue; // skip quoting
+                        }
+                    } else if (cp == VARIATION_SELECTOR_16) {
+                        if (afterEmojiPlus) {
+                            result.appendCodePoint(cp);
+                            continue;
+                        }
+                    } else {
+                        afterEmojiPlus = false;
+                    }
+                    quote(cp, result);
+                    continue;
+                } else if (EMOJI.contains(cp)) {
+                    afterEmojiPlus = true;
+                } else {
+                    afterEmojiPlus = false;
+                }
+                result.appendCodePoint(cp);
             }
             return result.toString();
         }
 
-        public void format(int cp, StringBuilder toAppendTo) {
+            /** return -1 if out of bounds, else codePointAt */
+        private int codePointAtBounded(String source, int index) {
+            return index < 0 || index >= source.length() ? null : source.codePointAt(index);
+        }
+
+        private void quote(int cp, StringBuilder toAppendTo) {
+            String name = MAP_CP_TO_NAME.get(cp);
+            toAppendTo.append("❰")
+            .append(name == null ? Utility.hex(cp, 2) : name)
+            .append("❱");
+        }
+        private void format(int cp, StringBuilder toAppendTo) {
             if (TO_QUOTE.contains(cp)) {
-                String name = NAME_MAP.get(cp);
-                toAppendTo.append("❰")
-                .append(name == null ? Utility.hex(cp, 2) : name)
-                .append("❱");
+                quote(cp, toAppendTo);
             } else {
                 toAppendTo.appendCodePoint(cp);
             }
@@ -49,7 +102,13 @@ public class UnicodeSetUtils {
 
     public static Quoter QUOTER = new Quoter();
 
-    private static final Map<Integer, String> NAME_MAP = ImmutableMap.<Integer, String>builder()
+    private static final BiMap<Integer, String> MAP_CP_TO_NAME = ImmutableBiMap.<Integer, String>builder()
+        // escapes for syntax
+        .put((int)'➖', "_")
+        .put((int)'❰', "ESC_S")
+        .put((int)'❱', "ESC_E")
+
+        // regular IDs; for now restricted to what we might encounter in CLDR
         .put(0x0020, "SP")
         .put(0x00AD, "SHY")
         .put(0x0009, "TAB")
@@ -70,6 +129,26 @@ public class UnicodeSetUtils {
         .put(0x2009, "NSP")
         .build();
 
+    /**
+     * FlatUnicodeSet is a basic format for use in exemplar sets and similar environments.
+     * The goal is a simple format for a set of literal characters that dispenses with
+     * operations and properties in favor of minimizing syntax and escaping.
+     * For the syntax needed, non-ASCII characters are used to further lessen escaping,
+     * and the escaping structure is semi-verbose.
+     * <br>The structure is:
+     * <pre>
+     * uset = range (' ' range)*
+     * range = codepoint+ | codepoint '➖' codepoint
+     * codepoint = literal | '❰' id '❱'
+     * id = hexCodePoint | character_acronym
+     * hexCodePoint = [A-F0-9]{2,6}
+     * character_acronym = [A-Z_]*
+     * </pre>
+     * So the syntax characters are space, ➖, ❰, ❱
+     * To express the literals, use ❰SP❱, ❰RANGE❱, ❰ESC_S❱, and ❰ESC_E❱
+     * @author markdavis
+     *
+     */
     public static class FlatUnicodeFormatter implements Function<UnicodeSet, String> {
 
         Collator col = Collator.getInstance(ULocale.ROOT);
@@ -91,22 +170,22 @@ public class UnicodeSetUtils {
                 col = Collator.getInstance(ULocale.ROOT);
             }
             col.setStrength(Collator.IDENTICAL);
-       }
+        }
 
         @Override
         public String apply(UnicodeSet t) {
             StringBuilder result = new StringBuilder();
             if (t.size() > 300) {
-                // do compressed format
+                // do compressed format, no spaces
                 for (EntryRange range : t.ranges()) {
-                    if (result.length() != 0) {
-                        result.append(' ');
-                    }
+//                    if (result.length() != 0) {
+//                        result.append(' ');
+//                    }
                     if (range.codepoint == range.codepointEnd) {
                         QUOTER.format(range.codepoint, result);
                     } else if (range.codepoint == range.codepointEnd - 1) {
                         QUOTER.format(range.codepoint, result);
-                        result.append(' ');
+                        //result.append(' ');
                         QUOTER.format(range.codepointEnd, result);
                     } else {
                         QUOTER.format(range.codepoint, result);
@@ -118,7 +197,7 @@ public class UnicodeSetUtils {
                     if (result.length() != 0) {
                         result.append(' ');
                     }
-                   result.append(QUOTER.apply(string));
+                    result.append(QUOTER.apply(string));
                 }
             }
 
@@ -135,73 +214,129 @@ public class UnicodeSetUtils {
             return result.toString();
         }
 
-        // TODO add parser
+        private static final Pattern END_OF_QUOTE = Pattern.compile("(([A-F0-9]{2,6})|([A-Z_]*))❱");
+
+        /*
+         * uset = range (' ' range)*
+         * range = codepoint+ | codepoint '➖' codepoint
+         * codepoint = literal | '❰' id '❱'
+         * id = hexCodePoint | character_acronym
+         * hexCodePoint = [A-F0-9]{2-6}
+         */
+        static final UnicodeSet parse(String source) {
+            UnicodeSet result = new UnicodeSet();
+            StringBuilder range = new StringBuilder(); // TODO optimize
+            Matcher matcher = END_OF_QUOTE.matcher(source);
+            int rangeStart = -1;
+            int cp;
+            for (int i = 0; i < source.length(); i += Character.charCount(cp)) {
+                cp = source.codePointAt(i);
+                switch (cp) {
+                default:
+                    range.appendCodePoint(cp);
+                    break;
+                case ' ':
+                    if (rangeStart >= 0) {
+                        throw new IllegalArgumentException();
+                    }
+                    if (range.length() != 0) {
+                        result.add(range);
+                        range.setLength(0);
+                    }
+                    break;
+                case '➖':
+                    switch (range.length()) {
+                    case 1:
+                        break;
+                    case 2: if (Character.codePointCount(range, 0, 2) == 1) {
+                        break;
+                    }
+                    default:
+                        throw new IllegalArgumentException();
+                    }
+                    rangeStart = cp;
+                    break;
+                case '❰':
+                    if (rangeStart >= 0) {
+                        throw new IllegalArgumentException();
+                    }
+                    matcher.region(i, source.length());
+                    if (!matcher.lookingAt()) {
+                        throw new IllegalArgumentException();
+                    }
+                    String group = matcher.group(2);
+                    if (group != null) {
+                        cp = Integer.parseInt(source);
+                    } else {
+                        group = matcher.group(3);
+                        cp = MAP_CP_TO_NAME.inverse().get(group);
+                    }
+                    range.appendCodePoint(Integer.parseInt(source));
+                    break;
+                case '❱':
+                    throw new IllegalArgumentException();
+                }
+            }
+            if (rangeStart >= 0) {
+                throw new IllegalArgumentException();
+            }
+            if (range.length() != 0) {
+                result.add(range);
+            }
+            return result;
+        }
     }
     // quick tests
     public static void main(String[] args) {
-//        UnicodeSetPrettyPrinter pp = new UnicodeSetPrettyPrinter()
-//            .setSpaceComparator(Collator.getInstance(ULocale.ROOT).setStrength2(Collator.PRIMARY))
-//            .setCompressRanges(false)
-//            .setQuoter(QUOTER)
-//            .setToQuote(TO_QUOTE);
+        Set<String> cldrLocales = StandardCodes.make().getLocaleCoverageLocales(Organization.cldr);
 
-        Set<String> locales = ImmutableSet.of("en", "sv", "ar");
         FlatUnicodeFormatter fuf = new FlatUnicodeFormatter();
+        String[][] tests = {
+            {"[abcq]", "a b c q"},
+            {"[ab{cq}]", "a b cq"},
+            {"[a\\u0020]", "❰SP❱ a"},
+            {"[\\u0019-!]", "❰19❱ ❰SP❱ !"},
+            {"[{2️⃣} 🪷-🪺 🫃{🫃🏻}{🇿🇼} {🏴\\U000E0067\\U000E0062\\U000E0065\\U000E006E\\U000E0067\\U000E007F}]", "🇿🇼 🏴󠁧󠁢󠁥󠁮󠁧󠁿 🪷 🪸 🪹 🪺 🫃 🫃🏻 2️⃣"},
+        };
+        for (String[] row : tests) {
+            UnicodeSet test = new UnicodeSet(row[0]);
+            String expected = row[1];
+            check("basic", test, fuf, expected);
+        }
+        Set<String> locales = ImmutableSet.of("en", "sv", "ar");
+        locales = StandardCodes.make().getLocaleCoverageLocales(Organization.cldr, ImmutableSet.of(Level.MODERN));
 
         for (String locale : locales) {
+            if (locale.equals("bgc") || locale.equals("bho")) {
+                continue;
+            }
             CLDRConfig CONFIG = CLDRConfig.getInstance();
             Factory factory = CONFIG.getCldrFactory();
             CLDRFile cldrFile = factory.make(locale, true);
             fuf.setLocale(locale);
-//            setForLocale(locale, pp);
-
-
-
-
             for (ExemplarType type : ExemplarType.values()) {
                 UnicodeSet exemplars = cldrFile.getRawExemplarSet(type, null);
-                String formatted = fuf.apply(exemplars);
-                System.out.println(locale + "\t" + type + "\t" + formatted);
+                check(locale + "\t" + type, exemplars, fuf, null);
             }
-
         }
-
-//        System.out.println(pp.format(new UnicodeSet("[\\u0000-\\u00ff]")));
-//        for (int i = 0; i < 0xff; ++i) {
-//            System.out.println(QUOTER.transform(new StringBuilder().appendCodePoint(i).toString()));
-//        }
-//        for (String s : TO_QUOTE) {
-//            System.out.println(QUOTER.transform(s));
-//        }
-
     }
-
-//    private static void setForLocale(String locale, UnicodeSetPrettyPrinter prettyPrinter) {
-//        ICUServiceBuilder isb = null;
-//        Collator col;
-//        try {
-//            isb = ICUServiceBuilder.forLocale(CLDRLocale.getInstance(locale));
-//        } catch (Exception e) {
-//        }
-//
-//        if (isb != null) {
-//            try {
-//                col = isb.getRuleBasedCollator();
-//            } catch (Exception e) {
-//                col = Collator.getInstance(ULocale.ROOT);
-//            }
-//        } else {
-//            col = Collator.getInstance(ULocale.ROOT);
-//        }
-//
-//        Collator spaceCol = Collator.getInstance(new ULocale(locale));
-//        if (spaceCol instanceof RuleBasedCollator) {
-//            ((RuleBasedCollator) spaceCol).setAlternateHandlingShifted(false);
-//        }
-//
-//        prettyPrinter
-//        .setOrdering(col)
-//        .setSpaceComparator(spaceCol);
-//
-//    }
+    private static String check(String message, UnicodeSet sample, FlatUnicodeFormatter fuf, String expected) {
+        try {
+            String formatted = fuf.apply(sample);
+            System.out.println(message + "\t" + formatted);
+            if (expected != null) {
+                if (!formatted.equals(expected)) {
+                    System.out.println("FAIL " + message + "\texpected " + expected + "\tactual " + formatted);
+                }
+            }
+            UnicodeSet reversed = FlatUnicodeFormatter.parse(formatted);
+            if (!reversed.equals(sample)) {
+                System.err.println("\tFAIL\t" + reversed);
+                throw new IllegalArgumentException("\tFAIL\t" + reversed);
+            }
+            return formatted;
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
 }

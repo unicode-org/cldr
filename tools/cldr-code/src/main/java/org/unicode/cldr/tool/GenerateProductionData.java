37057df68a2d20b8552c7097dfffb9fd7298622d
Peter Edberg <pedberg@unicode.org>
2022-08-16 11:02:26 -0700

    CLDR-15932 issues from prod test: copy empty annotation parents, fix en_AU/oc_ES/sw_KE

--- tools/cldr-code/src/main/java/org/unicode/cldr/tool/GenerateProductionData.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/tool/GenerateProductionData.java
@@ -222,6 +222,7 @@ public class GenerateProductionData {
             }
             boolean isMainDir = factory != null && sourceFile.getName().contentEquals("main");
             boolean isRbnfDir = factory != null && sourceFile.getName().contentEquals("rbnf");
+            boolean isAnnotationsDir = factory != null && sourceFile.getName().startsWith("annotations");
 
             Set<String> emptyLocales = new HashSet<>();
             final Stats stats2 = new Stats();
@@ -253,8 +254,8 @@ public class GenerateProductionData {
             stats2.showNonZero("\tTOTAL:\t");
             // if there are empty ldml files, AND we aren't in /main/,
             // then remove any without children
-            if (!emptyLocales.isEmpty() && !sourceFile.getName().equals("main")) {
-                Set<String> childless = getChildless(emptyLocales, factory.getAvailable());
+            if (!emptyLocales.isEmpty() && !isMainDir) {
+                Set<String> childless = getChildless(emptyLocales, factory.getAvailable(), isAnnotationsDir);
                 if (!childless.isEmpty()) {
                     if (VERBOSE) System.out.println("\t" + destinationFile + "\tRemoving empty locales:" + childless);
                     childless.stream().forEach(locale -> new File(destinationFile, locale + ".xml").delete());
@@ -551,13 +552,19 @@ public class GenerateProductionData {
         return result;
     }
 
-    private static Set<String> getChildless(Set<String> emptyLocales, Set<String> available) {
+    private static Set<String> getChildless(Set<String> emptyLocales, Set<String> available, boolean isAnnotationsDir) {
         // first build the parent2child map
         Multimap<String,String> parent2child = HashMultimap.create();
         for (String locale : available) {
             String parent = LocaleIDParser.getParent(locale);
             if (parent != null) {
                 parent2child.put(parent, locale);
+            }
+            if (isAnnotationsDir) {
+                String simpleParent = LocaleIDParser.getParent(locale, true);
+                if (simpleParent != null && (parent == null || simpleParent != parent)) {
+                    parent2child.put(simpleParent, locale);
+                }
             }
         }
 

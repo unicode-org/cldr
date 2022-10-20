e906a2e9a628e7339e4fbc68f8007747d5f5374d
Steven R. Loomis <srl295@gmail.com>
2022-10-20 16:22:19 -0500

    CLDR-15034 kbd: more exclusions of imports from DTD reading

--- tools/cldr-code/src/main/java/org/unicode/cldr/util/CLDRFile.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/CLDRFile.java
@@ -1427,7 +1427,9 @@ public class CLDRFile implements Freezable<CLDRFile>, Iterable<String>, LocaleSt
                     // <!ATTLIST version cldrVersion CDATA #FIXED "24" >
                     if (attribute.equals("cldrVersion")
                         && (qName.equals("version"))) {
-                        ((SimpleXMLSource) target.dataSource).setDtdVersionInfo(VersionInfo.getInstance(value));
+                        if (!value.equals("techpreview")) { // TODO: Keyboard techpreview
+                            ((SimpleXMLSource) target.dataSource).setDtdVersionInfo(VersionInfo.getInstance(value));
+                        }
                     } else {
                         putAndFixDeprecatedAttribute(qName, attribute, value);
                     }

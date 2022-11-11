aa4da5e96a22fa12de1e41dad7e0fc83874159b3
macchiati <mark@macchiato.com>
2022-11-11 10:55:51 -0800

    CLDR-4903 Rebase

---
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/SimpleUnicodeSetFormatter.java
@@ -0,0 +1,260 @@
+package org.unicode.cldr.util;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.TreeSet;
+import java.util.function.Function;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
+import com.ibm.icu.lang.CharSequences;
+import com.ibm.icu.text.Collator;
+import com.ibm.icu.text.Normalizer2;
+import com.ibm.icu.text.UnicodeSet;
+import com.ibm.icu.util.ULocale;
+
+/**
+ * Goal is a very simple format for UnicodeSet, that keeps vetters from having to know about \ for quoting
+ * There are 3 special characters, but we make sure that they don't have to be quoted if separated by spaces
+ *  -   a range, but if between two code points
+ *  {   start of a string, but only if followed by a }, with no intervening spaces
+ *  ⦕   start of hex, but only if followed by [A-Fa-f0-9]+ ⦖
+ *
+ *  EBNF
+ *  result = item + (" "? + (string | range | codepoint))*
+ *  string = "{" literal+ "} // literal must contain no spaces
+ *  range = codepoint "-" codepoint
+ *  codepoint = "⦕" [A-F0-9]+ "⦖" // escaped
+ *  codepoint = literal
+ *
+ *  The { is already used in UnicodeSet for strings, and familiar to vetters. ⦕ was chosen to be avoid special use of \\u or \x
+ *
+ *  TODO parse and format hex in strings
+ * @author markdavis
+ */
+public class SimpleUnicodeSetFormatter {
+    private static final char RANGE = '➖';
+    private static final char SESC = '❰';
+    private static final char EESC = '❱';
+    private static final char SSTR = '⦕';
+    private static final char ESTR = '⦖';
+
+    public static final UnicodeSet FORCE_HEX = new UnicodeSet("[[:c:][:z:][:whitespace:]]")
+        .add(RANGE).add(SESC).add(EESC).add(SSTR).add(ESTR).freeze();
+
+    public static Normalizer2 nfc = Normalizer2.getNFCInstance();
+
+    public static final Comparator<String> BASIC_COLLATOR;
+    static {
+        Collator temp = Collator.getInstance(ULocale.ROOT);
+        temp.setStrength(Collator.IDENTICAL);
+        temp.freeze();
+        BASIC_COLLATOR = (Comparator<String>) (Comparator<?>) temp;
+    }
+
+    public static class CodePointEscaper {
+        static final BiMap<Integer,String> codePointToName = ImmutableBiMap.<Integer,String>builder()
+            .put(0x20,"SP")
+            .put(0x061C,"ALM")
+            .put(0x200E,"LRM")
+            .put(0x200F,"RLM")
+            .put(0x200B,"ZWS")
+            .put(0x200C,"ZWNJ")
+            .put(0x200D,"ZWJ")
+            .put(0x2060,"WJ")
+            // TODO add more
+            .build();
+
+        public static int toCodePoint(CharSequence value) {
+            Integer codePoint = codePointToName.inverse().get(value.toString());
+            return codePoint == null
+                ? Integer.parseInt(value.toString(), 16)
+                    : codePoint; // todo, check codepoint bounds
+        }
+
+        public static String fromCodePoint(int cp) {
+            String result = codePointToName.get(cp);
+            return result == null
+                ? Integer.toString(cp, 16).toUpperCase(Locale.ROOT)
+                    : result;
+        }
+    }
+
+    private final Comparator<String> comparator;
+    private final UnicodeSet forceHex;
+
+    /**
+     * Create a simple formatter, with a comparator for the ordering and a UnicodeSet of characters that are to use hex
+     * @param col
+     * @param forceHex
+     */
+    public SimpleUnicodeSetFormatter(Comparator<String> col, UnicodeSet forceHex) {
+        this.comparator = col;
+        this.forceHex = forceHex == null ? FORCE_HEX : forceHex;
+    }
+
+    public String format(UnicodeSet input) {
+        StringBuilder result = new StringBuilder();
+        TreeSet<String> sorted = transformAndAddAllTo(input, x -> nfc.normalize(x), new TreeSet<>(comparator));
+
+        int firstOfRange = -2;
+        int lastOfRange = -2;
+        for (String item : sorted) {
+            int cp = CharSequences.getSingleCodePoint(item);
+            if (cp == Integer.MAX_VALUE) {
+                if (lastOfRange >= 0) {
+                    if (firstOfRange != lastOfRange) {
+                        result.append(firstOfRange + 1 != lastOfRange ? RANGE : ' ');
+                        appendWithHex(result, lastOfRange, forceHex);
+                    }
+                    firstOfRange = lastOfRange = -2;
+                }
+                if (result.length() > 0) {
+                    result.append(' ');
+                }
+                appendWithHex(result.append(SSTR), item, forceHex).append(ESTR);
+            } else if (lastOfRange == cp - 1) {
+                ++lastOfRange;
+            } else {
+                if (firstOfRange != lastOfRange) {
+                    result.append(firstOfRange + 1 != lastOfRange ? RANGE : ' ');
+                    appendWithHex(result, lastOfRange, forceHex);
+                }
+                if (result.length() > 0) {
+                    result.append(' ');
+                }
+                appendWithHex(result, cp, forceHex);
+                firstOfRange = lastOfRange = cp;
+            }
+        }
+        if (firstOfRange != lastOfRange) {
+            result.append(firstOfRange + 1 != lastOfRange ? RANGE : ' ');
+            appendWithHex(result, lastOfRange, forceHex);
+        }
+        return result.toString();
+    }
+
+    public static final StringBuilder appendWithHex(StringBuilder ap, CharSequence s, UnicodeSet forceHex) {
+        for (int cp : With.codePointArray(s)) {
+            appendWithHex(ap, cp, forceHex);
+        }
+        return ap;
+    }
+
+    public static StringBuilder appendWithHex(StringBuilder ap, int cp, UnicodeSet forceHex) {
+        if (!forceHex.contains(cp)) {
+            ap.appendCodePoint(cp);
+        } else {
+            ap.append(SESC).append(CodePointEscaper.fromCodePoint(cp)).append(EESC);
+        }
+        return ap;
+    }
+
+    private enum State {start, haveCp, haveHyphen, haveCurly, haveHex, haveHyphenHex}
+
+    public UnicodeSet parse(String input) {
+        UnicodeSet result = new UnicodeSet();
+        StringBuilder b = new StringBuilder();
+        State state = State.start;
+        StringBuilder toEscape = new StringBuilder();
+        int last = -1; // -1 indicates can't have -x for a range after it.
+        for (int cp : With.codePointArray(input)) {
+
+            switch (state) {
+            case start:
+                switch (cp) {
+                case ' ':
+                    break;
+                case SSTR:
+                    state=State.haveCurly; break;
+                case SESC:
+                    state=State.haveHex; break;
+                default:
+                    result.add(cp); last=cp; state=State.haveCp; break;
+                }
+                break;
+            case haveCp:
+                switch (cp) {
+                case ' ':
+                    state=State.start; break;
+                case SSTR:
+                    state=State.haveCurly; break;
+                case SESC:
+                    state=State.haveHex; break;
+                case RANGE:
+                    state=State.haveHyphen; break;
+                default:
+                    result.add(cp); last=cp; state=State.haveCp; break;
+                }
+                break;
+            case haveHyphen:
+                switch (cp) {
+                case ' ':
+                    result.add(RANGE); state=State.start; break; // failure
+                case SESC:
+                    state=State.haveHyphenHex; break;
+                default:
+                    result.add(last+1, cp); last=-1; state=State.start; break;
+                }
+                break;
+            case haveCurly:
+                switch (cp) {
+                case ESTR:
+                    result.add(b); b.setLength(0); state=State.start;
+                    break;
+                case ' ':
+                    b.insert(0, SSTR); result.add(b); state=State.start;
+                    break; // failure
+                    // TODO {a-c should be { followed by range a-b
+                default:
+                    b.appendCodePoint(cp); break;
+                }
+                break;
+            case haveHex:
+            case haveHyphenHex:
+                switch (cp) {
+                case EESC:
+                    int newCp;
+                    try {
+                        newCp = CodePointEscaper.toCodePoint(toEscape);
+                    } catch (Exception e) {
+                        CodePointEscaper.toCodePoint(toEscape);
+                        throw e;
+                    }
+                    if (state == State.haveHyphenHex) {
+                        result.add(last+1, newCp); last=-1;  state=State.start;
+                    } else {
+                        result.add(newCp); last=newCp; state=State.haveCp;
+                    }
+                    toEscape.setLength(0);
+                    b.setLength(0);
+                    break;
+                default:
+                    toEscape.appendCodePoint(cp);
+                    break;
+                }
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static UnicodeSet transform(UnicodeSet expected, Function<String,String> function) {
+        UnicodeSet result = new UnicodeSet();
+        for (String s : expected) {
+            String t = function.apply(s);
+            result.add(t);
+        }
+        return result;
+    }
+
+    public static <T extends Collection<String>> T transformAndAddAllTo(UnicodeSet expected, Function<String,String> function, T target) {
+        for (String s : expected) {
+            String t = function.apply(s);
+            target.add(t);
+        }
+        return target;
+    }
+
+}
--- tools/cldr-code/src/test/java/org/unicode/cldr/unittest/UnicodeSetPrettyPrinterTest.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/unittest/UnicodeSetPrettyPrinterTest.java
@@ -6,11 +6,22 @@
  */
 package org.unicode.cldr.unittest;
 
-import com.ibm.icu.dev.test.TestFmwk;
-import com.ibm.icu.text.UnicodeSet;
 import java.text.Collator;
 import java.util.Locale;
+import java.util.Set;
+
+import org.unicode.cldr.util.CLDRConfig;
+import org.unicode.cldr.util.CLDRFile;
+import org.unicode.cldr.util.CLDRFile.ExemplarType;
+import org.unicode.cldr.util.CLDRFile.WinningChoice;
+import org.unicode.cldr.util.Factory;
+import org.unicode.cldr.util.SimpleUnicodeSetFormatter;
+import org.unicode.cldr.util.SimpleUnicodeSetFormatter.CodePointEscaper;
 import org.unicode.cldr.util.UnicodeSetPrettyPrinter;
+
+import com.google.common.collect.ImmutableSet;
+import com.ibm.icu.dev.test.TestFmwk;
+import com.ibm.icu.text.UnicodeSet;
 
 public class UnicodeSetPrettyPrinterTest extends TestFmwk {
     public static void main(String[] args) throws Exception {
@@ -36,5 +47,94 @@ public class UnicodeSetPrettyPrinterTest extends TestFmwk {
         logln(formatted);
         UnicodeSet actual = new UnicodeSet(formatted);
         assertEquals("PrettyPrinter preserves meaning", expected, actual);
+    }
+
+    boolean showAnyway = false;
+
+    public void testCodePointEscaper() {
+        String [][] tests = {
+            {" ", "SP"},
+            {"\u000F", "F"},
+        };
+        for (String[] test : tests) {
+            final int source = test[0].codePointAt(0);
+            String actual = CodePointEscaper.fromCodePoint(source);
+            int expectedRoundtrip = CodePointEscaper.toCodePoint(actual);
+            String expected = test.length<2 ? actual : test[1];
+            assertEquals("", expected, actual);
+            assertEquals("", expectedRoundtrip, source);
+        }
+    }
+
+    public void testSimpleUnicodeSetFormatter() {
+        String [][] tests = {
+            {"[\\u0020]", "❰SP❱"},
+            {"[\u000F]", "❰F❱"},
+       };
+        SimpleUnicodeSetFormatter susf = new SimpleUnicodeSetFormatter(SimpleUnicodeSetFormatter.BASIC_COLLATOR, null);
+
+        for (String[] test : tests) {
+            final UnicodeSet source = new UnicodeSet(test[0]);
+            String actual = susf.format(source);
+            UnicodeSet expectedRoundtrip = null;
+            try {
+                expectedRoundtrip = susf.parse(actual);
+            } catch (Exception e) {}
+            String expected = test.length<2 ? actual : test[1];
+            assertEquals(source + " to format", expected, actual);
+            assertEquals(source + " roundtrip", expectedRoundtrip, source);
+        }
+    }
+
+    Set<String> SHOW_LOCALES = ImmutableSet.of("hu", "ar");
+
+    public void TestSimpleUnicodeSetFormatterWithLocales() {
+        final CLDRConfig CLDR_CONFIG = CLDRConfig.getInstance();
+        Factory cldrFactory = CLDR_CONFIG.getCldrFactory();
+        SimpleUnicodeSetFormatter susf = new SimpleUnicodeSetFormatter(SimpleUnicodeSetFormatter.BASIC_COLLATOR, null);
+
+        check(susf, "", ExemplarType.main, new UnicodeSet("[a-b]"), false);
+        check(susf, "", ExemplarType.main, new UnicodeSet("[-a-ceg-h{ef}]"), false);
+        check(susf, "", ExemplarType.main, new UnicodeSet("[\\u200D]"), false);
+        check(susf, "", ExemplarType.main, new UnicodeSet("[\\u200D-\\u200f]"), false);
+        check(susf, "", ExemplarType.main, new UnicodeSet("[\\u200D\\u200e]"), false);
+        // TODO also allow hex in strings check(susf, "", ExemplarType.main, new UnicodeSet("[{\\u200D\\u200e}]"), false);
+
+        for (String locale : cldrFactory.getAvailableLanguages()) {
+            CLDRFile cldrFile = cldrFactory.make(locale, true);
+            for (ExemplarType type : ExemplarType.values()) {
+                UnicodeSet expected = cldrFile.getExemplarSet(type, WinningChoice.WINNING);
+                expected = SimpleUnicodeSetFormatter.transform(expected, x -> SimpleUnicodeSetFormatter.nfc.normalize(x)); //current CLDR might not be normalized
+                check(susf, locale, type, expected, showAnyway || SHOW_LOCALES.contains(locale));
+            }
+        }
+    }
+
+    void check(SimpleUnicodeSetFormatter susf, String locale, ExemplarType type, UnicodeSet source, boolean showAnyway) {
+        String formatted = susf.format(source);
+        UnicodeSet roundtrip = susf.parse(formatted);
+        final boolean isOk = assertEquals(locale + ", " + type, source, roundtrip);
+        if (showAnyway || !isOk) {
+            UnicodeSet roundtrip_source = new UnicodeSet(roundtrip).removeAll(source);
+            UnicodeSet source_roundtrip = new UnicodeSet(source).removeAll(roundtrip);
+            System.out.println(locale + "\t" + type + "\tsource:  \t" + showInvisible(source, SimpleUnicodeSetFormatter.FORCE_HEX));
+            System.out.println(locale + "\t" + type + "\tformatted:\t" + formatted);
+            if (!roundtrip_source.isEmpty()) {
+                System.out.println(locale + "\t" + type + "\tFAIL, roundtrip-source:  \t" + showInvisible(roundtrip_source, SimpleUnicodeSetFormatter.FORCE_HEX));
+            }
+            if (!source_roundtrip.isEmpty()) {
+                System.out.println(locale + "\t" + type + "\tFAIL, source_roundtrip:  \t" + showInvisible(source_roundtrip, SimpleUnicodeSetFormatter.FORCE_HEX));
+            }
+
+            if (!isOk) {
+                // for debugging
+                String formattedDebug = susf.format(source);
+                UnicodeSet roundtripDebug = susf.parse(formatted);
+            }
+        }
+    }
+
+    public static String showInvisible(UnicodeSet input, UnicodeSet forceHex) {
+        return SimpleUnicodeSetFormatter.appendWithHex(new StringBuilder(), input.toPattern(false), forceHex).toString();
     }
 }

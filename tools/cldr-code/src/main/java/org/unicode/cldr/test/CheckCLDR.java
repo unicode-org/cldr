4991ece78f72757db4e28d63155672bcaa0a23b6
Steven R. Loomis <srl295@gmail.com>
2022-11-18 10:47:28 -0600

    CLDR-16140 v43 limited submission (#2545)
    - personNames
    - territory type=TR
    - locale: lmo, rif, ajp, pap
    - other changes per TC

--- tools/cldr-code/src/main/java/org/unicode/cldr/test/CheckCLDR.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/test/CheckCLDR.java
@@ -69,7 +69,7 @@ import com.ibm.icu.util.ICUUncheckedIOException;
  */
 abstract public class CheckCLDR implements CheckAccessor {
 
-    public static final boolean LIMITED_SUBMISSION = false; // TODO represent differently
+    public static final boolean LIMITED_SUBMISSION = true; // TODO: CLDR-13337: represent differently
 
     private static CLDRFile displayInformation;
 

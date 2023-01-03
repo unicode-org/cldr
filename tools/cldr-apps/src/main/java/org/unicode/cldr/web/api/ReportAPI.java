b760f821299612b53ba13ad218d3ee7be6240963
Steven R. Loomis <srl295@gmail.com>
2023-01-03 15:26:57 -0600

    CLDR-16272 disallow API to reports in limited
    - throw 403 Forbidden if user tries to navigate to or vote on un-available reports

--- tools/cldr-apps/src/main/java/org/unicode/cldr/web/api/ReportAPI.java
+++ tools/cldr-apps/src/main/java/org/unicode/cldr/web/api/ReportAPI.java
@@ -157,7 +157,7 @@ public class ReportAPI {
         for (final CLDRLocale loc : locales) {
             LocaleReportVettingResult rr = new LocaleReportVettingResult();
             rr.locale = loc.toString();
-            for (final ReportId report : ReportId.values()) {
+            for (final ReportId report : ReportId.getReportsAvailable()) {
                 Map<ReportAcceptability, Set<Integer>> statistics = db.updateResolver(loc, report, allUsers, res);
                 rr.reports.add(new ReportVettingResult(report, res, statistics));
                 statistics.values().forEach(s -> rr.addVoters(s));
@@ -274,6 +274,9 @@ public class ReportAPI {
         if (mySession.user == null || mySession.user.id != user) {
             return Response.status(Status.FORBIDDEN).build();
         }
+        if (!report.isAvailable()) {
+            return Response.status(Status.FORBIDDEN).build();
+        }
         ReportsDB.getInstance()
             .markReportComplete(user, CLDRLocale.getInstance(locale),
                 report, update.completed, update.acceptable);
@@ -328,6 +331,9 @@ public class ReportAPI {
         if (!SurveyMain.getLocalesSet().contains(loc)) {
             // No such locale
             return Response.status(Status.NOT_FOUND).build();
+        }
+        if (!report.isAvailable()) {
+            return Response.status(Status.FORBIDDEN).build();
         }
         final String result;
         try {

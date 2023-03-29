3a410d3b7b91a734a13d1d7e4498092a42f3b140
btangmu <thomasbishop@wenlin.com>
2023-03-29 16:03:05 -0400

    CLDR-16048 New forum post window sometimes disappears
    -Use LRU cache in cldrForumPanel.loadInfo and cldrSideways.loadMenu to avoid disappearance/reload
    
    -New cldrCache.mjs for simple front-end LRU cache, with unit test TestCldrCache.mjs
    
    -New Refresh button in Info Panel, clears caches and reloads
    
    -Clear forum cache when user submits a post
    
    -Remove cldrForumPanel.appendForumStuff, merge into loadInfo instead
    
    -Encapsulate FORUM_DIV_CLASS (forumDiv) and SIDEWAYS_AREA_CLASS (sidewaysArea)
    
    -Refactor/modernize some related code; reduce cruft; new subroutines to shorten long methods
    
    -Remove dead code (buttonTitle, theForum, replyStub, postUrl, tr.forumDiv.forumPosts, ...)
    
    -Update pretty.sh to recognize .mjs everywhere it recognizes .js
    
    -Comments

---
+++ tools/cldr-apps/js/test/TestCldrCache.mjs
@@ -0,0 +1,53 @@
+import * as cldrCache from "../src/esm/cldrCache.mjs";
+
+export const TestCldrc = "ok";
+
+const assert = chai.assert;
+
+describe("cldrCache.LRU", function () {
+  const c = new cldrCache.LRU(3);
+
+  it("should work up to its capacity", function () {
+    c.clear();
+    c.set("1", "test1");
+    c.set("2", "test2");
+    c.set("3", "test3");
+    const val1 = c.get("1");
+    const val2 = c.get("2");
+    const val3 = c.get("3");
+    assert(
+      val1 === "test1" && val2 === "test2" && val3 === "test3",
+      "should get 3 items that were set but got " +
+        val1 +
+        " " +
+        val2 +
+        " " +
+        val3
+    );
+  });
+
+  it("should discard LRU", function () {
+    c.set("4", "test4"); // 1 should be discarded
+    assert(c.get("4") === "test4", "should get MRU");
+    assert(
+      c.get("1") === undefined,
+      "should not get LRU that should have been discarded"
+    );
+  });
+
+  it("should clear", function () {
+    c.clear();
+    assert(c.get("4") === undefined, "should not get anything after clear");
+  });
+
+  it("should revise LRU for get", function () {
+    c.set("A", "testA");
+    c.set("B", "testB");
+    c.set("C", "testC");
+    c.get("A"); // now A should be MRU and B should be LRU
+    c.set("D", "testD"); // B (not A) should get discarded
+    assert(c.get("B") === undefined, "should have discarded B");
+    assert(c.get("A") === "testA", "should not have discarded A");
+    assert(c.get("D") === "testD", "should have added D");
+  });
+});

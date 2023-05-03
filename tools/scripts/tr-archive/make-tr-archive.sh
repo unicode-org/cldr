a40406aa281415b76465c72b14e49ec471402c44
Steven R. Loomis <srl295@gmail.com>
2023-05-03 09:43:02 -0500

    CLDR-15034 kbd: artifacts for PRI

--- tools/scripts/tr-archive/make-tr-archive.sh
+++ tools/scripts/tr-archive/make-tr-archive.sh
@@ -12,5 +12,14 @@ cp node_modules/anchor-js/anchor.min.js dist/js
 # copy the source .md and other stuff into dist
 cp -vR ../../../docs/ldml/* ./dist/
 
-# Generate output .html, and zip it all up.
-node archive.js && zip -r tr35.zip dist/*.html dist/images dist/js
+# Generate output .html
+node archive.js || exit 1
+
+# Generate .pdf for each page
+for file in dist/tr35-keyboards.html; do # TODO: dist/*.html for all
+    base=$(basename "${file}" .html)
+    npx electron-pdf -m 0 file://$(pwd)/"${file}" dist/"${base}".pdf
+done
+
+# zip it up
+exec zip -r tr35.zip dist/*.html dist/*.pdf dist/images dist/js

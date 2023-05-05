9c7aa220b5c5f6a3e497fc915fc402512b18a588
Tom Bishop <thomasbishop@wenlin.com>
2023-05-04 20:17:41 -0400

    CLDR-16560 Requirements for storing inheritance marker (#2889)
    -Change VoteResolver.DROP_HARD_INHERITANCE to true
    
    -When needed to meet requirements, change INHERITANCE_MARKER to Bailey or vice-versa
    
    -New method STFactory.PerLocaleData.reviseInheritanceAsNeeded called by setValueFromResolver
    
    -New method VoteResolver.reviseInheritanceAsNeeded called by STFactory.PerLocaleData.reviseInheritanceAsNeeded
    
    -New unit test TestVoteResolver.testReviseInheritance for VoteResolver.reviseInheritanceAsNeeded
    
    -Remove DataPage.DataRow.inheritsFromRootOrFallback, no longer necessary or appropriate
    
    -Make isBaselineValue true for inheritance marker in DataPage.DataRow.addItem when appropriate to display star
    
    -Comments

--- tools/cldr-code/src/test/java/org/unicode/cldr/util/TestVoteResolver.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/util/TestVoteResolver.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.ibm.icu.util.Output;
 import org.junit.jupiter.api.Test;
 import org.unicode.cldr.unittest.TestUtilities;
 import org.unicode.cldr.util.VoteResolver.Status;
@@ -42,5 +43,95 @@ public class TestVoteResolver {
 
     private VoteResolver<String> getStringResolver() {
         return new VoteResolver<String>(TestUtilities.getTestVoterInfoList());
+    }
+
+    /**
+     * Test VoteResolver.reviseInheritanceAsNeeded
+     *
+     * <p>Inheritance marker should remain unchanged if pathWhereFound.value.equals(path).
+     *
+     * <p>Bailey should change to inheritance marker if pathWhereFound.value.equals(path).
+     *
+     * <p>Inheritance marker should change to bailey if !pathWhereFound.value.equals(path).
+     *
+     * <p>Bailey should remain unchanged if !pathWhereFound.value.equals(path).
+     *
+     * <p>Any other value should remain unchanged no matter what.
+     */
+    @Test
+    public void testReviseInheritance() {
+        if (!VoteResolver.DROP_HARD_INHERITANCE) {
+            return;
+        }
+        final String localeId = "fr_CA";
+        final CLDRFile cldrFile = CLDRConfig.getInstance().getCLDRFile(localeId, true);
+
+        // Go through all paths until we have tested one for which pathWhereFound is the same,
+        // and another one for which pathWhereFound is different
+        boolean gotSamePath = false, gotDifferentPath = false;
+        for (String path : cldrFile.fullIterable()) {
+            Output<String> pathWhereFound = new Output<>();
+            String baileyValue = cldrFile.getBaileyValue(path, pathWhereFound, null);
+            if (baileyValue == null) {
+                continue;
+            }
+            String anyOtherValue = "x" + baileyValue; // anything but bailey or inheritance marker
+            if (pathWhereFound.value.equals(path)) {
+                if (!gotSamePath) {
+                    gotSamePath = true;
+                    testSamePath(path, baileyValue, anyOtherValue, cldrFile);
+                }
+            } else {
+                if (!gotDifferentPath) {
+                    gotDifferentPath = true;
+                    testDifferentPath(path, baileyValue, anyOtherValue, cldrFile);
+                }
+            }
+            if (gotSamePath && gotDifferentPath) {
+                return;
+            }
+        }
+        assertTrue(gotSamePath);
+        assertTrue(gotDifferentPath);
+    }
+
+    private void testSamePath(
+            String path, String baileyValue, String anyOtherValue, CLDRFile cldrFile) {
+        String value1 =
+                VoteResolver.reviseInheritanceAsNeeded(
+                        path, CldrUtility.INHERITANCE_MARKER, cldrFile);
+        assertEquals(
+                CldrUtility.INHERITANCE_MARKER,
+                value1,
+                "inheritance marker should remain unchanged");
+
+        String value2 = VoteResolver.reviseInheritanceAsNeeded(path, baileyValue, cldrFile);
+        assertEquals(
+                CldrUtility.INHERITANCE_MARKER,
+                value2,
+                "bailey should change to inheritance marker");
+
+        String value3 = VoteResolver.reviseInheritanceAsNeeded(path, anyOtherValue, cldrFile);
+        assertEquals(
+                anyOtherValue,
+                value3,
+                "any other value should remain unchanged when paths are same");
+    }
+
+    private void testDifferentPath(
+            String path, String baileyValue, String anyOtherValue, CLDRFile cldrFile) {
+        String value1 =
+                VoteResolver.reviseInheritanceAsNeeded(
+                        path, CldrUtility.INHERITANCE_MARKER, cldrFile);
+        assertEquals(baileyValue, value1, "inheritance marker should change to bailey");
+
+        String value2 = VoteResolver.reviseInheritanceAsNeeded(path, baileyValue, cldrFile);
+        assertEquals(baileyValue, value2, "bailey should remain unchanged");
+
+        String value3 = VoteResolver.reviseInheritanceAsNeeded(path, anyOtherValue, cldrFile);
+        assertEquals(
+                anyOtherValue,
+                value3,
+                "any other value should remain unchanged when paths are different");
     }
 }

05cabef8eaab60f1a6f986924779b6b4436919a9
Mark Davis <mark@macchiato.com>
2023-05-15 11:21:20 -0700

    CLDR-16654 Add tool and code for downgrading <locale, path, value> matches (#2926)

---
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/DowngradePaths.java
@@ -0,0 +1,17 @@
+package org.unicode.cldr.util;
+
+import java.nio.file.Paths;
+import org.unicode.cldr.draft.FileUtilities;
+
+public class DowngradePaths {
+
+    static LocalePathValueListMatcher data =
+            LocalePathValueListMatcher.load(
+                    Paths.get(
+                            FileUtilities.getRelativeFileName(
+                                    DowngradePaths.class, "downgrade.txt")));
+
+    public static boolean lookingAt(String locale, String path, String value) {
+        return data.lookingAt(locale, path, value);
+    }
+}

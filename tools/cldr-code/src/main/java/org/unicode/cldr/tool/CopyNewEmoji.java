54742b5c43ecfcacb4e5e987308327888daa865a
macchiati <mark@macchiato.com>
2023-05-28 14:01:40 -0700

    CLDR-16605 minor cleanup

--- tools/cldr-code/src/main/java/org/unicode/cldr/tool/CopyNewEmoji.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/tool/CopyNewEmoji.java
@@ -5,18 +5,29 @@ import com.google.common.io.Files;
 import java.io.File;
 import java.io.IOException;
 import java.util.Set;
+import org.unicode.cldr.util.CLDRPaths;
 
 public class CopyNewEmoji {
     public static void main(String[] args) throws IOException {
-        File sourceDir = new File("/Users/markdavis/eclipse-workspace/new_emoji");
-        File targetDir =
-                new File(
-                        "/Users/markdavis/github/cldr/tools/cldr-apps/src/main/webapp/images/emoji");
+        File targetDir = new File(CLDRPaths.APPS_EMOJI_DIRECTORY);
+
+        // TODO make this a command argument
+        File sourceDir = new File(CLDRPaths.CLDR_PRIVATE_DIRECTORY + "new_emoji");
 
         System.out.println(
-                "sourceDir.isDirectory(): " + sourceDir.isDirectory() + ", " + sourceDir.exists());
+                "sourceDir: "
+                        + sourceDir
+                        + "; isDirectory(), exists(): "
+                        + sourceDir.isDirectory()
+                        + ", "
+                        + sourceDir.exists());
         System.out.println(
-                "targetDir.isDirectory(): " + targetDir.isDirectory() + ", " + targetDir.exists());
+                "targetDir: "
+                        + targetDir
+                        + "; isDirectory(), exists(): "
+                        + targetDir.isDirectory()
+                        + ", "
+                        + targetDir.exists());
 
         final String[] sourceList = sourceDir.list();
         final String[] targetList = targetDir.list();
@@ -24,8 +35,8 @@ public class CopyNewEmoji {
         Set<String> sourceFiles = ImmutableSet.copyOf(sourceList);
         Set<String> targetFiles = ImmutableSet.copyOf(targetList);
 
-        System.out.println("sourceDir.isDirectory(): " + sourceFiles.size());
-        System.out.println("targetDir.isDirectory(): " + targetFiles.size());
+        System.out.println("sourceFiles: " + sourceFiles.size());
+        System.out.println("targetFiles: " + targetFiles.size());
 
         for (String sourceFile : sourceList) {
             if (!targetFiles.contains(sourceFile)) {

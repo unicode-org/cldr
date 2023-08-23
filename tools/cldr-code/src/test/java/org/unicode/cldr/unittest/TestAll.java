019fff0aac7ded593f827cd7f4fff16ce3b6f62e
Mark Davis <mark@macchiato.com>
2023-08-23 16:12:18 -0700

    CLDR-17028 Add test for person name formatting and regenerate test files (#3211)
    See #3211

--- tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestAll.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestAll.java
@@ -229,6 +229,7 @@ public class TestAll extends TestGroup {
                     "org.unicode.cldr.unittest.TestPersonNameFormatter",
                     "org.unicode.cldr.unittest.TestCompactNumbers",
                     "org.unicode.cldr.unittest.TestPathLookup",
+                    "org.unicode.cldr.unittest.TestDataTest",
                     //            "org.unicode.cldr.unittest.TestCollators" See Ticket #8288
                     "org.unicode.cldr.api.AllTests",
                 },

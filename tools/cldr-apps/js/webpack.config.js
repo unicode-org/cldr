2ffeb1f3a0169781c23b6b5fd85778fe39f1dd5d
Steven R. Loomis <srl295@gmail.com>
2024-02-02 14:04:24 -0600

    CLDR-17270 st: don't use late cache busting for bundle.js
    - instead, let webpack create a bundle name based on the content's hash, which will truly change when and only when the content changes.
    - webpack hook to write the file
    - read a manifest.json on the server side to serve that

--- tools/cldr-apps/js/webpack.config.js
+++ tools/cldr-apps/js/webpack.config.js
@@ -1,6 +1,26 @@
 const path = require("path");
+const fs = require('fs');
 const { VueLoaderPlugin } = require("vue-loader");
-const { DefinePlugin } = require("webpack");
+const { DefinePlugin, Compiler } = require("webpack");
+
+class SurveyToolPlugin {
+  constructor() {
+
+  }
+  /**
+   *
+   * @param {Compiler} compiler
+   */
+  apply(compiler) {
+    compiler.hooks.afterEmit.tap('SurveyToolPlugin', (compilation) => {
+      const { assets } = compilation;
+      const jsfiles = Object.keys(assets).filter(s => /\.js$/.test(s));
+      const manifestFile = path.resolve(compiler.outputPath, 'manifest.json');
+      fs.writeFileSync(manifestFile, JSON.stringify({ jsfiles }), 'utf-8');
+      console.log('# SurveyToolPlugin Wrote: ', manifestFile);
+    });
+  }
+};
 
 module.exports = (env, argv) => {
   const {mode} = argv;
@@ -12,7 +32,7 @@ module.exports = (env, argv) => {
       cacheDirectory: path.resolve(__dirname, '../target/webpack_cache'),
     },
     output: {
-      filename: "bundle.js",
+      filename: "[name].[contenthash].js",
       path: path.resolve(__dirname, "..", "src", "main", "webapp", "dist"),
       library: "cldrBundle",
       libraryTarget: "var",
@@ -43,7 +63,9 @@ module.exports = (env, argv) => {
         // esm bundler flags,
         // see <https://github.com/vuejs/vue-next/tree/master/packages/vue#bundler-build-feature-flags>
         __VUE_PROD_DEVTOOLS__: JSON.stringify(DEV), // TODO: support dev mode
-        __VUE_OPTIONS_API__:   JSON.stringify(true),
-      })],
-    };
+        __VUE_OPTIONS_API__: JSON.stringify(true),
+      }),
+      new SurveyToolPlugin(),
+    ]
+  };
 };

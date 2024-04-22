af19649b0c44565a3ead370412ddacaf7b57b2a5
Steven R. Loomis <srl295@gmail.com>
2024-04-22 18:40:55 -0500

    CLDR-17540 update SupplementalDataInfo reader to not use reentrant XPathParts (#3632)

--- tools/cldr-code/src/main/java/org/unicode/cldr/util/SimpleXPathParts.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/SimpleXPathParts.java
@@ -5,7 +5,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-/** Like {@link XPathParts} but does not depend on DTDs, etc. Safe for use at startup. */
+/**
+ * Similar to {@link XPathParts} but does not depend on DTDs, etc. Doesn't cache, so most
+ * appropriate for one-time data loading where paths will be unique. Safe for use during static
+ * init. API conforms to {@link XPathValue}
+ */
 public class SimpleXPathParts extends XPathParser {
 
     private final class Element {
@@ -15,9 +19,18 @@ public class SimpleXPathParts extends XPathParser {
         public Element(String name) {
             this.name = name;
         }
+
+        public Map<String, String> getAttributes() {
+            return attributes;
+        }
+
+        public boolean containsAttribute(String attribute) {
+            return attributes.containsKey(attribute);
+        }
     }
 
     final List<Element> elements = new LinkedList<>();
+    private final String xpath;
 
     @Override
     public String getElement(int i) {
@@ -27,12 +40,50 @@ public class SimpleXPathParts extends XPathParser {
         return elements.get(i).name;
     }
 
+    /**
+     * Search for an element within the path.
+     *
+     * @param elementName the element to look for
+     * @return element number if found, else -1 if not found
+     */
+    public int findElement(String elementName) {
+        for (int i = 0; i < elements.size(); ++i) {
+            Element e = elements.get(i);
+            if (!e.name.equals(elementName)) {
+                continue;
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean containsElement(String elementName) {
+        return findElement(elementName) >= 0;
+    }
+
     @Override
     public String getAttributeValue(int i, String attribute) {
         if (i < 0) {
             i = elements.size() + i;
         }
         return elements.get(i).attributes.get(attribute);
+    }
+
+    @Override
+    public Map<String, String> getAttributes(int elementIndex) {
+        return elements.get(elementIndex >= 0 ? elementIndex : elementIndex + size())
+                .getAttributes();
+    }
+
+    @Override
+    public boolean containsAttribute(String attribute) {
+        for (final Element e : elements) {
+            if (e.containsAttribute(attribute)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -51,16 +102,28 @@ public class SimpleXPathParts extends XPathParser {
     }
 
     SimpleXPathParts(String xpath) {
+        this.xpath = xpath;
         handleParse(xpath, true);
     }
 
+    @Override
+    public String toString() {
+        return xpath;
+    }
+
     /**
-     * for API compatibility
+     * Parse an XPath string into a read-only structure.
      *
      * @param xpath
-     * @return
+     * @return the parsed XPath
      */
-    static SimpleXPathParts getFrozenInstance(String xpath) {
+    static XPathValue getFrozenInstance(String xpath) {
+        // return the abstract class
         return new SimpleXPathParts(xpath);
+    }
+
+    @Override
+    public int size() {
+        return elements.size();
     }
 }
--- tools/cldr-code/src/main/java/org/unicode/cldr/util/XPathParts.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/XPathParts.java
@@ -34,6 +34,11 @@ import java.util.concurrent.ConcurrentHashMap;
  * //ldml/characters/exemplarCharacters[@type="auxiliary"] and a list of Element objects that depend
  * on xPath. Each Element object has an "element" string such as "ldml", "characters", or
  * "exemplarCharacters", plus attributes such as a Map from key "type" to value "auxiliary".
+ *
+ * <p>Caches values for fast lookup.
+ *
+ * <p>(If caching isn't needed, such as with supplemental data at static init time, see {@link
+ * SimpleXPathParts#getFrozenInstance(String)}
  */
 public final class XPathParts extends XPathParser
         implements Freezable<XPathParts>, Comparable<XPathParts> {
@@ -58,12 +63,7 @@ public final class XPathParts extends XPathParser
 
     /** See if the xpath contains an element */
     public boolean containsElement(String element) {
-        for (int i = 0; i < elements.size(); ++i) {
-            if (elements.get(i).getElement().equals(element)) {
-                return true;
-            }
-        }
-        return false;
+        return findElement(element) >= 0;
     }
 
     /**
@@ -362,6 +362,7 @@ public final class XPathParts extends XPathParser
     }
 
     /** Does this xpath contain the attribute at all? */
+    @Override
     public boolean containsAttribute(String attribute) {
         for (int i = 0; i < elements.size(); ++i) {
             Element element = elements.get(i);
@@ -381,7 +382,7 @@ public final class XPathParts extends XPathParser
         return false;
     }
 
-    /** How many elements are in this xpath? */
+    @Override
     public int size() {
         return elements.size();
     }
@@ -938,13 +939,11 @@ public final class XPathParts extends XPathParser
     }
 
     /**
-     * Determines if an elementName is contained in the path.
-     *
-     * @param elementName
-     * @return
+     * @deprecated use {@link #containsElement(String)}
      */
+    @Deprecated
     public boolean contains(String elementName) {
-        return findElement(elementName) >= 0;
+        return containsElement(elementName);
     }
 
     /** add a relative path to this XPathParts. */

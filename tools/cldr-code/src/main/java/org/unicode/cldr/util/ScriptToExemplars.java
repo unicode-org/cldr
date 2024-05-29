a9bf06a635a97abe5b3affb2f505e4264be57cf1
Steven R. Loomis <srl295@gmail.com>
2024-05-28 19:22:59 -0500

    CLDR-17395 update scriptToExemplars.txt from tests
    - change tests to update scriptToExemplars.txt when -DSHOW_EXEMPLARS is set
    - update scriptToExemplars.txt with instructions

--- tools/cldr-code/src/main/java/org/unicode/cldr/util/ScriptToExemplars.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/ScriptToExemplars.java
@@ -5,18 +5,29 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.ibm.icu.text.UnicodeSet;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class ScriptToExemplars {
+    public static final String FILE_PATH = "data/locales/scriptToExemplars.txt";
+
     public static UnicodeSet getExemplars(String script) {
         return ScriptToExemplarsLoader.SINGLETON.getExemplars(script);
+    }
+
+    /** return the comment block from the original file */
+    private static String getCommentBlock() {
+        return String.join("\n", ScriptToExemplarsLoader.SINGLETON.comments) + '\n';
     }
 
     private static class ScriptToExemplarsLoader {
         private static final ScriptToExemplarsLoader SINGLETON = new ScriptToExemplarsLoader();
         private Map<String, UnicodeSet> data;
+        private String[] comments;
 
         private UnicodeSet getExemplars(String script) {
             UnicodeSet result = data.get(script);
@@ -25,13 +36,15 @@ public class ScriptToExemplars {
 
         {
             Map<String, UnicodeSet> _data = Maps.newTreeMap();
-            try (BufferedReader reader =
-                    FileReaders.openFile(
-                            ScriptToExemplars.class, "data/locales/scriptToExemplars.txt")) {
+            List<String> _comments = new ArrayList<String>();
+            try (BufferedReader reader = FileReaders.openFile(ScriptToExemplars.class, FILE_PATH)) {
                 Iterable<String> rlsi =
                         With.toIterable(new FileReaders.ReadLineSimpleIterator(reader));
                 for (String line : rlsi) {
-                    if (line.isBlank() || line.startsWith("#")) {
+                    if (line.isBlank()) {
+                        continue;
+                    } else if (line.startsWith("#")) {
+                        _comments.add(line.trim());
                         continue;
                     }
                     Iterator<String> parts = Splitter.on(';').trimResults().split(line).iterator();
@@ -44,6 +57,33 @@ public class ScriptToExemplars {
                 throw new RuntimeException(e);
             }
             data = ImmutableMap.copyOf(_data);
+            comments = _comments.toArray(new String[_comments.size()]);
+        }
+    }
+
+    /** Called by LikelySubtagsTest.testGetResolvedScriptVsExemplars */
+    public static void write(Map<String, UnicodeSet> expected) {
+        final File file = new File(CLDRPaths.UTIL_SRC_DATA_DIR, FILE_PATH);
+        try (TempPrintWriter out = new TempPrintWriter(file)) {
+            // copy all comment lines in the file
+            out.println(getCommentBlock());
+            // copy all updated sets
+            for (Map.Entry<String, UnicodeSet> entry : expected.entrySet()) {
+                String script = entry.getKey();
+                UnicodeSet flattened = entry.getValue();
+                if (!flattened.isEmpty()) {
+                    out.println(
+                            script
+                                    + " ;\t"
+                                    + flattened.size()
+                                    + " ;\t"
+                                    + flattened.toPattern(false));
+                }
+            }
+            System.err.println(
+                    "Wrote: "
+                            + file.getAbsolutePath()
+                            + "\n Please check it carefully and commit it if needed.");
         }
     }
 }

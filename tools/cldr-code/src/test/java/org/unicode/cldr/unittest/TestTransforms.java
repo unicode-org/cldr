5806ae7102daac160898f5fbe598db28c9e0d866
macchiati <mark@macchiato.com>
2024-07-27 21:48:23 -0700

    CLDR-17844 Suppress certain characters newly given explicit scx values

--- tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestTransforms.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestTransforms.java
@@ -926,6 +926,14 @@ public class TestTransforms extends TestFmwkPlus {
                             + allMissing.dataSet.keySet().toPattern(false)
                             + "="
                             + allMissing.dataSet.keySet());
+            // Suppress Common/Inherited characters that are given scx properties
+            UnicodeSet suppressHack =
+                    new UnicodeSet(
+                                    "[\u0301\u0300\u0306\u0302\u030C\u030A\u0308\u0303\u0307\u0304\u0309\u0310\u0323-\u0325\u0330\u0331 \u00B7 \u02BC]")
+                            .freeze();
+            for (String s : suppressHack) {
+                allMissing.scriptMissing.remove(s);
+            }
             for (String script : allMissing.scriptMissing.values()) {
                 UnicodeSet missingFoScript = allMissing.scriptMissing.getKeys(script);
                 String localeForScript =

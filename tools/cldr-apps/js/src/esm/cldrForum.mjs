ffe3fa3afc0c99d0eb5e29416a9fafcb8df71f04
btangmu <thomasbishop@wenlin.com>
2024-08-06 13:36:46 -0400

    CLDR-17799 Forum, fix bug in last PR
    -Change postType to pi.postType in openPostOrReply

--- tools/cldr-apps/js/src/esm/cldrForum.mjs
+++ tools/cldr-apps/js/src/esm/cldrForum.mjs
@@ -247,7 +247,7 @@ function openPostOrReply(pi) {
   const subject = makePostSubject(isReply, rootPost, pi.subject);
   const root = isReply ? rootPost.id : -1;
   const open = isReply ? rootPost.open : true;
-  const typeLabel = makePostTypeLabel(postType, isReply);
+  const typeLabel = makePostTypeLabel(pi.postType, isReply);
   const html = makePostHtml(
     pi.postType,
     typeLabel,

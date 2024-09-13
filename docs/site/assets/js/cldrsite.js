45e10590a0c45ab7cb222834a330381c3e1a773b
Steven R. Loomis <srl295@gmail.com>
2024-09-13 09:19:32 -0500

    CLDR-17934 site: drive from sitemap.tsv (#4033)

--- docs/site/assets/js/cldrsite.js
+++ docs/site/assets/js/cldrsite.js
@@ -18,9 +18,27 @@ function md2html(p) {
   return p.replace(/\.md$/, ".html");
 }
 
+/** replace a/b/c with to /a/b/c, also '' => '/' */
+function path2url(p) {
+  if (p === "index") {
+    return "/";
+  }
+  return `/${p}`;
+}
+
 /** replace a/b/c.html with a/b/c.md */
 function html2md(p) {
   return p.replace(/\.html$/, ".md");
+}
+
+/** replace a/b/c.md with a/b/c */
+function dropmd(p) {
+  return p.replace(/\.md$/, "");
+}
+
+/** replace a/b/c.html with a/b/c */
+function drophtml(p) {
+  return p.replace(/\.html$/, "").replace(/\/$/, "");
 }
 
 /** load and cook the site data */
@@ -28,45 +46,8 @@ async function siteData() {
   // load the json
   const d = await fetch("/assets/json/tree.json");
   const j = await d.json();
-  const { all } = j;
+  const { usermap } = j;
 
-  // 'all' is an array of { title, fullPath } entries.
-  // Flat list of paths
-  const allPaths = all.map(({ fullPath }) => fullPath);
-  // Find all 'directories' (ending with /)
-  const allDirs = new Set();
-  allPaths.forEach((p) => {
-    const segs = p.split("/").slice(0, -1); // ['', 'dir1']
-    for (let n = 0; n <= segs.length; n++) {
-      // add all parent paths, so: '', dir1, dir1/dir2 etc.
-      const subpath = segs.slice(0, n).join("/");
-      allDirs.add(subpath);
-    }
-  });
-  j.allDirs = {};
-  j.allIndexes = [];
-  // allDirs:  '', index, downloads, etc…
-  allDirs.forEach((dir) => {
-    // presumed index page:  /downloads -> /downloads.md
-    // also / -> /index.md
-    const dirIndex = `${dir || "index"}.md`;
-    // console.dir({dir, dirIndex});
-    if (allPaths.indexOf(dirIndex) !== -1) {
-      j.allDirs[dir] = { index: dirIndex };
-      j.allIndexes.push(dirIndex);
-    } else {
-      console.error(`No index page: ${dirIndex}`);
-      j.allDirs[dir] = {};
-    }
-    j.allDirs[dir].pages = [];
-  });
-  allPaths.forEach((p) => {
-    const dir = path2dir(p);
-    j.allDirs[dir].pages.push(p);
-  });
-  // map md -> title
-  j.title = {};
-  all.forEach(({ title, fullPath }) => (j.title[fullPath] = title));
   return j;
 }
 
@@ -97,109 +78,73 @@ const app = Vue.createApp(
       path: String,
     },
     computed: {
-      mdPath() {
+      /** base path:  'index' or 'downloads/cldr-33' */
+      base() {
         if (this.path) {
-          return html2md(this.path);
+          return drophtml(this.path);
+        } else {
+          return "index"; // '' => 'index'
         }
         return null;
-      },
-      ourDir() {
-        if (this.path) {
-          return path2dir(this.path);
-        }
-        return "";
-      },
-      ourIndex() {
-        if (this.tree?.value) {
-          // first ARE we an index page?
-          if (this.tree.value.allIndexes.indexOf(this.mdPath) != -1) {
-            return this.mdPath; // we are an index
-          }
-          return this.tree.value.allDirs[this.ourDir].index;
-        }
-        return null;
-      },
-      ourIndexHtml() {
-        if (this.ourIndex) {
-          return md2html(this.ourIndex);
-        } else {
-          return null;
-        }
-      },
-      ourIndexTitle() {
-        if (this.ourIndex && this.tree?.value) {
-          return this.tree.value.title[this.ourIndex] || this.ourIndex;
-        } else {
-          return null;
-        }
       },
       ourTitle() {
         if (this.tree?.value) {
           if (this.path === "") return this.rootTitle;
-          return this.tree.value.title[html2md(this.path)];
+          return this?.tree?.value?.usermap[this.base]?.title;
         }
       },
       // title of root
       rootTitle() {
-        return this.tree?.value?.title["index.md"];
+        const usermap = this?.tree?.value?.usermap ?? {};
+        return usermap?.index?.title ?? "CLDR";
       },
-      // list of pages for siblings of this dir
-      siblingPages() {
-        if (!this.tree?.value) return [];
-        let dirForPage = this.ourDir;
-        if (this.tree.value.allIndexes.indexOf(this.mdPath) != -1) {
-          const dirPages = Object.entries(this.tree?.value?.allDirs).filter(
-            ([k, { index }]) => index == this.mdPath
-          )[0];
-          if (dirPages) {
-            // our page is an index -so, show the subpages instead of the siblings.
-            dirForPage = dirPages[0]; // the adjusted index
-          } else {
-            return []; // no sibling pages;
-          }
-        } else {
-          return []; // no sibling pages
-        }
-        let thePages = this.tree?.value?.allDirs[dirForPage].pages ?? [];
-        if (dirForPage === "") {
-          thePages = [...thePages, ...this.tree?.value?.allDirs["index"].pages];
-        }
-        const c = new Intl.Collator([]);
-        const t = this;
-        return thePages
-          .map((path) => ({
-            path,
-            html: md2html(path),
-            title: this.tree.value.title[path] ?? path,
-          }))
-          .sort((a, b) => c.compare(a.title, b.title))
-          .filter(({ html }) => html != t.path); // skip showing the index page in the subpage list
+      children() {
+        const usermap = this?.tree?.value?.usermap;
+        if (!usermap) return []; // no children
+        const entry = usermap[this.base];
+        const children = entry?.children;
+        if (!children || !children.length) return [];
+        return children.map((path) => ({
+          path,
+          href: path2url(path),
+          title: usermap[path]?.title || path,
+          children: (usermap[path].children ?? []).length > 0,
+        }));
       },
       ancestorPages() {
         const pages = [];
         // if we are not loaded, or if we're at the root, then exit
-        if (!this.tree?.value || !this.path || this.path == "index.html")
-          return pages;
+        const usermap = this?.tree?.value?.usermap;
+        if (
+          !usermap ||
+          !this.path ||
+          this.path == "index.html" ||
+          this.map == "index"
+        ) {
+          return [];
+        }
         // traverse
-        let path = this.path;
+        let path = drophtml(this.path); // can't be null, empty, or index (see above). Map a/b/c.html to a/b/c
         do {
           // calculate the immediate ancestor
-          const pathMd = html2md(path);
-          const dir = path2dir(path);
-          const nextIndex = this.tree.value.allDirs[dir].index || "index.md"; // falls back to top
-          const nextIndexHtml = md2html(nextIndex);
-          const nextIndexTitle = this.tree.value.title[nextIndex];
+          const nextParentPath = usermap[path]?.parent;
+          if (!nextParentPath) break;
+          if (nextParentPath == path) {
+            console.error("Loop detected!");
+            break;
+          }
+          const nextParent = usermap[nextParentPath];
+          if (!nextParent) break;
+          const href = path2url(nextParentPath);
+          const { title } = nextParent || nextParentPath;
           // prepend
           pages.push({
-            href: "/" + nextIndexHtml,
-            title: nextIndexTitle,
+            href,
+            title,
+            path: nextParentPath,
           });
-          if (nextIndexHtml == path) {
-            console.error("Loop detected from " + this.path);
-            path = "index.html"; // exit
-          }
-          path = nextIndexHtml;
-        } while (path && path != "index.html"); // we iterate over 'path', so html
+          path = nextParentPath;
+        } while (path); // we iterate over 'path' until it returns null
         pages.reverse();
         return pages;
       },
@@ -215,19 +160,17 @@ const app = Vue.createApp(
        <span class="ancestor" v-for="ancestor of ancestorPages" :key="ancestor.href">
          <a class="uplink" v-bind:href="ancestor.href">{{ ancestor.title }}</a><span class="crumb">❱</span>
        </span>
-       <div v-if="!siblingPages || !siblingPages.length" class="title"> {{ ourTitle }} </div>
+       <div v-if="!children || !children.length" class="title"> {{ ourTitle }} </div>
        <div v-else class="title"  @mouseover="popup = true"><span class="hamburger" @click="popup = !popup">≡</span>
             {{ ourTitle }}
 
         <div class="subpages" v-if="popup">
           <span class="hamburger" @click="popup=false">✕</span>
           <ul class="subpages" >
-              <li v-for="subpage of siblingPages" :key="subpage.path">
-                  <span v-if="path == subpage.html">
-                      <b>{{ subpage.title }}</b>
-                  </span>
-                  <a v-else v-bind:href="'/'+subpage.html">
+              <li v-for="subpage of children" :key="subpage.path">
+                  <a v-bind:href="subpage.href">
                       {{ subpage.title }}
+                       <span class="hamburger" v-if="subpage.children">❱</span>
                   </a>
               </li>
           </ul>
@@ -243,19 +186,6 @@ const app = Vue.createApp(
     path: myPath,
   }
 );
-
-// app.component("CldrPage", {
-//   setup() {},
-//   template: `<p>Hello</p>
-//         `,
-// });
-
-// app.component("CldrList", {
-//   setup() {},
-//   template: `
-//         <p>Hullo</p>
-//         `,
-// });
 
 app.mount("#nav");
 

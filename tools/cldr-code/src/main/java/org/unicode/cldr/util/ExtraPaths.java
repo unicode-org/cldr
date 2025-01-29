61b16cb9fbb08827f15deaffe2009e2d255fa0b5
btangmu <thomasbishop@wenlin.com>
2025-01-29 12:50:03 -0500

    CLDR-17014 Change HashMap to ConcurrentHashMap in ExtraPaths
    -ConcurrentModificationException was thrown when ExtraPaths.getInstance called computeIfAbsent during cldr-generate-json.sh
    
    -Unable to repro the exception before making this change; anyway, this should prevent it

--- tools/cldr-code/src/main/java/org/unicode/cldr/util/ExtraPaths.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/ExtraPaths.java
@@ -1,16 +1,16 @@
 package org.unicode.cldr.util;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ExtraPaths {
 
-    private static final Map<NameType, ExtraPaths> instances = new HashMap<>();
+    private static final Map<NameType, ExtraPaths> instances = new ConcurrentHashMap<>();
 
     public static ExtraPaths getInstance(NameType nameType) {
         return instances.computeIfAbsent(nameType, ExtraPaths::new);

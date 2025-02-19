786490f368da672e71c7c227abeead7e47ab9351
btangmu <thomasbishop@wenlin.com>
2025-02-19 13:20:03 -0500

    CLDR-13589 Revise unit test that locale time cycle matches region prefs
    -Similar to icu4j DateTimeGeneratorTest.testJjMapping
    
    -Use current CLDR data, not ICU data

--- tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestTimeCycle.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestTimeCycle.java
@@ -1,11 +1,15 @@
 package org.unicode.cldr.unittest;
 
-import com.ibm.icu.text.DateFormat;
 import com.ibm.icu.text.DateTimePatternGenerator;
 import com.ibm.icu.text.SimpleDateFormat;
 import com.ibm.icu.util.ULocale;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
+import org.unicode.cldr.icu.LDMLConstants;
+import org.unicode.cldr.util.CLDRConfig;
+import org.unicode.cldr.util.CLDRLocale;
+import org.unicode.cldr.util.Factory;
+import org.unicode.cldr.util.ICUServiceBuilder;
 
 public class TestTimeCycle {
     private static final Logger logger = Logger.getLogger(TestTimeCycle.class.getName());
@@ -13,7 +17,8 @@ public class TestTimeCycle {
     /**
      * Test that a locale's standard time format matches the region's preferred time cycle
      *
-     * <p>This is essentially the same as icu4j DateTimeGeneratorTest.testJjMapping
+     * <p>This is similar to icu4j DateTimeGeneratorTest.testJjMapping. It uses current CLDR data,
+     * not ICU data.
      */
     @Test
     public void testJjMapping() {
@@ -37,13 +42,28 @@ public class TestTimeCycle {
 
         // Next test that in all available Locales, the actual short time pattern uses the same
         // cycle as produced by 'j'
-        ULocale[] locales = DateFormat.getAvailableULocales();
-        for (ULocale locale : locales) {
-            String localeID = locale.getName();
-            DateTimePatternGenerator dtpg = DateTimePatternGenerator.getInstance(locale);
-            DateFormat dfmt = DateFormat.getTimeInstance(DateFormat.SHORT, locale);
-            String shortPattern = ((SimpleDateFormat) dfmt).toPattern();
+
+        CLDRConfig testInfo = CLDRConfig.getInstance();
+        Factory cldrFactory = testInfo.getCldrFactory();
+        for (String localeID : cldrFactory.getAvailable()) {
+            CLDRLocale loc = CLDRLocale.getInstance(localeID);
+            ICUServiceBuilder icuServiceBuilder = ICUServiceBuilder.forLocale(loc);
+
+            // Compare ICU data version:
+            // DateTimePatternGenerator dtpg = DateTimePatternGenerator.getInstance(uloc);
+
+            DateTimePatternGenerator dtpg = DateTimePatternGenerator.getEmptyInstance();
+
             String jPattern = dtpg.getBestPattern(jSkeleton);
+
+            // Compare ICU data version:
+            // DateFormat dfmt = DateFormat.getTimeInstance(DateFormat.SHORT, uloc);
+
+            SimpleDateFormat dfmt =
+                    icuServiceBuilder.getDateFormat(LDMLConstants.GREGORIAN, jPattern);
+
+            String shortPattern = dfmt.toPattern();
+
             // Now check that shortPattern and jPattern use the same hour cycle
             String jPatSkeleton = dtpg.getSkeleton(jPattern);
             String shortPatSkeleton = dtpg.getSkeleton(shortPattern);

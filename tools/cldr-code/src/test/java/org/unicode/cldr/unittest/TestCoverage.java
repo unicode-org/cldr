2f2c42595420376bd4c09b43cef7848172cc644b
Mark Davis <mark@macchiato.com>
2025-03-17 15:58:14 -0700

    CLDR-15331 Add RBNF to coverage (#4503)

--- tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestCoverage.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/unittest/TestCoverage.java
@@ -41,14 +41,14 @@ public class TestCoverage extends TestFmwkPlus {
         Multimap<CoreItems, String> errors = LinkedHashMultimap.create();
         Set<CoreItems> coreCoverage = CoreCoverageInfo.getCoreCoverageInfo(engCldrFile, errors);
         if (!assertEquals("English should be complete", all, coreCoverage)) {
-            showDiff("Missing", all, coreCoverage);
+            showDiff("English Missing", all, coreCoverage);
         }
-        CLDRFile skimpyLocale = testInfo.getCldrFactory().make("asa", false);
+        CLDRFile skimpyLocale = testInfo.getCldrFactory().make("asa", true);
         errors.clear();
         coreCoverage = CoreCoverageInfo.getCoreCoverageInfo(skimpyLocale, errors);
         if (!assertEquals("Skimpy locale should not be complete", none, coreCoverage)) {
-            showDiff("Missing", all, coreCoverage);
-            showDiff("Extra", coreCoverage, none);
+            showDiff("Skimpy Missing", all, coreCoverage);
+            showDiff("Skimpy Extra", coreCoverage, none);
         }
     }
 

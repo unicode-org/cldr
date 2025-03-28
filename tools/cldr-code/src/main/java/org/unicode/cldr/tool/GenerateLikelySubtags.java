ca8263cd0347fc5b67d7bb727d0425b4cd38ee55
Markus Scherer <markus.icu@gmail.com>
2025-03-27 17:54:30 -0700

    CLDR-18283 Unicode 17 script metadata

--- tools/cldr-code/src/main/java/org/unicode/cldr/tool/GenerateLikelySubtags.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/tool/GenerateLikelySubtags.java
@@ -332,7 +332,11 @@ public class GenerateLikelySubtags {
                             "no_Latn_NO",
                             "tok_Latn_001",
                             "prg_Latn_PL",
-                            "ie_Latn_EE");
+                            "ie_Latn_EE",
+                            // kyw=Kurmali: Devanagari not Chis=Chisoi
+                            "kyw_Deva_IN",
+                            // tyj=Tai Yo: Latin not Tayo=Tai Yo
+                            "tyj_Latn_VN");
 
     /**
      * The following overrides do MASH the final values, so they may not result in consistent

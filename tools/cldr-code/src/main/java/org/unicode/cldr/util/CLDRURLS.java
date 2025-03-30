b67d2acb9dfbd55fe29721403cd79f238b64f34f
Mark Davis <mark@unicode.org>
2025-03-29 17:13:36 -0700

    CLDR-17570 Try markdown

--- tools/cldr-code/src/main/java/org/unicode/cldr/util/CLDRURLS.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/CLDRURLS.java
@@ -145,7 +145,7 @@ public abstract class CLDRURLS {
     public static final String NUMBER_PATTERNS =
             "https://cldr.unicode.org/translation/number-currency-formats/number-and-currency-patterns#h.j899g3kk2p1z";
     public static final String RATIONAL_NUMBERS_HELP =
-            "https://cldr.unicode.org/translation/number-currency-formats/number-and-currency-patterns#rational-formatting";
+            "[Rational Number Formatting](https://cldr.unicode.org/translation/number-currency-formats/number-and-currency-patterns#rational-formatting)";
     public static final String PARSE_LENIENT =
             "https://cldr.unicode.org/translation/core-data/characters#h.j3x0cwalqgqt";
     public static final String PERSON_NAME_FORMATS =

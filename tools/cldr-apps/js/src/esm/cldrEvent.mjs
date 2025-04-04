5d074a059ab36150e859b33c791f16ec7cd2ecdf
Steven R. Loomis <srl295@gmail.com>
2025-04-04 16:03:52 -0500

    CLDR-18475 st: fix RTL on examples
    - popover CSS also forced the alignment 'left' - change to initial
    - add comment about how to keep popovers around

--- tools/cldr-apps/js/src/esm/cldrEvent.mjs
+++ tools/cldr-apps/js/src/esm/cldrEvent.mjs
@@ -76,14 +76,14 @@ function startup() {
         .closest(".d-disp,.d-item,.d-item-err,.d-item-warn")
         .find(".d-example");
       if (example) {
+        const lang = example.attr("lang");
+        const dir = example.attr("dir");
         $(this)
           .popover({
             html: true,
             placement: "top",
-            content: `<div class='d-example-popover' lang='${example.attr(
-              "lang"
-            )}' dir='${example.attr("dir")}'>
-                        ${example.html()}
+            content: `<div class='d-example-popover' lang='${lang}' dir='${dir}'>
+                ${example.html()}
             </div>`,
             dir: example.dir,
           })
@@ -95,7 +95,7 @@ function startup() {
     "mouseleave",
     ".vetting-page .d-example-img, .vetting-page .subSpan",
     function () {
-      $(this).popover("hide");
+      $(this).popover("hide"); // comment this out to keep example popup around
     }
   );
 
@@ -122,7 +122,7 @@ function startup() {
     "mouseleave",
     ".vetting-page .d-trans-hint-img, .vetting-page .subSpan",
     function () {
-      $(this).popover("hide");
+      $(this).popover("hide"); // comment this out to keep example popup around
     }
   );
   resizeSidebar();

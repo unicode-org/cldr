a50f267f576c03302c266f5d931cde02329b11f1
btangmu <thomasbishop@wenlin.com>
2025-04-22 14:51:44 -0400

    CLDR-14996 Temporarily disable test on short timezone names

--- tools/cldr-code/src/test/java/org/unicode/cldr/test/TestEn001Time.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/test/TestEn001Time.java
@@ -7,12 +7,17 @@ import org.unicode.cldr.util.*;
 
 public class TestEn001Time {
 
+    // TODO: for short timezone names, check that en_001 has “∅∅∅” iff en has a value, and
+    // that en_001 otherwise has no short timezone names (except through inheritance).
+    // Reference: https://unicode-org.atlassian.net/browse/CLDR-14996
+    static final boolean CHECK_SHORT_TIMEZONE_NAMES = false;
     static final String TIME_FORMAT_PREFIX =
             "//ldml/dates/calendars/calendar[@type=\"gregorian\"]/timeFormats/timeFormatLength";
+
     static final String TIMEZONE_NAME_PREFIX = "//ldml/dates/timeZoneNames";
     static final String TIMEZONE_NAME_SHORT = "short";
 
-    /** en_001 should not have standard time formats or any short timezone names */
+    /** en_001 should not have standard time formats */
     @Test
     void testTime() {
         Factory factory = CLDRConfig.getInstance().getCommonAndSeedAndMainAndAnnotationsFactory();
@@ -21,14 +26,16 @@ public class TestEn001Time {
             assertFalse(
                     path.startsWith(TIME_FORMAT_PREFIX),
                     "Path starts with " + TIME_FORMAT_PREFIX + ": " + path);
-            assertFalse(
-                    path.startsWith(TIMEZONE_NAME_PREFIX) && path.contains(TIMEZONE_NAME_SHORT),
-                    "Path starts with "
-                            + TIMEZONE_NAME_PREFIX
-                            + " + and contains "
-                            + TIMEZONE_NAME_SHORT
-                            + ": "
-                            + path);
+            if (CHECK_SHORT_TIMEZONE_NAMES) {
+                assertFalse(
+                        path.startsWith(TIMEZONE_NAME_PREFIX) && path.contains(TIMEZONE_NAME_SHORT),
+                        "Path starts with "
+                                + TIMEZONE_NAME_PREFIX
+                                + " + and contains "
+                                + TIMEZONE_NAME_SHORT
+                                + ": "
+                                + path);
+            }
         }
     }
 }

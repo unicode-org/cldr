fd39c687555cdc780a472cedeaa5f6b8f9b35ecb
Steven R. Loomis <srl295@gmail.com>
2025-04-23 14:08:59 -0500

    CLDR-17829 move PathDescriptions.txt to PathDescriptions.md
    - add lychee build task to link check.
    - added PathDescriptionParser and unit tests

--- tools/cldr-code/src/test/java/org/unicode/cldr/util/TestPathDescription.java
+++ tools/cldr-code/src/test/java/org/unicode/cldr/util/TestPathDescription.java
@@ -1,5 +1,6 @@
 package org.unicode.cldr.util;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -7,7 +8,13 @@ import org.junit.jupiter.api.Test;
 public class TestPathDescription {
     @Test
     public void testParseGiantString() {
-        RegexLookup<String> l = PathDescription.parseLookupString();
-        assertNotNull(l);
+        final PathDescriptionParser parser = new PathDescriptionParser();
+        RegexLookup<Pair<String, String>> lookup =
+                parser.parse(PathDescription.getPathDescriptionString());
+        assertNotNull(lookup);
+        assertFalse(lookup.size() == 0, "lookup is empty");
+        String references = parser.getReferences();
+        assertNotNull(references);
+        assertFalse(parser.getReferences().trim().isBlank());
     }
 }

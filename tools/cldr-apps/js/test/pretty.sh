1227ef5beb3058d3daac8ce780cefdafe7b1b91c
Steven R. Loomis <srl295@gmail.com>
2025-05-06 12:11:32 -0500

    CLDR-14409 fetest: refactor so we can run client tests
    - split out cldrClient / cldrClientInternal
    - add a basic test of the REST api

--- tools/cldr-apps/js/test/pretty.sh
+++ tools/cldr-apps/js/test/pretty.sh
@@ -5,5 +5,6 @@ npx prettier@2.8.8 --no-error-on-unmatched-pattern --write \
     tools/cldr-apps/js/src/views/*.vue \
     tools/cldr-apps/js/test/*.{js,mjs} \
     tools/cldr-apps/js/test/nonbrowser/*.{js,mjs} \
+    tools/cldr-apps/js/test/client/*.{js,mjs} \
     tools/cldr-apps/js/test/*.html \
     tools/cldr-apps/js/util/*.{js,mjs}

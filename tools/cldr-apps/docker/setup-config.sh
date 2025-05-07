5608239e869846a01f75259c7a9335227d7c8c39
Steven R. Loomis <srl295@gmail.com>
2025-05-07 12:38:54 -0500

    CLDR-14409 iterate on server file

--- tools/cldr-apps/docker/setup-config.sh
+++ tools/cldr-apps/docker/setup-config.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 echo "Trying to determine CLDR version"
-CLDR_VERSION=$(java -jar $(find /opt/ol/wlp/usr/servers/defaultServer/ -name cldr-code*.jar) | fgrep GEN_VERSION | cut -d= -f2 | cut -d. -f1)
+java -jar /tmp/cldr-code.jar > /dev/null || exit 1
+CLDR_VERSION=$($(java -jar /tmp/cldr-code.jar) | fgrep GEN_VERSION | cut -d= -f2 | cut -d. -f1)
+rm -f /tmp/cldr-code.jar
 echo "CLDR_VERSION=${CLDR_VERSION}"
 CLDR_OLDVERSION=$(expr ${CLDR_VERSION} - 1)
 

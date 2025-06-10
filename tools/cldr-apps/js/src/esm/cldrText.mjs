09f2c245600b6b14aaa62c77799034def7d0bfe3
Tom Bishop <thomasbishop@wenlin.com>
2025-06-10 16:38:07 -0400

    CLDR-18744 Revise flagged item message in Survey Tool (#4802)

--- tools/cldr-apps/js/src/esm/cldrText.mjs
+++ tools/cldr-apps/js/src/esm/cldrText.mjs
@@ -188,6 +188,8 @@ const strings = {
   flag_desc:
     "This item has been flagged for review by the CLDR Technical Committee.",
   explainRequiredVotes: "Changes to this item require ${requiredVotes} votes.",
+  explainFlagForReview:
+    "To change this item, you have to flag it for review! See <a target='_blank' href='https://cldr.unicode.org/translation/getting-started/guide#changing-protected-items'>Changing Protected Items</a> for more information.",
   valueIsLocked:
     "This item has been locked by the CLDR Technical Committee. See the forum entry.",
   xpath_desc:

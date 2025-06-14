b52767ae7b6d9e185765f654fb5761b69f5ba99d
Mark Davis <mark@unicode.org>
2025-06-13 17:10:18 -0700

    CLDR-18758 Find plural rules with overlaps, by locale

--- tools/cldr-code/src/main/java/org/unicode/cldr/util/DtdData.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/DtdData.java
@@ -1881,7 +1881,7 @@ public class DtdData extends XMLFileReader.SimpleHandler {
                         .freeze();
     }
 
-    static MapComparator<String> countValueOrder =
+    public static MapComparator<String> countValueOrder =
             new MapComparator<String>()
                     .add("0", "1", "zero", "one", "two", "few", "many", "other")
                     .freeze();

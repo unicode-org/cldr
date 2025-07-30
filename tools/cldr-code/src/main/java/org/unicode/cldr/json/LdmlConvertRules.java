b4b2b928600731e24e8c6bf22562ed08037c8386
Robert Bastian <robertbastian@unicode.org>
2025-07-30 12:11:22 -0400

    fix generate-json

--- tools/cldr-code/src/main/java/org/unicode/cldr/json/LdmlConvertRules.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/json/LdmlConvertRules.java
@@ -27,6 +27,7 @@ class LdmlConvertRules {
     // common/main
     static final ImmutableSet<String> NAME_PART_DISTINGUISHING_ATTR_SET =
             ImmutableSet.of(
+                    "languages:language:type",
                     "monthWidth:month:yeartype",
                     "characters:parseLenients:scope",
                     "dateFormat:pattern:numbers",

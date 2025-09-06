1f3700fc06ab4e4269f0d17c7eaa83bfda87b238
Tom Bishop <thomasbishop@wenlin.com>
2025-09-06 09:03:45 -0400

    CLDR-18837 Button to download generated VXML in a zip (#5021)

--- tools/cldr-apps/src/main/java/org/unicode/cldr/web/api/GenerateVxml.java
+++ tools/cldr-apps/src/main/java/org/unicode/cldr/web/api/GenerateVxml.java
@@ -1,5 +1,6 @@
 package org.unicode.cldr.web.api;
 
+import java.io.File;
 import java.io.IOException;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.*;
@@ -12,6 +13,7 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.unicode.cldr.util.CLDRLocale;
+import org.unicode.cldr.util.Zipper;
 import org.unicode.cldr.web.*;
 
 @ApplicationScoped
@@ -133,5 +135,90 @@ public class GenerateVxml {
 
         @Schema(description = "Verification warning messages")
         public String[] verificationWarnings;
+    }
+
+    @GET
+    @Path("/download")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    @Operation(summary = "Download VXML", description = "Download VXML as zip file")
+    @APIResponses(
+            value = {
+                @APIResponse(
+                        responseCode = "200",
+                        description = "Download VXML",
+                        content =
+                                @Content(
+                                        mediaType = "application/zip",
+                                        schema = @Schema(implementation = Response.class))),
+                @APIResponse(responseCode = "403", description = "Forbidden"),
+                @APIResponse(responseCode = "404", description = "Not Found"),
+                @APIResponse(
+                        responseCode = "500",
+                        description = "Internal Server Error",
+                        content =
+                                @Content(
+                                        mediaType = "application/json",
+                                        schema = @Schema(implementation = STError.class))),
+            })
+    public Response downloadVxml(
+            @QueryParam("dirName")
+                    @Schema(
+                            description =
+                                    "The name (not full path) of the directory containing VXML")
+                    String dirName,
+            @HeaderParam(Auth.SESSION_HEADER) String sessionString) {
+        try {
+            CookieSession cs = Auth.getSession(sessionString);
+            if (cs == null) {
+                return Auth.noSessionResponse();
+            }
+            if (!UserRegistry.userIsTCOrStronger(cs.user)) {
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
+            if (SurveyMain.isBusted()
+                    || !SurveyMain.wasInitCalled()
+                    || !SurveyMain.triedToStartUp()) {
+                return STError.surveyNotQuiteReady();
+            }
+            cs.userDidAction();
+            File dir = getDirectory(dirName);
+            if (dir == null) {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+            File zipFile = Zipper.zipDirectory(dir);
+            return Response.ok(zipFile).header("Content-Type", "application/zip").build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e).build();
+        }
+    }
+
+    /**
+     * Sanitize the "user-provided" dirName, normally actually provided by the back end, but
+     * possibly concocted by evildoers. It should contain no slashes or periods, and should match
+     * one of the "vetdata-..." siblings of the automatic vetdata dir CookieSession.sm.getVetdir().
+     * Some of this sanitizing is redundant and is really intended to satisfy automatic checkers and
+     * prevent failures like "Uncontrolled data used in path expression".
+     *
+     * @param dirName the directory name (not full path)
+     * @return the directory (sibling of the automatic vetdata directory)
+     */
+    private File getDirectory(String dirName) {
+        if (dirName.contains("/") || dirName.contains(".")) {
+            return null;
+        }
+        File autoVetDir = CookieSession.sm.getVetdir();
+        if (!dirName.startsWith(autoVetDir.getName())) {
+            return null;
+        }
+        File parent = autoVetDir.getParentFile();
+        File[] children = parent.listFiles();
+        if (children != null) {
+            for (File child : children) {
+                if (dirName.equals(child.getName())) {
+                    return child;
+                }
+            }
+        }
+        return null;
     }
 }

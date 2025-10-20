f405c6c71f8fa060f8345c0e056d6960bc69dfe1
btangmu <thomasbishop@wenlin.com>
2025-10-20 11:36:21 -0400

    CLDR-18963 Explicit numberSystem attribute
    -Revise ldml.dtd to allow numberSystem attribute for pattern element
    
    -Add numberSystem (latn) for pattern/decimal/group elements where needed in locales ca, el, en_150, en_AT, eu, gl, it, tr
    
    -Re-enable the check for numberSystem in CheckNumbers.java
    
    -Use parts.containsAttribute instead of parts.getAttribute(2, ...) since the element index may not be 2

--- tools/cldr-code/src/main/java/org/unicode/cldr/test/CheckNumbers.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/test/CheckNumbers.java
@@ -259,12 +259,8 @@ public class CheckNumbers extends FactoryCheckCLDR {
         }
         XPathParts parts = XPathParts.getFrozenInstance(path);
 
-        // TODO: re-enable this commented-out check
-        // Reference: https://unicode-org.atlassian.net/browse/CLDR-18722
-        /*
         // Check that number formats are for an explicit number system.
-        String numberSystem = parts.getAttributeValue(2, "numberSystem");
-        if (numberSystem == null) {
+        if (!parts.containsAttribute("numberSystem")) {
             result.add(
                     new CheckStatus()
                             .setCause(this)
@@ -273,7 +269,6 @@ public class CheckNumbers extends FactoryCheckCLDR {
                             .setMessage(
                                     "Number formats must have an explicit numberSystem attribute."));
         }
-         */
 
         // Do tests that need to split the values
 

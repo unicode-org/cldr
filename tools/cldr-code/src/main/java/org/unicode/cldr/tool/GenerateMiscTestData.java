6a6414eb27d6f3b87eecc68e1b7b26c973a9133e
Mark Davis <mark@unicode.org>
2025-10-27 11:28:33 +0100

    CLDR-18847 Update

--- tools/cldr-code/src/main/java/org/unicode/cldr/tool/GenerateMiscTestData.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/tool/GenerateMiscTestData.java
@@ -1,9 +1,24 @@
 package org.unicode.cldr.tool;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.TreeMultimap;
+import com.ibm.icu.impl.Relation;
 import com.ibm.icu.impl.Row;
+import com.ibm.icu.impl.Row.R2;
+import com.ibm.icu.impl.Row.R4;
+import com.ibm.icu.text.DateFormat;
 import com.ibm.icu.text.DecimalFormat;
+import com.ibm.icu.text.SimpleDateFormat;
+import com.ibm.icu.util.TimeZone;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import org.unicode.cldr.util.CLDRConfig;
 import org.unicode.cldr.util.CLDRFile;
@@ -14,12 +29,14 @@ import org.unicode.cldr.util.Level;
 import org.unicode.cldr.util.NameGetter;
 import org.unicode.cldr.util.Organization;
 import org.unicode.cldr.util.StandardCodes;
+import org.unicode.cldr.util.SupplementalDataInfo;
 
 public class GenerateMiscTestData {
     private static final Factory CLDR_FACTORY = CLDRConfig.getInstance().getCldrFactory();
 
     public static void main(String[] args) {
-        generateGmt();
+        checkTimezoneTransitions();
+        // generateGmt();
     }
 
     public static void generateGmt() {
@@ -79,6 +96,96 @@ public class GenerateMiscTestData {
                         samples.stream()
                                 .map(x -> gmtData.format(x.get1(), x.get2(), guess, numSystem))
                                 .collect(Collectors.joining("\t")));
+            }
+        }
+    }
+
+    static void checkTimezoneTransitions() {
+        long jan15 = Instant.parse("2025-01-15T12:00:00Z").toEpochMilli();
+        long jul15 = Instant.parse("2025-07-15T12:00:00Z").toEpochMilli();
+        long jan15_next = Instant.parse("2026-01-15T12:00:00Z").toEpochMilli();
+        DateFormat df = new SimpleDateFormat("y-MM-dd", Locale.ENGLISH);
+        DateFormat hf = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+
+        SupplementalDataInfo SDI = CLDRConfig.getInstance().getSupplementalDataInfo();
+        /*
+        * <key name="tz" description="Time zone key" alias="timezone">
+            <type name="adalv" description="Andorra" alias="Europe/Andorra"/>
+            <type name="ancur" description="Curaçao" alias="America/Curacao" region="CW"/>
+            <type name="aukns" description="Currie, Australia" deprecated="true" preferred="auhba"/>
+        */
+        Set<String> shortTzIds = SDI.bcp47Key2Subtypes.get("tz");
+        Map<R2<String, String>, String> deprecated = SDI.getBcp47Deprecated();
+        Relation<R2<String, String>, String> aliases = SDI.getBcp47Aliases();
+
+        Map<Row.R4<String, String, Duration, Duration>, Multimap<String, String>> data =
+                new TreeMap<>();
+        for (String shortTzId : shortTzIds) {
+            R2<String, String> keySubtype = R2.of("tz", shortTzId);
+            if (deprecated.get(keySubtype).equals("true")) {
+                continue;
+            }
+            Set<String> a = aliases.get(keySubtype);
+            String zone = a.iterator().next();
+            TimeZone foo = TimeZone.getTimeZone(zone).freeze();
+            Duration jan15Offset = Duration.ofMillis(foo.getOffset(jan15));
+            Duration jul15Offset = Duration.ofMillis(foo.getOffset(jul15));
+            if (jan15Offset.equals(jul15Offset)) {
+                continue;
+            }
+            // find the transition dates
+            long first = findTransition(foo, jan15, jul15);
+            long second = findTransition(foo, jul15, jan15_next);
+            R4<String, String, Duration, Duration> key =
+                    Row.R4.of(df.format(first), df.format(second), jan15Offset, jul15Offset);
+            String region = shortTzId.substring(0, 2).toUpperCase(Locale.ENGLISH);
+            String[] parts = zone.split("/");
+            String lastPart = parts[parts.length - 1];
+            Multimap<String, String> map = data.get(key);
+            if (map == null) {
+                data.put(key, map = TreeMultimap.create());
+            }
+            map.put(region, lastPart);
+        }
+        data.entrySet().stream()
+                .forEach(
+                        x ->
+                                println(
+                                        "",
+                                        x.getKey().get0(),
+                                        "..",
+                                        x.getKey().get1(),
+                                        "\t",
+                                        formatDuration(x.getKey().get2()),
+                                        "\t",
+                                        formatDuration(x.getKey().get3()),
+                                        "\t➡︎\t",
+                                        Joiners.COMMA_SP.join(x.getValue().asMap().entrySet())));
+    }
+
+    private static double formatDuration(Duration duration) {
+        return duration.toMinutes() / 60.0;
+    }
+
+    private static void println(String joinWith, Object... toJoin) {
+        System.out.println(Joiner.on(joinWith).join(toJoin));
+    }
+
+    private static long findTransition(TimeZone foo, long start, long end) {
+        int startOffset = foo.getOffset(start);
+        int endOffset = foo.getOffset(end);
+        while (true) {
+            long mid = (start + end) / 2;
+            if (end - start < 86400) {
+                return mid;
+            }
+            int midOffset = foo.getOffset(mid);
+            if (midOffset == startOffset) {
+                start = mid;
+            } else if (midOffset == endOffset) {
+                end = mid;
+            } else {
+                throw new IllegalArgumentException("Bizarre behavior");
             }
         }
     }

02e2d09d9a4e0b1810e21bf278c1e827701eb8c0
Steven R. Loomis <srl295@gmail.com>
2026-06-17 22:04:52 -0500

    CLDR-19572 Fix VP loading issue
    - load VettingParticipation only a few tasks at a time
    - beginning of DownloadAllUserActivity

---
+++ tools/cldr-apps/js/src/views/DownloadAllUserActivity.vue
@@ -0,0 +1,33 @@
+<script setup>
+import * as cldrUserListExport from "../esm/cldrUserListExport.mjs";
+
+import { ref } from "vue";
+
+const message = ref("");
+const percent = ref(0);
+
+function canCancel() {
+  return false;
+}
+
+function progressBarStatus() {
+  return "normal";
+}
+
+function start() {}
+</script>
+
+<template>
+  <p>
+    This section will download a large .xlsx file with all of your user’s
+    activity for this release.
+  </p>
+
+  <button v-if="canCancel()" @click="cancel()">Cancel</button>
+  <button v-else @click="start()">Generate .xlsx</button>
+  <br />
+  <p class="progressBar">
+    <a-progress :percent="percent" :status="progressBarStatus()" />
+  </p>
+  <p v-if="message">{{ message }}</p>
+</template>
